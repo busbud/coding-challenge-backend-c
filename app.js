895c9cf55c2a46e756bd453ae31416832ec8b4df
Jonathan Llewellyn <inexistenz@gmail.com>
2014-05-02 22:43:18 -0400

    Now returns search portion of url as json string

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var http = require('http');
+var url = require('url');
 var port = process.env.PORT || 2345;
+
+var parseQuery = function(requestUrl) {
+    parsedUrl = url.parse(requestUrl, true);
+    return parsedUrl.query;
+}
 
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
   if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var suggestions = parseQuery(req.url);
+    console.log(suggestions);
+    res.end(JSON.stringify(suggestions));
   } else {
     res.end();
   }
