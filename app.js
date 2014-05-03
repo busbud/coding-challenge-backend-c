baf1f5ec7e899f7032ae697135390b2252726758
Jonathan Llewellyn <inexistenz@gmail.com>
2014-05-03 03:03:17 -0400

    Reading in and parsing tsv file and making easily searchable companion array

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 var http = require('http');
 var url = require('url');
-var port = process.env.PORT || 2345;
+var fs = require('fs');
+var tsv = require('tsv');
 
 var parseQuery = function(requestUrl) {
     parsedUrl = url.parse(requestUrl, true);
     return parsedUrl.query;
 }
+
+var readTable = function() {
+    var tableText = fs.readFileSync('data/cities_canada-usa.tsv','utf-8');
+    return tsv.parse(tableText);
+}
+
+var search = function(query) {
+    var q = query.toLowerCase();
+    console.log(q)
+    var res = [];
+    tableIndex.forEach(function(element,index) {
+        console.log(element.name);
+        if(element.name.indexOf(q) != -1) {
+            res.push(element);
+        }
+    });
+    return res;
+}
+
+var port = process.env.PORT || 2345;
+
+var table = readTable();
+var tableIndex = [];
+
+table.forEach(function(element, index) {
+    if(element.ascii != undefined)
+        tableIndex.push({name: element.ascii.toLowerCase(), index: index});
+});
+
+var result = search("Montreal");
+
+console.log(JSON.stringify(result));
 
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
