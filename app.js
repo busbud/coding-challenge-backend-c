65bb53e937621abdaede14c807c17b971c9b61d4
Dan Greencorn <dan@dangreencorn.ca>
2014-08-08 04:11:59 -0400

    Setting host to heroku hostname

--- app.js
+++ app.js
@@ -173,6 +173,6 @@ module.exports = http.createServer(function (req, res) {
   	//
     res.end();
   }
-}).listen(port, '127.0.0.1');
+}).listen(port, 'dangreencorn.herokuapp.com');
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
