4298e8992ecc98c17ec00a575445a42d6d437d09
Chris911 <christophe.naud.dulude@gmail.com>
2014-08-26 19:16:20 -0400

    Fix test cases

--- test/suggestions.js
+++ test/suggestions.js
@@ -51,7 +51,7 @@ describe('GET /suggestions', function() {
     it('contains a match', function () {
       expect(response.json.suggestions).to.satisfy(function (suggestions) {
         return suggestions.some(function (suggestion) {
-          return suggestion.name.test(/montreal/i);
+          return (/montreal/i).test(suggestion.name);
         });
       })
     });
@@ -67,7 +67,7 @@ describe('GET /suggestions', function() {
     it('contains scores', function () {
       expect(response.json.suggestions).to.satisfy(function (suggestions) {
         return suggestions.every(function (suggestion) {
-          return suggestion.latitude && suggestion.longitude;
+          return suggestion.score;
         });
       })
     });
