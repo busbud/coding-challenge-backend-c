c24a1a5c5b5d8bffab4840f0342c082e9ebc4914
Benjamin Suire <bensuire@gmail.com>
2014-09-26 01:18:22 +0200

    enabled query paramter parsing

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var http = require('http');
 var port = process.env.PORT || 2345;
 
+var geolocation = false; 
+
+function city(name,latitude,longitude,score){
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.score = score;
+}
+var london = new city("London, ON", "42,000","-2389",0.5);
+var london_bis = new city("London, NH", "42,000","-2389",0.4);
+
+
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
   if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
+  
+    var query = require('url').parse(req.url,true)["query"]  // parse query parameters from url 
+    console.log(query) // DEBUG
+
+    if (query.latitude === undefined){
+        geolocation = false;
+    } else {
+        geolocation = true;
+    } 
+    console.log("geolocation: "+ geolocation);
+
+    res.end(JSON.stringify({suggestions:[london,london_bis]},null,4));
+
   } else {
     res.end();
   }
