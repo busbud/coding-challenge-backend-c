cac48c0cc240ead71f7342ae757fa263357c3666
russellsaintcyr <russellsaintcyr@gmail.com>
2014-10-10 15:09:22 -0400

    Added tests for "valid city with non-ASCII characters (Québec)". Added tests for "Cities scored by location to user".

--- test/suggestions.js
+++ test/suggestions.js
@@ -3,12 +3,12 @@ var app     = require('../app');
 var request = require('supertest')(app);
 
 describe('GET /suggestions', function() {
-  describe('with a non-existent city', function () {
+  describe('non-existent city', function () {
     var response;
 
     before(function (done) {
       request
-        .get('/suggestions?q=SomeRandomCityInTheMiddleOfNowhere')
+        .get('/suggestions?q=Zzz')
         .end(function (err, res) {
           response = res;
           response.json = JSON.parse(res.text);
@@ -26,7 +26,7 @@ describe('GET /suggestions', function() {
     });
   });
 
-  describe('with a valid city', function () {
+  describe('valid city', function () {
     var response;
 
     before(function (done) {
@@ -71,7 +71,59 @@ describe('GET /suggestions', function() {
         });
       })
     });
-    
-    // my extra tests with a valid city
   });
+  
+  describe('valid city with non-ASCII characters (Québec)', function () {
+	    var response;
+
+	    before(function (done) {
+	      request
+	        .get('/suggestions?q=Québec')
+	        .end(function (err, res) {
+	          response = res;
+	          response.json = JSON.parse(res.text);
+	          done(err);
+	        });
+	    });
+
+	    it('returns a 200', function () {
+	      expect(response.statusCode).to.equal(200);
+	    });
+
+	    it('returns an array with 1 suggestion', function () {
+	      expect(response.json.suggestions).to.be instanceof(Array);
+	      expect(response.json.suggestions).to.have.length(1);
+	    });
+	  });
+  
+  describe('Cities scored by location to user', function () {
+	    var response;
+
+	    before(function (done) {
+	      request
+	        .get('/suggestions?q=Mont&latitude=45.496784&longitude=-73.574124')
+	        .end(function (err, res) {
+	          response = res;
+	          response.json = JSON.parse(res.text);
+	          done(err);
+	        });
+	    });
+
+	    it('returns a 200', function () {
+	      expect(response.statusCode).to.equal(200);
+	    });
+
+	    it('returns an array with 37 suggestions', function () {
+	      expect(response.json.suggestions).to.be instanceof(Array);
+	      expect(response.json.suggestions).to.have.length(37);
+	    });
+	    
+	    it('first array element is Montréal', function () {
+	        expect(response.json.suggestions).to.satisfy(function (suggestions) {
+	            return suggestions.some(function (suggestion) {
+	            return (suggestion.name.match(/montreal|montréal/i) == null) ? false : true;
+            });
+          })
+	    });
+	  });
 });
