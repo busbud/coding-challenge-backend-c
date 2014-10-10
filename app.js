ae983a4e28fce2988753912ec16591370d406fdc
russellsaintcyr <russellsaintcyr@gmail.com>
2014-10-10 12:56:19 -0400

    modified API_URL because of how Heroku handles ports

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 var api = require('./api-logic');
 
 // setup express environment
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 2345);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use('/public', express.static(__dirname + '/public'));
@@ -19,7 +19,7 @@ var dotenv = require('dotenv');
 dotenv.load();
 // load the environment
 var PORT = process.env.PORT || 2345;
-var API_HOST = process.env.API_HOST || 'localhost';
+var API_URL = process.env.API_URL || 'localhost';
 
 app.get('/suggestions', function(req, res) {
 	api.handleRequest(req,res);
@@ -27,12 +27,12 @@ app.get('/suggestions', function(req, res) {
 
 app.get('/apitest', function(req, res) {
     res.render('index', { 
-    	API_URL: 'http://' + API_HOST + ':' + PORT, 
+    	API_URL: 'http://' + API_URL, 
     	title: 'API endpoint test page', 
     	message: 'API endpoint test page'
     });
 });
 
 var server = app.listen(PORT, function() {
-	util.log(util.format('Server running at http://%s:%d', API_HOST, PORT));
+	util.log(util.format('Server running at http://%s', API_HOST));
 });
