8556828bb000d54d37b393b8de98e0c38ec0dfe5
Frederic Gingras <frederic@gingras.cc>
2014-10-24 19:27:01 -0400

    Bump NodeJS version & ignore node_modules/

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ var http = require('http');
 var port = process.env.PORT || 2345;
 
 module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
-
   if (req.url.indexOf('/suggestions') === 0) {
+
+    // Send suggestions
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({
       suggestions: []
     }));
+
   } else {
+
+    // Send 404 Not Found
+    res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end();
+
   }
 }).listen(port, '127.0.0.1');
 
