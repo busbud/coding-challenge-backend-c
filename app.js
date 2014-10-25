e719bc78803f5672bc2ff03fbc3f0835dc4ebca5
Frederic Gingras <frederic@gingras.cc>
2014-10-25 17:48:43 -0400

    Parsing of tsv cities
    Uses the `ya-csv` library, tried out fast-csv before but had problems
    with parsing and delimiters. The endpoint now temporarly outputs five
    cities.

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-var http = require('http');
+var http   = require('http');
+var path   = require('path');
+var _      = require('lodash');
+var Reader = require('./lib/reader');
+
 var port = process.env.PORT || 2345;
+var cities_file_path = path.join(__dirname, 'data', 'cities.tsv');
+var server_started = false;
 
-module.exports = http.createServer(function (req, res) {
-  if (req.url.indexOf('/suggestions') === 0) {
+var citiesReader = new Reader();
+citiesReader.load(cities_file_path, function(cities) {
+  if (server_started) return;
 
-    // Send suggestions
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
+  module.exports = http.createServer(function (req, res) {
+    if (req.url.indexOf('/suggestions') === 0) {
 
-  } else {
+      // Send suggestions
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({
+        suggestions: _.map(cities.slice(0,5), function(i) { return i.toObject(); })
+      }));
 
-    // Send 404 Not Found
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end();
+    } else {
 
-  }
-}).listen(port, '127.0.0.1');
+      // Send 404 Not Found
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end();
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+    }
+  }).listen(port, '127.0.0.1');
+
+  server_started = true;
+  console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+});
