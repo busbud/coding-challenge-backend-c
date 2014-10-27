b6b5978f62c76e50524283ed6b4cfb3d361aa74d
Frederic Gingras <frederic@gingras.cc>
2014-10-27 11:06:54 -0400

    Benchmark for city storage and benchmark for searching

--- app.js
+++ app.js
@@ -2,33 +2,41 @@ var http   = require('http');
 var path   = require('path');
 var _      = require('lodash');
 var Reader = require('./lib/reader');
+var async = require('async');
 
 var port = process.env.PORT || 2345;
 var cities_file_path = path.join(__dirname, 'data', 'cities.tsv');
-var server_started = false;
 
-var citiesReader = new Reader();
-citiesReader.load(cities_file_path, function(cities) {
-  if (server_started) return;
+async.waterfall([
+    function(done) {
+      var citiesReader = new Reader();
+      citiesReader.load(cities_file_path, function(cities) {
+        done(null, cities);
+      });
+    },
+    function(cities, done) {
 
-  module.exports = http.createServer(function (req, res) {
-    if (req.url.indexOf('/suggestions') === 0) {
+      module.exports = http.createServer(function (req, res) {
+        if (req.url.indexOf('/suggestions') === 0) {
 
-      // Send suggestions
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({
-        suggestions: _.map(cities.slice(0,5), function(i) { return i.toObject(); })
-      }));
+          // Send suggestions
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.end(JSON.stringify({
+            suggestions: _.map(cities.slice(0,5), function(i) { return i.toObject(); })
+          }));
 
-    } else {
+        } else {
 
-      // Send 404 Not Found
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end();
+          // Send 404 Not Found
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.end();
 
+        }
+      }).listen(port, '127.0.0.1');
+
+      console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+      done(cities);
     }
-  }).listen(port, '127.0.0.1');
+]);
 
-  server_started = true;
-  console.log('Server running at http://127.0.0.1:%d/suggestions', port);
-});
+
