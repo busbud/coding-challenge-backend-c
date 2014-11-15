bb7358c7775a8e816d4de03c6478677c11e0c172
mac-adam-chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-11-14 23:18:25 -0500

    Cleaned up the project

--- app.js
+++ app.js
@@ -9,72 +9,32 @@ var DATA_FILE_PATH = __dirname + '/data/cities_canada-usa.tsv';
 module.exports = http.createServer(function (req, res) {
   
   var suggestions = [];
+  var success = false;
 
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
-  if (req.url.indexOf('/suggestions') === 0) {
-  	var query_args = querystring.parse(req.url.substring(13));
-  	var q = query_args.q;
-  	var lon = query_args.longitude;
-  	var lat = query_args.latitude;
+  if (req.url.indexOf('/suggestions?') === 0) {
+  	var args = querystring.parse(req.url.substring(13));
 
   	fs.readFile(DATA_FILE_PATH, 'utf8', function(err, data) {
-  		var cities = data.split('\n');
+  		// Get city matches
+  		var cities = bh.getMatches(data.split('\n'), args.q);
   		
-  		// Split the lines of the data file
-  		// Then filter out the cities with population less than 5000
-  		// as well as cities that do not match the input string
-  		cities = cities.map(function(line) {
-  			return line.split('\t');
-  		}).filter(function(city) {
-  			return city[14] >= 5000 && city[1].indexOf(q) === 0;
-  		});
-  		
-  		// Make suggestions
-  		// Sort them by score
-  		suggestions = cities.map(function(city) {
-  			return {
-  				name: city[1]+", "+city[8],
-  				longitude: city[4],
-  				latitude: city[5],
-  				score: bh.getScore(city[1], city[4], city[5], q, lon, lat)
-  			};
-  		}).sort(function(a,b) {
-  			return b.score - a.score;
-  		});
+  		// Make suggestions based on matches and query input
+  		suggestions = bh.makeSuggestions(cities, args.q, args.longitude, args.latitude);
 
-  		if(suggestions.length > 0)
+  		if(suggestions.length > 0) {
   			res.writeHead(200);
-  		res.end(JSON.stringify({suggestions: suggestions}, null, 2));
+  			success = true;
+  		}
+  		res.end(JSON.stringify({
+  			success: success,
+  			suggestions: suggestions
+  		}, null, 2));
   	});
-  	// var stream = fs.createReadStream(DATA_FILE_PATH);
-  	// stream.pipe(res, {end : false});
-  	// stream.on('end', function() {
-  	// 	console.log(res);
-  	// });
   } else {
-  	res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end(JSON.stringify({
-    	status: 'bad endpoint',
-    	suggestions: suggestions
-    }));
+    res.end("You are probably looking for the Busbud suggestions endpoint. Try appending '/suggestions' to the base url.");
   }
 }).listen(port, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
-
-var f = function(res) {
-
-};
-
-// 1. Read URL, 
-// 		If it contains 'suggestions/' then parse query
-//			If parsed query is empty then return empty suggestions, statusCode = 404
-//			Else Search (filtering out cities less than 5000 population) the file
-//				 Compute scores
-//				 Sort by score
-//				 Set the suggestions to the sorted list
-//				 Set success flag
-//			Send the stringified JSON output, statusCode = 200
-//		Else statusCode = 404 
-//			 Send empty response, set failed flag
--- backend_helper.js
+++ backend_helper.js
@@ -1,12 +1,40 @@
+exports.MIN_POPULATION = 5000;
+
 // Computes the score based on how close the query match is
 // Includes how close the latitude and longitude are to the requested longitude and latitude as well
 exports.getScore = function(name, longitude, latitude, q, lon, lat) {
 	var name_score = 1 - Math.abs(name.length-q.length)/name.length;
 	var longitude_score = 1;
 	var latitude_score = 1; 
-	if(lon && lat) {
+	if(lon && !isNaN(lon))
 		longitude_score = 1 - Math.abs(longitude-lon)/longitude;
+	if(lat && !isNaN(lat))
 		latitude_score = 1 - Math.abs(latitude-lat)/latitude;
-	};
-	return (name_score + longitude_score + latitude_score)/3;
+	return Math.min(1,((name_score + longitude_score + latitude_score)/3).toFixed(3));
 };
+
+// Computes the matches between the input query argument and cities in the data
+// Filters out the cities with population below minimum threshold
+// as well as cities that do not match the input string
+exports.getMatches = function(data, q) {
+	return data.map(function(line) {
+		return line.split('\t');
+  	}).filter(function(city) {
+  		return city[14] >= exports.MIN_POPULATION && city[1].indexOf(q) === 0;
+  	});
+}
+
+// Format the suggestions, compute their score based on given inputs
+// Then sort them by score
+exports.makeSuggestions = function(cities, q, longitude, latitude) {
+	return cities.map(function(city) {
+		return {
+			name: city[1]+", "+city[8],
+  			longitude: city[4],
+  			latitude: city[5],
+  			score: exports.getScore(city[1], city[4], city[5], q, longitude, latitude)
+  		};
+  	}).sort(function(a,b) {
+  		return b.score - a.score;
+  	});
+}
