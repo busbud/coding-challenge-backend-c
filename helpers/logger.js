5601806330460b16160c24b4c6e9db08f4627136
Issam Hakimi aka Killix <issam.hakimi@gmail.com>
2014-11-29 11:56:08 -0500

    add logger helpers

---
+++ helpers/logger.js
@@ -0,0 +1,23 @@
+// logger function
+//
+// @param {string} title
+// @return {function}
+//
+var debug = require('debug');
+var util = require('util');
+var DEBUG_MODE = process.env.DEBUG_MODE || 'false';
+
+module.exports = function logger(space) {
+  var log = debug('suggestions:'+space);
+  return function(str, str1, str2){
+    if (str2 != undefined) {
+      log(util.format(str,str1,str2));
+    } else if (str1 != undefined) {
+      log(util.format(str,str1));
+    } else {
+      log(util.format(str));
+    }
+  }
+
+}
+
