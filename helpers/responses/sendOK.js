57ad2754be23a08ad120f0747ba127b875350b2a
Issam Hakimi aka Killix <issam.hakimi@gmail.com>
2014-11-29 22:38:28 -0500

    rename response ok file to sendOK

--- helpers/responses/sendOK.js
+++ helpers/responses/sendOK.js
@@ -4,7 +4,7 @@
 // Usage:
 // return sendOK(res, data);
 
-module.exports = function sendOK (res, data) {
+module.exports = function sendOK(res, data) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   var json = JSON.stringify(data);
   return res.end(json);
