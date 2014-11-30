980401038ef7a32800da7e1260c46e5088551c05
Issam Hakimi aka Killix <issam.hakimi@gmail.com>
2014-11-29 22:45:51 -0500

    add command line tool for convert .tsv file to .json

---
+++ tools/tsv_to_json.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+
+// command line tool for convert tsv file to json
+
+var program = require('commander'),
+    http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    tsv = require("node-tsv-json");
+    //mkdirp = require('mkdirp');
+
+program
+  .version('0.0.1')
+  .option('-s, --source <path>', 'file to convert')
+  .option('-d, --destination <path>', 'file to output json')
+  .parse(process.argv);
+
+if (typeof(program.source) === 'string' && typeof(program.destination) === 'string' ) {
+  if (fs.existsSync(program.source)) {
+    tsvToJson(program.source, program.destination);
+  } else{
+    console.error('Source File: ' + sourceFile+ ' not found.');
+    process.exit(1);
+  }
+} else {
+  program.help();
+}
+
+
+function tsvToJson(src,output) {
+  return tsv({ input: src,
+               output: output,
+               parseRows: false }, function(err, result){
+                if(err) {
+                  console.error(err);
+                  process.exit(1);                    
+                }
+               });
+}
