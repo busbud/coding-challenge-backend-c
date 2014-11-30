766227288ac9bb2aa7d5d200d59f190582a9ca7d
Issam Hakimi aka Killix <issam.hakimi@gmail.com>
2014-11-30 02:17:47 -0500

    rename servers variable by cluster and add return

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var Cluster = require('./cluster'),
-      logger = require('./helpers/logger'),
-      PORT = process.env.PORT || 2345,
-      MEMCACHED = process.env.MEMCACHED || null,
-      DEBUG_MODE = process.env.DEBUG || false;
+module.exports = (function(){
+  var environment = process.env.NODE_ENV || 'development',
+        logger = require('./helpers/logger'),
+        PORT = process.env.PORT || 2345,
+        MEMCACHED = process.env.MEMCACHED || null,
+        DEBUG_MODE = process.env.DEBUG || false,
+        Cluster = require('./cluster');
 
-  var servers = new Cluster('suggestions',{
+  var cluster = new Cluster('suggestions',{
     'port':PORT,
     'memcached':MEMCACHED
   });
-  servers.initialize();
-
-  if(DEBUG_MODE != false) servers.watchMemory();
+  cluster.initialize();
+  if(DEBUG_MODE != false) cluster.watchMemory();
+  return cluster;
 })();
+
+
