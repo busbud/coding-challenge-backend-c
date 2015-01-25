7787a83475e060ad96627d8dede59c88566dac7c
Laurens Coderre <lcoderre@me.com>
2015-01-25 06:59:34 -0500

    add /reset instead

--- app.js
+++ app.js
@@ -86,144 +86,147 @@ app.route('/suggestions').get(function(req, res) {
 
 });
 
+app.route('/reset').get(function(req, res) {
+    async.waterfall([
+        function deleteIndex(callback){
+            // Uncomment this line to skip the whole seeding/prepping of ES instance
+            //return callback('skip');
+            console.log("Find this comment in the code to skip all this process.");
 
-var server = http.createServer(app);
+            console.log("Deleting the index 'cities'")
+            client.indices.delete({ index: 'cities', ignore: [404] }, function (error, response) {
+                callback(error);
+            });
+        },    
+        function create(callback){
+            console.log("Creating the index 'cities'")
+            client.indices.create({ index: 'cities' }, function (error, response) {
+                callback(error);
+            });
+        },    
+        function close(callback){
+            console.log("Closing the index 'cities'")
+            client.indices.close({ index: 'cities' }, function (error, response) {
+                callback(error);
+            });
+        },
+        function putSettings(callback){
+            console.log("Changing settings in the index 'cities' (analyzer and filters)")
 
-async.waterfall([
-    function deleteIndex(callback){
-        // Uncomment this line to skip the whole seeding/prepping of ES instance
-        //return callback('skip');
-        console.log("Find this comment in the code to skip all this process.");
-
-        console.log("Deleting the index 'cities'")
-        client.indices.delete({ index: 'cities', ignore: [404] }, function (error, response) {
-            callback(error);
-        });
-    },    
-    function create(callback){
-        console.log("Creating the index 'cities'")
-        client.indices.create({ index: 'cities' }, function (error, response) {
-            callback(error);
-        });
-    },    
-    function close(callback){
-        console.log("Closing the index 'cities'")
-        client.indices.close({ index: 'cities' }, function (error, response) {
-            callback(error);
-        });
-    },
-    function putSettings(callback){
-        console.log("Changing settings in the index 'cities' (analyzer and filters)")
-
-        client.indices.putSettings({ 
-            index: 'cities',
-            body: {
-                index: {
-                    "analysis" : {
-                        "analyzer" : {
-                            "autocomplete_analyzer" : {
-                                "type" : "custom",
-                                "tokenizer" : "lowercase",
-                                "filter"    : ["asciifolding", "title_ngram"]
+            client.indices.putSettings({ 
+                index: 'cities',
+                body: {
+                    index: {
+                        "analysis" : {
+                            "analyzer" : {
+                                "autocomplete_analyzer" : {
+                                    "type" : "custom",
+                                    "tokenizer" : "lowercase",
+                                    "filter"    : ["asciifolding", "title_ngram"]
+                                }
+                            },
+                            "filter" : {
+                                "title_ngram" : {
+                                    "type" : "edgeNGram",
+                                    "min_gram": 3,
+                                    "max_gram" : 20
+                                }
                             }
+                        }
+                    }
+                }
+            }, function (error, response) {
+                callback(error);
+            });
+        },
+        function putMapping(callback){
+            console.log("Changing mapping in the index 'cities'")
+            client.indices.putMapping({ 
+                index: 'cities',
+                type: 'city',
+                body: { 
+                    "properties" : {
+                        name : { 
+                            type : "string",
+                            "analyzer": "autocomplete_analyzer",
+                            "boost": 15
                         },
-                        "filter" : {
-                            "title_ngram" : {
-                                "type" : "edgeNGram",
-                                "min_gram": 3,
-                                "max_gram" : 20
-                            }
+                        point :{
+                            type: "geo_point",
+                            lat_lon: true
                         }
                     }
                 }
-            }
-        }, function (error, response) {
-            callback(error);
-        });
-    },
-    function putMapping(callback){
-        console.log("Changing mapping in the index 'cities'")
-        client.indices.putMapping({ 
-            index: 'cities',
-            type: 'city',
-            body: { 
-                "properties" : {
-                    name : { 
-                        type : "string",
-                        "analyzer": "autocomplete_analyzer",
-                        "boost": 15
-                    },
-                    point :{
-                        type: "geo_point",
-                        lat_lon: true
-                    }
-                }
-            }
-        }, function (error, response) {
-            callback();
-        });
-    },
-    function open(callback){
-        console.log("Reopening the index 'cities'");
-        client.indices.open({ index: 'cities' }, function (error, response) {
-            callback(error);
-        });
-    },
-    function seed(callback) {
-        console.log("Seeding the ES instance with the cities_canada-usa.tsv data... Might take a few moments.")
-
-        var fs = require('fs');
-
-        fs.readFile('data/cities_canada-usa.tsv', function (err, data) {
-            if (err) console.log (err);
-
-            data = String(data).split("\n");
-            data = _.map(data, function (d) {
-                return d.split("\t");
+            }, function (error, response) {
+                callback();
             });
-
-            // first row is the keys
-            var keys = _.first(data); 
-
-            // making an array of objects with proper keys-values
-            data = _.map(_.rest(data), function (d) {
-                var city = {};
-                for (var i = 0; i < keys.length; i++) {
-                    city[keys[i]] = d[i];
-                };
-                return city;
+        },
+        function open(callback){
+            console.log("Reopening the index 'cities'");
+            client.indices.open({ index: 'cities' }, function (error, response) {
+                callback(error);
             });
+        },
+        function seed(callback) {
+            console.log("Seeding the ES instance with the cities_canada-usa.tsv data... Might take a few moments.")
 
-            // indexing
-            async.each(data, function(city, cb) {
-                client.index({
-                    index: 'cities',
-                    type: 'city',
-                    id: city.id,
-                    body: {
-                        name: city.name,
-                        display: city.name + ', ' + city.admin1 + ', ' + city.country,
-                        point: {
-                            lat: Number(city.lat),
-                            lon: Number(city.long)
-                        }
-                    }
-                }, function (error, response) {
-                    cb();
+            var fs = require('fs');
+
+            fs.readFile('data/cities_canada-usa.tsv', function (err, data) {
+                if (err) console.log (err);
+
+                data = String(data).split("\n");
+                data = _.map(data, function (d) {
+                    return d.split("\t");
                 });
 
-            }, function (err) {
-                callback();
+                // first row is the keys
+                var keys = _.first(data); 
+
+                // making an array of objects with proper keys-values
+                data = _.map(_.rest(data), function (d) {
+                    var city = {};
+                    for (var i = 0; i < keys.length; i++) {
+                        city[keys[i]] = d[i];
+                    };
+                    return city;
+                });
+
+                // indexing
+                async.each(data, function(city, cb) {
+                    client.index({
+                        index: 'cities',
+                        type: 'city',
+                        id: city.id,
+                        body: {
+                            name: city.name,
+                            display: city.name + ', ' + city.admin1 + ', ' + city.country,
+                            point: {
+                                lat: Number(city.lat),
+                                lon: Number(city.long)
+                            }
+                        }
+                    }, function (error, response) {
+                        cb();
+                    });
+
+                }, function (err) {
+                    console.log("Done seeding.")
+                    callback();
+                })
             })
-        })
-}], 
-function search(err, result) {
-    if (err) console.log(err);
-    else {
-        console.log("Express server started on port " + port);
-        console.log ("Okay, ready to give suggestions.");
-    }
+    }], 
+    function search(err, result) {
+        if (err) console.log(err);
+        else {
+        }
+
+        res.status(200);
+    });
+
 });
 
+var server = http.createServer(app);
 server.listen(port);
+console.log("Express server started on port " + port);
 exports = module.exports = app
