e8896616856de78fc00a3b2ef9bcc319038e4d6e
Cole-macd <colemacdonald@Coles-MacBook-Pro.local>
2015-03-13 15:15:10 -0400

    Add app.js and package.json to last commit

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var http = require('http');
+// Setup
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 2345;
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
+// Suggestions
+app.get('/suggestions', function(req, res){
+  res.json({ 
+  	suggestions:[]
+  });
+});
+
+
+app.listen(port);
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
