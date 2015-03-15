42ca6f66a1a25bd905c23dabf8279d27ddb77990
Jean-Simon Barry <jean.simon.barry@gmail.com>
2015-03-14 23:11:46 -0400

    fixed limit problem where results wouldnt be in set. Refactored aggregates to make it more clear

--- data/locations.js
+++ data/locations.js
@@ -8,54 +8,77 @@ mongoose.connect('mongodb://localhost/location-db', function(err) {
         console.log('connection to db successful');
     }
 });
-	//need to search for lat and longetitude if present
-	//need to sanitize query input
-	//locations.aggregate( [ { $match: { $text : { $search : queryString.q } } }, 
-	//the query must at least start with the correct word.
+
+//allow match on 1 extra character or 1 missing character.
+//also allow match on 1 modified character
+//and finally match on any two letters interchanged
+//allowing any more than that will return erroneous results...if the user types in Shanghai
+//he shouldnt expect to see Berlin in the suggestions.
+
+function createRegex(cityName){
+    var regex = "/";
+
+    for (var i = 0, len = cityName.length; i <= len; i++) {
+	regex = regex + '^' + cityName.substring(0,i) + '.' + cityName.substring(i,len) + '|';
+    }
+
+    for (var i = 0, len = cityName.length; i <= len; i++) {
+	regex = regex + '^' + cityName.substring(0,i) + '.' +  cityName.substring(i+1,len) + '$|'; 
+	regex = regex + '^' +  cityName.substring(0,i) +  cityName.substring(i+1,len) + '$|' ;
+	regex = regex + '^' +  cityName.substring(0,i) +  
+	    cityName.substring(i+1,i+2) +  
+	    cityName.substring(i,i+1) +  
+	    cityName.substring(i+2, len) + '$';
+    }
+    regex = regex + "/";
+    //console.log(regex);
+    return regex;
+    
+}
 
 function constructParams(queryString, params){
-    var aggregateParams = [];
+    var aggregates = [];
+    var geoNear = {$geoNear: {
+	near : { type: "Point", coordinates: [ parseFloat(queryString.longitude) ,  parseFloat(queryString.latitude) ] },
+	distanceField: "dist.calculated",
+	spherical: true,
+	query : {population : { $gt : 5000 }},
+	limit : 100000
+	}
+	
+    };
+
+    var sort =  { $sort: { 
+	score: { $meta: "textScore" }, name: 1 
+    }};
+    var project =   { $project : { 
+	"ascii" : 1, 
+	"name" : 1,
+	"country" : 1, 
+	"loc" : 1,
+	"admin1" : 1,
+	"dist.calculated" : 1,
+	"population" : 1
+    }};
+
     if(queryString.longitude != null && queryString.latitude != null){
 	console.log("long/lat sent");
-	aggregateParams.push({ 
-	    $geoNear: {
-		near : { type: "Point", coordinates: [ parseFloat(queryString.longitude) ,  parseFloat(queryString.latitude) ] },
-		distanceField: "dist.calculated",
-		spherical: true,
-		limit : 10
+	aggregates.push(geoNear);
+    }
 
-	    }
-	}
-		      );
-    }
     if(queryString.q != null){
-	aggregateParams.push( 
-	    { $match: { 
-		//how to deal with accents?
-		name : { $regex : new RegExp("^"+queryString.q), $options:'i' },
-		population : { $gt : 5000 }
-	    }},
-	    { $sort: { 
-		score: {$meta: "textScore" }, name: 1 }
-	    },
-	    { $project : { 
-		"ascii" : 1, 
-		"name" : 1,
-		"country" : 1, 
-		"loc" : 1,
-		"admin1" : 1,
-		"score" : 1,
-		"dist.calculated" : 1,
-		"population" : 1
-	    }
-	    });
+	var match =  { $match: { 
+	    name : { $regex : createRegex(queryString.q), $options:'i' },
+	    population : { $gt : 5000 }
+	}};
+	aggregates.push(match, sort)
     }
-    return aggregateParams;
+    aggregates.push(project);
+    return aggregates;
 }
 
 var locations = {
     search : function(queryString, callback){
-	//console.log(queryString.longitude + " " + queryString.latitude + " long and lat");
 	locationObject.aggregate(constructParams(queryString, null), function(err, locs){
 	    if(err){
 		console.log(err);
@@ -64,7 +87,6 @@ var locations = {
 	    
 	    else{
 		console.log("successfull query execution");
-		//console.log(locs[1]); //debugging
 		callback(null,locs);
 	    }
 	});
@@ -72,3 +94,4 @@ var locations = {
 };
 
 module.exports = locations;
+
