b3d7a8db6f1c0848a4b51b887da5c65d34a89360
Jean-Simon Barry <jean.simon.barry@gmail.com>
2015-03-15 13:41:55 -0400

    all tests pass

--- app.js
+++ app.js
@@ -8,24 +8,20 @@ module.exports = http.createServer(function (req, res) {
     //use express routes here instead?
     if (req.url.indexOf('/suggestions') === 0) {
 	var queryString = url.parse(req.url, true).query;
-	console.log('query is' + JSON.stringify(queryString));
 	locations.search(queryString, function(err, suggestions){
-	    if(err){
+	    if(err || suggestions.length === 0){
 		console.log(err);
-		res.statusCode = 404;
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.end();
+		res.statusCode = '404';
+		res.writeHead('404', {'Content-Type': 'text/plain'});
+		res.end(JSON.stringify({suggestions : []}));
 	    }
-	    
-	    else
-		//TODO:set content and header
-		res.writeHead(200, {"Content-Type": "application/json; charset=utf-8;"});
-		res.end(JSON.stringify(suggestions, null, 2));
-	});
-		  
+	    else{
+		res.writeHead('200', {"Content-Type": "application/json; charset=utf-8;"});
+		res.end(JSON.stringify({suggestions : suggestions}, null, 2));
+	    }
+	});		  
     }
     else{
-	//TODO:set content and header
 	res.end();
     }
   }).listen(port, '127.0.0.1');
--- data/locations.js
+++ data/locations.js
@@ -17,7 +17,6 @@ mongoose.connect('mongodb://localhost/location-db', function(err) {
 
 function createRegex(cityName){
     var regex = "/";
-
     for (var i = 0, len = cityName.length; i <= len; i++) {
 	regex = regex + '^' + cityName.substring(0,i) + '.' + cityName.substring(i,len) + '|';
     }
@@ -43,31 +42,35 @@ function constructParams(queryString, params){
 	spherical: true,
 	query : {population : { $gt : 5000 }},
 	limit : 100000
-	}
-	
+	}	
     };
-
     var sort =  { $sort: { 
-	score: { $meta: "textScore" }, name: 1 
+	score: { $meta: "textScore" } , name : 1,
     }};
     var project =   { $project : { 
 	"ascii" : 1, 
-	"name" : 1,
 	"country" : 1, 
 	"loc" : 1,
 	"admin1" : 1,
 	"dist.calculated" : 1,
-	"population" : 1
+	"population" : 1,
+	"_id" : 0,
+	"score" : {$literal : 0.1},
+	"name" : { $concat : ["$ascii" , ", " , {$substr : ["$admin1", 0, 2]}, ", " , "$country"] }
     }};
 
     if(queryString.longitude != null && queryString.latitude != null){
-	console.log("long/lat sent");
-	aggregates.push(geoNear);
+	aggregates.push(geoNear, sort);
     }
 
     if(queryString.q != null){
+	var regex = createRegex(queryString.q);
 	var match =  { $match: { 
-	    name : { $regex : createRegex(queryString.q), $options:'i' },
+	    $or : [
+		{name : { $regex : regex, $options:'i' }},
+		{ascii : { $regex : regex, $options:'i'}}
+	    ]
+	    ,
 	    population : { $gt : 5000 }
 	}};
 	aggregates.push(match, sort)
@@ -85,7 +88,6 @@ var locations = {
 	    }
 	    
 	    else{
-		console.log("successfull query execution");
 		callback(null,locs);
 	    }
 	});
