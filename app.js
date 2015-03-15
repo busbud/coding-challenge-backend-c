0d5942beabb59d1eb318f526d5ab6e3b4f248898
Cole-macd <colemacdonald@Coles-MacBook-Pro.local>
2015-03-14 22:52:19 -0400

    Added typechecks on numeric inputs and modified testing file so regex matches

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Setup
 var express = require('express');
 var app = express();
+module.exports = app;
+
 //var url = require('url');
 var port = process.env.PORT || 2345;
 
@@ -10,15 +12,37 @@ data.updateRecords('data/cities_canada-usa.tsv');
 
 // Suggestions
 app.get('/suggestions', function(req, res){
-  var q = req.query.q;
-  var lon = req.query.longitude;
-  var lat = req.query.latitude;
-  var lim = req.query.limit;
-  var matches = data.getMatches(q.toLowerCase(), lon, lat, lim);
-  if (matches.length > 0)
-		res.end(JSON.stringify({ suggestions: matches }, null, 2));
-	else
-		res.status(404).send('No matches found');
+  	var q = req.query.q;
+  	var lon = req.query.longitude;
+  	var lat = req.query.latitude;
+  	var lim = req.query.limit;
+
+  	var errorMessage = [];
+	if (req.query.longitude != undefined && isNaN(parseFloat(lon))) {
+  		errorMessage.push("Longitude must be a float");
+  	};
+  	if (req.query.latitude != undefined && isNaN(parseFloat(lat))) {
+  		errorMessage.push("Latitude must be a float");
+  	};
+  	if (req.query.limit != undefined && isNaN(parseInt(lim))) {
+  		errorMessage.push("Limit must be an integer");
+  	};
+
+  	if (errorMessage.length > 0) {
+  		res.status(404).json({
+			Errors: errorMessage
+		});
+  	} else {
+
+  		var matches = data.getMatches(q.toLowerCase(), lon, lat, lim);
+  		if (matches.length > 0) {
+			res.end(JSON.stringify({ suggestions: matches }, null, 2));
+	  	} else {
+			res.status(404).json({
+				suggestions: []
+			});
+		}
+	}
 });
 
 
