67f914bf255ed8cbb94fc5195dc661e76e4f91ac
Jean-Simon Barry <jean.simon.barry@gmail.com>
2015-03-17 10:37:29 -0400

    removed first document generated

--- data/createIndex.js
+++ data/createIndex.js
@@ -1,5 +1,5 @@
 var conn = new Mongo();
-var db = conn.getDB("location-db");
+var db = conn.getDB("location-dbxxx");
 
 //create index here for full text search on the City names
 db.locations.createIndex({ascii : "text"});
@@ -11,29 +11,33 @@ db.locations.find().snapshot().forEach(function(myDoc){
     delete myDoc.longitude;
     //just update here instead of issuing another $update command to mongo
     if(myDoc.admin1 == 1)
-	myDoc.admin1 = "AB"
+	myDoc.admin1 = "AB";
     if(myDoc.admin1 == 2)
-	myDoc.admin1 = "BC"
+	myDoc.admin1 = "BC";
     if(myDoc.admin1 == 3)
-	myDoc.admin1 = "MB"
+	myDoc.admin1 = "MB";
     if(myDoc.admin1 == 4)
-	myDoc.admin1 = "NB"
+	myDoc.admin1 = "NB";
     if(myDoc.admin1 == 5)
-	myDoc.admin1 = "NL"
+	myDoc.admin1 = "NL";
     if(myDoc.admin1 == 7)
-	myDoc.admin1 = "NS"
+	myDoc.admin1 = "NS";
     if(myDoc.admin1 == 8)
-	myDoc.admin1 = "ON"
+	myDoc.admin1 = "ON";
     if(myDoc.admin1 == 9)
-	myDoc.admin1 = "PE"
+	myDoc.admin1 = "PE";
     if(myDoc.admin1 == 10)
-	myDoc.admin1 = "QC"
+	myDoc.admin1 = "QC";
     if(myDoc.admin1 == 11)
-	myDoc.admin1 = "SK"
+	myDoc.admin1 = "SK";
     if(myDoc.admin1 == 12)
-	myDoc.admin1 = "YT"
+	myDoc.admin1 = "YT";
     if(myDoc.admin1 == 13)
-	myDoc.admin1 = "NT"
-    db.locations.save(myDoc)});
+	myDoc.admin1 = "NT";
+    db.locations.save(myDoc);
+});
+//remove the document which has the fields as values
+db.locations.remove({name : "name"});
+//create the 2dsphere index
+db.locations.createIndex( {loc: "2dsphere" });
 
-db.locations.createIndex( { "loc": "2dsphere" } )
