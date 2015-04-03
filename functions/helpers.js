e1eb032bcde2d9c15319741b016fb29cf2630b9a
Dustin Blackman <dev@dustinblackman.com>
2015-04-03 13:23:50 -0400

    SortByScore and RemoveAccents (frontend and backend)

---
+++ functions/helpers.js
@@ -0,0 +1,27 @@
+module.exports = {
+  sortByScore: function(a, b) {
+    if (a.score < b.score) {
+      return 1;
+    }
+    if (a.score > b.score) {
+      return -1;
+    }
+    return 0;
+  },
+
+  removeAccents: function(s) {
+    var r;
+    r = s.toLowerCase();
+    r = r.replace(new RegExp('[àáâãäå]', 'g'), 'a');
+    r = r.replace(new RegExp('æ', 'g'), 'ae');
+    r = r.replace(new RegExp('ç', 'g'), 'c');
+    r = r.replace(new RegExp('[èéêë]', 'g'), 'e');
+    r = r.replace(new RegExp('[ìíîï]', 'g'), 'i');
+    r = r.replace(new RegExp('ñ', 'g'), 'n');
+    r = r.replace(new RegExp('[òóôõö]', 'g'), 'o');
+    r = r.replace(new RegExp('œ', 'g'), 'oe');
+    r = r.replace(new RegExp('[ùúûü]', 'g'), 'u');
+    r = r.replace(new RegExp('[ýÿ]', 'g'), 'y');
+    return r;
+  }
+};
