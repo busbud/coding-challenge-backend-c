09b75a99ca0c2bcf5df5c69600cc84c5cb541b7c
Robbie Caputo <hebime@hotmail.com>
2015-05-18 19:39:21 -0400

    added dataBuilder to app.js

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var express    = require('express'),
-    reqHandler = require('./reqHandler');
+var express     = require('express'),
+    reqHandler  = require('./reqHandler')
+    dataBuilder = require('./dataBuilder');
 
 var port = process.env.PORT || 2345;
 var app = express();
+var db = new dataBuilder();
 
+//set up routes
 app.get('/suggestions', reqHandler.getSuggestions)
 
+//catch all
 app.use(function(req, res, next) {
     res.set('Connection', 'close')
     res.set('Content-Type', 'text/plain');
     res.status(404).send()
 });
 
-var server = app.listen(port, function() {
-   console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+//prime the db
+db.primeRead().on('primeReadDone', function() {
+   app.listen(port, function() {
+     console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+   });
 });
 
-module.exports = app
+module.exports = {
+    app: app,
+    db: db
+}
