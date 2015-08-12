2aadb7e17b1b5d523cd2ed392cea34e0a39d5218
Kelvin Hammond <hammond.kelvin@gmail.com>
2015-08-12 10:16:30 -0400

    Added data loading with search

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
+var _ = require('lodash');
 var http = require('http');
 var host = process.env.HOST || '127.0.0.1';
 var port = process.env.PORT || 2345;
+var data = require('./data.js');
+
+var input_filepath = './data/cities_canada-usa.tsv';
+data.readData(input_filepath).on("end", function() {
+    console.log("Finished loading country data");
+});
+
+/**
+ * Gets the suggestions from the database and returns an array of dictionaries with scores
+ * @param {Object} params - query params
+ */
+function getSuggestions(params) {
+    if(_.isUndefined(params.q)) {
+        return [];
+    }
+    var q = _.trim(params.q);
+    if(_.isEmpty(q)) {
+        return [];
+    }
+
+    var results = data.search(q);
+    return results;
+}
 
 module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
+    if (req.url.indexOf('/suggestions') === 0) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+
+        var parsed_url = require('url').parse(req.url, true);
+        res.end(JSON.stringify({
+            suggestions: getSuggestions(parsed_url.query)
+        }));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end();
+    }
 }).listen(port, host);
 
 console.log('Server running at http://%s:%d/suggestions', host, port);
---
+++ data.js
@@ -0,0 +1,89 @@
+/**
+ * Gets the application data and stores it into an sqlite3 database
+ */
+var fs = require("fs");
+var es = require("event-stream");
+var _ = require('lodash');
+
+var DATA = [];
+
+/**
+ * Parses a line from the database
+ * @param {array} data - to be parsed 
+ * @warning internal function, should not be called alone
+ */
+function parseLine(data) {
+    var id = data[0],
+        name = _.trim(data[1]),
+        alt_names = data[3],
+        lat = data[4],
+        longi = data[5];
+
+    // make alt_names an array
+    if(!_.isUndefined(alt_names)) {
+        alt_names = _.filter(alt_names.split(','), function(alt) {
+            return !_.isEmpty(_.trim(alt));
+        });
+    } else {
+        alt_names = [];
+    }
+
+    // insert values
+    return {
+        geonames_id: id,
+        name: name,
+        alt_names: alt_names,
+        latitude: lat,
+        longitude: longi
+    };
+}
+
+/** 
+ * reads data from the file and calls parseLine to format the data
+ * @param {string} filepath - url of file to parse
+ */
+function readData(filepath) {
+    console.log('parsing file data');
+    // read file
+    return fs.createReadStream(filepath)
+        // split lines
+        .pipe(es.split("\n"))
+        // split tabs into array
+        .pipe(es.mapSync(function(data) {
+            return data.split("\t");
+        }))
+        // parse data
+        .pipe(es.mapSync(function(data) {
+            DATA.push(parseLine(data));
+        }));
+}
+
+/**
+ * Searches data for name
+ * @param {string} name - name to search for, can be a partial name
+ * @returns {array} - results of dictonary values matching search
+ */
+function search(name) {
+    var results = [];
+    name = name.toLowerCase();
+    _.each(DATA, function(data) {
+        if(_.startsWith(data.name.toLowerCase(), name)) {
+            results.push(data);
+        } else {
+            var matched = false; // only do it once
+            _.each(data.alt_names, function(alt) {
+                if(!matched && _.startsWith(alt.toLowerCase(), name)) {
+                    results.push(data);
+                    matched = true;
+                }
+            });
+        }
+    });
+    return results;
+}
+
+// module exports
+module.exports = {
+    readData: readData,
+    search: search
+};
