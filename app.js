321f5008a41525687e1615c58d6b9b9c0bc63800
Kelvin Hammond <hammond.kelvin@gmail.com>
2015-08-12 11:43:20 -0400

    Added scoring and sorting

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ data.readData(input_filepath).on("end", function() {
 });
 
 /**
+ * Calculates the distance between two points
+ * @param {decimal} lat1 - latitude of point one
+ * @param {decimal} lon1 - longitude of point one
+ * @param {decimal} lat2 - latitude of point two
+ * @param {decimal} lon2 - longitude of point two
+ * @source http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+ */
+function distance(lat1, lon1, lat2, lon2) {
+    var R = 6371;
+    var a = 
+        0.5 - Math.cos((lat2 - lat1) * Math.PI / 180)/2 + 
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
+        (1 - Math.cos((lon2 - lon1) * Math.PI / 180))/2;
+    return R * 2 * Math.asin(Math.sqrt(a));
+}
+
+/**
  * Gets the suggestions from the database and returns an array of dictionaries with scores
  * @param {Object} params - query params
  */
@@ -22,8 +39,50 @@ function getSuggestions(params) {
         return [];
     }
 
+    var point = null;
+    if(!_.isUndefined(params.latitude) && !_.isUndefined(params.longitude)) {
+        var lat = parseFloat(params.latitude),
+            lon = parseFloat(params.longitude);
+        if(_.isNumber(lat) && _.isNumber(lon)) {
+            point = {
+                latitude: params.latitude,
+                longitude: params.longitude
+            };
+        }
+    }
+
     var results = data.search(q);
-    return results;
+    var max = 0;
+    if(!_.isNull(point)) {
+        _.each(results, function(res) {
+            res.distance = distance(
+                point.latitude, point.longitude,
+                res.latitude, res.longitude);
+            if(res.distance > max) {
+                max = res.distance;
+            }
+        });
+    }
+
+    results = _.map(results, function(res) {
+        var ret = {
+            name: res.name + ', ' + res.country,
+            latitude: res.latitude,
+            longitude: res.longitude,
+        };
+        if(max > 0 && _.has(res, 'distance')) {
+            ret.score = res.distance / max;
+        }
+        return ret;
+    });
+
+    return _.sortBy(results, function(res) {
+        // sort by score otehrwise by name
+        if(_.has(res, 'score')) {
+            return -res.score;
+        }
+        return res.name;
+    });
 }
 
 module.exports = http.createServer(function (req, res) {
