acda260e50fb349e2cbc9d177bed011f6cbfe33f
Valérian Galliat <val@codejam.info>
2015-08-13 20:05:30 +0200

    Enable strict mode

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var http = require('http');
 var port = process.env.PORT || 2345;
 
--- test/suggestions.js
+++ test/suggestions.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var expect  = require('chai').expect;
 var app     = require('../app');
 var request = require('supertest')(app);
