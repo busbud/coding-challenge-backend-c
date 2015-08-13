f3115fa33c0d7dc44c039be5eff2e309c19e3895
Eric Mc Sween <eric.mcsween@gmail.com>
2015-08-13 16:48:51 -0400

    Implementation of the /suggestions endpoint

--- app.js
+++ app.js
@@ -1,16 +1,95 @@
+var CitiesDb = require('./cities-db');
 var http = require('http');
+var path = require('path');
+var url = require('url');
+
+var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || 2345;
+var distanceScoreWeight = Number(process.env.DISTANCE_SCORE_WEIGHT) || 0.5;
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+// Create the cities DB
+var citiesDb = new CitiesDb({
+  distanceScoreWeight: distanceScoreWeight
+});
 
+// Create the HTTP server
+var server = http.createServer(function (req, res) {
+
+  // Endpoint: GET /suggestions
   if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+    // Validate method
+    if (req.method !== 'GET') {
+      sendJson(res, 405, {error: 'Method not allowed: ' + req.method});
+      return;
+    }
+
+    // Validate parameters
+    var params = url.parse(req.url, true).query;
+    var q = '';
+    var longitude = null;
+    var latitude = null;
+    if (params.q) {
+      q = params.q;
+      if (typeof q !== 'string') {
+        sendJson(res, 400, {error: 'Malformed parameter: q'});
+        return;
+      }
+    }
+    if (params.latitude) {
+      latitude = Number(params.latitude);
+      if (isNaN(latitude)) {
+        sendJson(res, 400, {error: 'Malformed parameter: latitude'});
+        return;
+      }
+    }
+    if (params.longitude) {
+      longitude = Number(params.longitude);
+      if (isNaN(longitude)) {
+        sendJson(res, 400, {error: 'Malformed parameter: longitude'});
+        return;
+      }
+    }
+
+    // Generate response
+    var suggestions = citiesDb.getSuggestions(params.q, latitude, longitude);
+    var status = suggestions.length > 0 ? 200 : 404;
+    sendJson(res, status, {suggestions: suggestions});
+  }
+
+  // Fallback: 404
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('404 Not Found');
+  }
+});
+
+module.exports = server;
+
+// Populate the DB then start listening for requests
+var here = path.dirname(module.filename);
+citiesDb.readFile(
+  path.join(here, 'data/cities_canada-usa.tsv'),
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      server.listen(port, host);
+      console.log('Server running at http://%s:%d/suggestions', host, port);
+    }
+  }
+);
+
+
+/*
+ * Build a JSON response
+ */
+function sendJson(res, status, obj) {
+  var json = JSON.stringify(obj);
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Content-Length', json.length);
+  res.statusCode = status;
+  res.end(json);
+}
--- test/suggestions.js
+++ test/suggestions.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var app     = require('../app');
 var request = require('supertest')(app);
 
-describe('GET /suggestions', function() {
+describe('GET /suggestions', function () {
   describe('with a non-existent city', function () {
     var response;
 
@@ -51,9 +51,9 @@ describe('GET /suggestions', function() {
     it('contains a match', function () {
       expect(response.json.suggestions).to.satisfy(function (suggestions) {
         return suggestions.some(function (suggestion) {
-          return suggestion.name.test(/montreal/i);
+          return suggestion.name.match(/montr[eé]al/i);
         });
-      })
+      });
     });
 
     it('contains latitudes and longitudes', function () {
@@ -61,7 +61,7 @@ describe('GET /suggestions', function() {
         return suggestions.every(function (suggestion) {
           return suggestion.latitude && suggestion.longitude;
         });
-      })
+      });
     });
 
     it('contains scores', function () {
@@ -69,7 +69,69 @@ describe('GET /suggestions', function() {
         return suggestions.every(function (suggestion) {
           return suggestion.latitude && suggestion.longitude;
         });
-      })
+      });
     });
+  });
+
+  describe('with an invalid longitude', function () {
+    var response;
+
+    before(function (done) {
+      request.get('/suggestions?longitude=abc').end(function (err, res) {
+        response = res;
+        done(err);
+      });
+    });
+
+    it('returns a 400', function () {
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+
+  describe('with an invalid latitude', function () {
+    var response;
+
+    before(function (done) {
+      request.get('/suggestions?latitude=abc').end(function (err, res) {
+        response = res;
+        done(err);
+      });
+    });
+
+    it('returns a 400', function () {
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+
+  describe('with a city that appears twice in the DB', function () {
+    var response;
+
+    before(function (done) {
+      request.get('/suggestions?q=monticello+ky').end(function (err, res) {
+        response = res;
+        response.json = JSON.parse(res.text);
+        done(err);
+      });
+    });
+
+    it('returns only one instance', function () {
+      expect(response.json.suggestions).to.have.length(1);
+    });
+  });
+
+});
+
+describe('POST /suggestions', function () {
+  var response;
+
+  before(function (done) {
+    request.post('/suggestions').end(function (err, res) {
+      response = res;
+      done(err);
+    });
+  });
+
+  it('returns a 405', function () {
+    expect(response.statusCode).to.equal(405);
   });
 });
