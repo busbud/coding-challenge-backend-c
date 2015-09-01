6808741a85dd9d42823aed0b13f0063ecd145d97
Nicolas Chaulet <n.chaulet@gmail.com>
2015-09-01 21:44:14 +0200

    Enable Circle CI

--- app/elastic.js
+++ app/elastic.js
@@ -1,6 +1,9 @@
 import {Client} from 'elasticsearch';
 
-const host = process.env.SEARCHBOX_SSL_URL || ('http://' + process.env.ELASTIC_PORT_9200_TCP_ADDR + ':' + process.env.ELASTIC_PORT_9200_TCP_PORT);
+const host = process.env.SEARCHBOX_SSL_URL || (
+  process.env.ELASTIC_PORT_9200_TCP_ADDR && process.env.ELASTIC_PORT_9200_TCP_PORT ?
+  'http://' + process.env.ELASTIC_PORT_9200_TCP_ADDR + ':' + process.env.ELASTIC_PORT_9200_TCP_PORT : null)
+  || 'http://localhost:9200';
 
 const client = new Client({
   host: host
