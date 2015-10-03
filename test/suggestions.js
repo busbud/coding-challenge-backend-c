bd360b4cdf01c7cc26665a856b97fb67c8606cc1
Keegan Parker <KeeganDParker@gmail.com>
2015-10-02 20:08:35 -0400

    Make case-insensitive, update readme, tests

--- test/suggestions.js
+++ test/suggestions.js
@@ -119,4 +119,157 @@ describe('GET /suggestions', function() {
       })
     });
   });
+
+  describe('with a valid city (lowercase)', function () {
+    var response;
+
+    before(function (done) {
+      request
+        .get('/suggestions?q=london')
+        .end(function (err, res) {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
+        });
+    });
+
+    it('returns a 200', function () {
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('returns an array of suggestions', function () {
+      expect(response.json.suggestions).to.be.instanceof(Array);
+      expect(response.json.suggestions).to.have.length.above(0);
+    });
+
+    it('contains a match', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.some(function (suggestion) {
+          return suggestion.name.match(/london/i);
+        });
+      })
+    });
+
+    it('contains latitudes and longitudes', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.every(function (suggestion) {
+          return suggestion.latitude && suggestion.longitude;
+        });
+      })
+    });
+
+    it('contains scores', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.every(function (suggestion) {
+          return suggestion.score;
+        });
+      })
+    });
+  });
+
+  describe('ordering without long and lat', function () {
+    var response;
+
+    before(function (done) {
+      request
+        .get('/suggestions?q=london')
+        .end(function (err, res) {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
+        });
+    });
+
+    it('returns a 200', function () {
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('returns an array of suggestions', function () {
+      expect(response.json.suggestions).to.be.instanceof(Array);
+      expect(response.json.suggestions).to.have.length.above(0);
+    });
+
+    it('contains a match', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.some(function (suggestion) {
+          return suggestion.name.match(/london/i);
+        });
+      })
+    });
+
+    it('contains latitudes and longitudes', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.every(function (suggestion) {
+          return suggestion.latitude && suggestion.longitude;
+        });
+      })
+    });
+
+    it('contains scores', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.every(function (suggestion) {
+          return suggestion.score;
+        });
+      })
+    });
+
+    it('first is in KY', function() {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.length && suggestions[0].name.match(/KY/);
+      });
+    });
+  });
+
+  describe('ordering with long and lat', function () {
+    var response;
+
+    before(function (done) {
+      request
+        .get('/suggestions?q=londo&latitude=43.70011&longitude=-79.4163')
+        .end(function (err, res) {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
+        });
+    });
+
+    it('returns a 200', function () {
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('returns an array of suggestions', function () {
+      expect(response.json.suggestions).to.be.instanceof(Array);
+      expect(response.json.suggestions).to.have.length.above(0);
+    });
+
+    it('contains a match', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.some(function (suggestion) {
+          return suggestion.name.match(/london/i);
+        });
+      })
+    });
+
+    it('contains latitudes and longitudes', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.every(function (suggestion) {
+          return suggestion.latitude && suggestion.longitude;
+        });
+      })
+    });
+
+    it('contains scores', function () {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.every(function (suggestion) {
+          return suggestion.score;
+        });
+      })
+    });
+
+    it('first is in ON', function() {
+      expect(response.json.suggestions).to.satisfy(function (suggestions) {
+        return suggestions.length && suggestions[0].name.match(/ON/);
+      });
+    });
+  });
 });
