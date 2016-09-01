b4ab889a2a65978a28626a485b32209c4db1c52b
Andrey Kamozin <andreykamozin@gmail.com>
2016-09-01 17:30:05 -0400

    Implemented loading the cities list.
    Using the csv-parser package (https://github.com/wdavidw/node-csv-parse) I load and parse the TSV list of the cities on the script load. I made it syncronous to make sure that the list is available _before_ the server is initialized (and they're kept in the memory).
    
    It might be not the fastest and/or best way, but will work for now.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var http = require('http');
+var url = require('url');
+var parse = require('csv-parse/lib/sync'); // we're gonna use syncronous method... it's slow, but we need the data before proceding anyway
+var fs = require('fs');
 var port = process.env.PORT || 2345;
+
+let fileContents = fs.readFileSync(__dirname+'/data/cities_canada-usa.tsv');
+var cities = parse(
+  fileContents,
+  {
+    delimiter: "\t",
+    quote: "",
+    columns: true
+  });
 
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
   if (req.url.indexOf('/suggestions') === 0) {
+    let params = url.parse(req.url, true).query
+      , query = params.q
+      , latitude = params.latitude || 0
+      , longitude = params.longitude || 0
+      , results = [];
+
+    // magic will happen here
+
     res.end(JSON.stringify({
       suggestions: []
     }));
