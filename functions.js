9ee642284aea60c7c6fd2fb4de62e92edc6810e1
Andrey Kamozin <andreykamozin@gmail.com>
2016-09-02 01:33:16 -0400

    Calculate score based on the name match
    Created the simple algorithm to calculate the match score based on the name match. If exact match, the score is 100%, if not, calculated based on position and the number of symbols matched. If the position of the search query isn't in the beginning of the matched city, the score is squared => goes down in the list.

---
+++ functions.js
@@ -0,0 +1,71 @@
+module.exports = {
+  getCityNameMatch: getCityNameMatch,
+  getStateSymbol: getStateSymbol,
+  getCountryName: getCountryName,
+  calculateScore: calculateScore
+};
+
+function getCityNameMatch(query, name, alt_names) {
+  let result = "";
+  let names = alt_names.split(',');
+  names.unshift(name);
+
+  if (names.indexOf(query) !== -1) result = query;
+  else {
+    names.forEach((name) => {
+      if (name.indexOf(query) !== -1) result = name;
+    });
+  }
+
+  return result;
+}
+
+function getStateSymbol(city) {
+  if (city.country === "US") return city.admin1;
+  else {
+    let map = {
+      '01': "AB",
+      '02': "BC",
+      '03': "MB",
+      '04': "NB",
+      '05': "NL",
+      '07': "NS",
+      '08': "ON",
+      '09': "PE",
+      '10': "QC",
+      '11': "SK",
+      '12': "YT",
+      '13': "NT"
+    };
+
+    return map[city.admin1];
+  }
+}
+
+function getCountryName(countryCode) {
+  let map = {
+    'CA': "Canada",
+    'US': "USA"
+  };
+
+  return map[countryCode];
+}
+
+function calculateScore(query, latitude, longitude, match) {
+  var score = 0.0;
+  var nameMatch = ""+getCityNameMatch(query, match.name, match.alt_name);
+
+  if (query === nameMatch) return 1.0;
+  else {
+    score = Math.round(query.length / nameMatch.length * 10) / 10;
+    if (nameMatch.indexOf(query) > 0) {
+      score *= score;
+    }
+  }
+
+  if (latitude && longitude) {
+
+  }
+
+  return score;
+}
