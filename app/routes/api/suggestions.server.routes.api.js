018e438b44330c3836e45003d90c409708c49877
Joel Langlois <joel.inf2990@gmail.com>
2017-05-06 20:44:50 -0400

    added memory & db caching

--- app/routes/api/suggestions.server.routes.api.js
+++ app/routes/api/suggestions.server.routes.api.js
@@ -3,21 +3,22 @@
  * suggestions.server.routes.api.js
  * ------------------------------
  */
-var _ = require('lodash');
-var async = require('async');
+var multiCache = require('../../utils/multicache');
 var citiesController = require('../controllers/cities.server.routes.controller');
 
 module.exports.get = function(req, res) {
-	(req.query.latitude && req.query.longitude
-	  ? citiesController.findNearStartsWith(req)
-	  : citiesController.findStartsWith(req))
-	.then(function(suggestions) {
+	var cacheKey = JSON.stringify(req.query);
+	multiCache.wrap(cacheKey => {
+    	return req.query.latitude && req.query.longitude
+		  ? citiesController.findNearStartsWith(req)
+	  	  : citiesController.findStartsWith(req);
+    }).then(function(suggestions) {
 		if (!suggestions || suggestions.length < 1) {
 			return res.apiError("NotFound", new Error('No matching results'), null, 404);
 		}
 		return res.apiSuccess(suggestions);
 	}).catch(function(e) {
-		//if (process.env.NODE_ENV === 'development')
-    		return res.apiError("Oops! Something went wrong!", e, null, 500);
+		return res.apiError("Oops! Something went wrong!", e, null, 500);
 	});
+
 };
