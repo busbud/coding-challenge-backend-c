4a2bfdb609b77b01dc80796f471a34aa3b26c785
Joel Langlois <joel.inf2990@gmail.com>
2017-05-06 22:11:21 -0400

    heroku patch

--- config/env.js
+++ config/env.js
@@ -1,6 +1,5 @@
 'use strict';
 
 process.env.NODE_ENV = 'development';
-process.env.PORT = 2345;
-process.env.MONGOLAB_URI = 'REPLACE_WITH_YOUR_URI'
-process.env.MONGOLAB_CACHE_URI = 'REPLACE_WITH_YOUR_URI'
+process.env.MONGOLAB_URI = 'REPLACE_WITH_URI'
+process.env.MONGOLAB_CACHE_URI = 'REPLACE_WITH_CACHE_URI'
