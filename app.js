2813c82a58ec1ff9c6e2169dbc6a5fa17f77269c
Camila Rondinini <crondinini@gmail.com>
2017-09-13 12:42:53 -0700

    Removed duplicated app listening to the port

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var app = express();
 var removeAccents = require("remove-accents");
 var outputSuggestion = require("./output-suggestion.js");
 // var outputSuggestion = require("./convert-data.js");
+var cache = require('express-redis-cache')({
+  host: 'redis-18604.c10.us-east-1-4.ec2.cloud.redislabs.com', 
+  port: 18604,
+  auth_pass: 'B4NyN8D0kMWM9rlc',
+});
+
 
 var jsonQuery = require('json-query')
 var data = require('./data/cities_canada-usa.json');
 
 var port = process.env.PORT || 2345;
 
-app.get('/suggestions', function(req, res){
+app.get('/suggestions',  cache.route(), function(req, res){
   var city = req.query.q !== undefined ? removeAccents(req.query.q) : "";
   var longitude = req.query.longitude ? req.query.longitude : null;
   var latitude = req.query.latitude ? req.query.latitude : null;
@@ -49,8 +55,6 @@ app.get('/suggestions', function(req, res){
 app.get('*', function(req, res){
   res.status(404).json({})
 });
-
-app.listen(port);
 
 module.exports = http.createServer(app).listen(port, '127.0.0.1');
 
