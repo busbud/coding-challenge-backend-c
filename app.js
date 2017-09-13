4e194274b8be3d9b31e096e001ff62195706bec5
Camila Rondinini <crondinini@gmail.com>
2017-09-13 00:30:47 -0700

    Ignore accent on query
    To ensure the query would yield correct result regardless of accents, remove-accents module is used to substitute accents for their corresponding ASCII character.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var express = require('express');
-var request = require('request');
 var app = express();
+var removeAccents = require("remove-accents");
 var outputSuggestion = require("./output-suggestion.js");
 // var outputSuggestion = require("./convert-data.js");
 
@@ -11,19 +11,19 @@ var data = require('./data/cities_canada-usa.json');
 var port = process.env.PORT || 2345;
 
 app.get('/suggestions', function(req, res){
-  var country = req.query.q;
+  var city = removeAccents(req.query.q);
   var longitude = req.query.longitude ? req.query.longitude : null;
   var latitude = req.query.latitude ? req.query.latitude : null;
   var suggestions = [];
-  if( country ){
-    var result = jsonQuery('cities[*asciiname~/^'+country+'/i]', {
+  if( city ){
+    var result = jsonQuery('cities[*asciiname~/^' + city + '/i]', {
       data: data,
       allowRegexp: true
     }).value;
     console.log(result );
     res.status(200)
     var searchResults = result;
-    var suggestionResults = outputSuggestion(searchResults, longitude, latitude, country);
+    var suggestionResults = outputSuggestion(searchResults, longitude, latitude, city);
     res.json( {
       suggestions: suggestionResults
     } );
