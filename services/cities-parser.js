9ce2af0be83fbd175bf2376f7c442c9dce2f9d92
Thomas Bousquet <thomas.bousquet@gmail.com>
2017-09-30 11:45:21 -0400

    added missing folder

---
+++ services/cities-parser.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var es = require('event-stream');
+
+var getData = (fileName) => {
+
+    // Return promise, async handling so we have the final data
+    return new Promise((resolve, reject) => {
+  
+        let data = [];
+
+        // Read TSV file
+        fs.createReadStream(`data/${fileName}.tsv`)
+            // Split Strings
+            .pipe(es.split("\n"))
+            // Split Strings into Array
+            .pipe(es.mapSync(line => {
+
+                // Get rid of non-sense / corrupt data
+                if(line.length >= 1) {
+                    // Format line in a array
+                    line =  line.split("\t");
+                    // Get needed fields
+                    let name = line[1];
+                    // let latitude = parseFloat(line[4]);
+                    // let longitude = parseFloat(line[5]);
+                    // Create object line by line
+                    // line = { name, latitude, longitude };
+                    line = { name };
+                    data.push(line);
+                }
+
+            }))
+            .on('close', () => {
+                resolve(data);
+            });
+    });
+  }
+
+module.exports = getData;
