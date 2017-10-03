33964a8a58941ea78f1677131eae7bd7944c9ad8
Thomas Bousquet <thomas.bousquet@gmail.com>
2017-10-03 16:07:17 -0400

    changed env var name

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const http = require('http');
-const port = process.env.PORT || 2345;
-const suggestionsController = require('./controllers/suggestions');
+    port = process.env.PORT || 2345,
+    suggestionsController = require('./controllers/suggestions'),
+    toobusy = require('toobusy-js');
 mongo = require('./mongo');
 mongo.connect();
 
 module.exports = http.createServer(function (req, res) {
+
+    // Settings with a small library for high traffic volume
+    toobusy.maxLag(10);
+    toobusy.interval(250);
+    toobusy.onLag((currentLag) => {
+        res.statusCode = 503;
+        return res.end(JSON.stringify({
+            suggestions: []
+        }));
+      });
+    
 
     if (req.url.indexOf('/suggestions') === 0) {
         return suggestionsController.handleRequest(req, res);
