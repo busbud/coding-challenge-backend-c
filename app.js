48deae71e11ce6c5b49a53192bcab02223c6b548
Paul Chobert <paul@chobert.fr>
2017-10-07 09:23:57 -0400

    Setup eslint and prettier

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const http = require('http');
+const suggestionsHanlder = require('./handlers/suggestions');
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+const port = process.env.PORT || 2345;
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
+module.exports = http
+  .createServer((req, res) => {
+    if (req.url.indexOf('/suggestions') === 0) {
+      const { status = 404, headers = {}, body = {} } = suggestionsHanlder(req);
+
+      res.writeHead(status, headers);
+      res.end(JSON.stringify(body));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end();
+    }
+  })
+  .listen(port, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
