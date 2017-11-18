a87328223c2b8d478f7a117d489df9c75da29568
Programmer In Paradise <programmer.in.paradise@gmail.com>
2017-11-18 01:12:29 -0500

    Fix module export problem revealed by unit test

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ var http = require('http');
 import loadData from './data/data-loader';
 var port = process.env.PORT || 2345;
 
+var server = http.createServer(function (req, res) {
+	res.writeHead(404, {'Content-Type': 'text/plain'});
+
+	if (req.url.indexOf('/suggestions') === 0) {
+		res.end(JSON.stringify({
+			suggestions: []
+		}));
+	} else {
+		res.end();
+	}
+});
+
 loadData().then(() => {
   console.log('Finished loading data.')
-  module.exports = http.createServer(function (req, res) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-
-    if (req.url.indexOf('/suggestions') === 0) {
-      res.end(JSON.stringify({
-        suggestions: []
-      }));
-    } else {
-      res.end();
-    }
-  }).listen(port, '127.0.0.1');
-
+  server.listen(port, '127.0.0.1');
   console.log('Server running at http://127.0.0.1:%d/suggestions', port);
 }).catch((err) => {
   console.error(err);
   process.exit(1);
 });
+
+module.exports = server;
