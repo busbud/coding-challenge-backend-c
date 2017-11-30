20b11ffc836d570b4f804eede3ddfce69e8590a9
jrichardeau <jeremie.richardeau@atinternet.com>
2017-11-30 13:54:16 +0100

    remove nested code

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ const cityRequester = require("./modules/city-requester");
 
 module.exports = http.createServer(function(req, res) {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    if (req.url.indexOf('/suggestions') === 0) {
-        const requestQuery = querystring.parse(url.parse(req.url).query);
-        const suggestionsQuery = {
-            destination: requestQuery.q,
-            latitude: requestQuery.latitude,
-            longitude: requestQuery.longitude
-        };
-        cityRequester.getSuggestions(suggestionsQuery)
-            .then(suggestions => {
-                res.statusCode = 200;
-                res.end(JSON.stringify({suggestions: suggestions}));
-            })
-            .catch(err => {
-                res.statusCode = err.status || 500;
-                res.end(JSON.stringify({'error': err.error || err.message}));
-            });
-    } else {
+    if (req.url.indexOf('/suggestions') !== 0) {
         res.statusCode = 404;
-        res.end(JSON.stringify({'error': 'not implemented'}));
+        res.end(JSON.stringify({'error': 'route not implemented'}));
+        return;
     }
+    const requestQuery = querystring.parse(url.parse(req.url).query);
+    const suggestionsQuery = {
+        destination: requestQuery.q,
+        latitude: requestQuery.latitude,
+        longitude: requestQuery.longitude
+    };
+    cityRequester.getSuggestions(suggestionsQuery)
+        .then(suggestions => {
+            res.statusCode = 200;
+            res.end(JSON.stringify({suggestions: suggestions}));
+        })
+        .catch(err => {
+            res.statusCode = err.status || 500;
+            res.end(JSON.stringify({'error': err.error || err.message}));
+        });
+
 }).listen(port, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
