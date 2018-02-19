51780dfb5ff1fdb7b636ab843a6c6393f00447f5
Nora Top <nora@sweetiq.com>
2018-02-19 21:13:49 +0000

    code rearrangement

--- models/Cities.js
+++ models/Cities.js
@@ -3,10 +3,19 @@ const _ = require('lodash');
 const { normalize } = require('../utility/normalizer');
 
 class Cities {
-  static getSuggestions(esClient, opts) {
-    const query = {
-      'size': opts.limit,
-      'query': {
+  static formatHitToCity(hit) {
+    return {
+      name: `${hit._source.name}, ${hit._source.country}`,
+      latitude: hit._source.location.lat,
+      longitude: hit._source.location.lon,
+      score: hit._score
+    }
+  }
+
+  static buildSearchQuery(opts) {
+    let termQuery = {};
+    if (!opts.fuzzy) {
+      termQuery = {
         'bool': {
           'should': [
             {
@@ -24,26 +33,9 @@ class Cities {
             }
           ]
         }
-      }
-    };
-
-    return Promise.resolve(esClient.search({
-      index: 'cities',
-      body: query
-    })).then(result => result.hits.hits).then(hits => {
-      return _.map(hits, hit => ({
-        name: hit._source.name,
-        latitude: hit._source.location.lat,
-        longitude: hit._source.location.lon,
-        score: hit._score
-      }));
-    });
-  }
-
-  static getFuzzySuggestions(esClient, opts) {
-    const query = {
-      'size': opts.limit,
-      'query': {
+      };
+    } else {
+      termQuery = {
         'match': {
           'rawName': {
             'query': opts.term,
@@ -51,19 +43,20 @@ class Cities {
             'fuzziness': 'AUTO'
           }
         }
-      }
+      };
+    }
+    return {
+      'size': opts.limit,
+      'query': termQuery
     };
+  }
 
+  static getSuggestions(esClient, opts) {
     return Promise.resolve(esClient.search({
       index: 'cities',
-      body: query
+      body: Cities.buildSearchQuery(opts)
     })).then(result => result.hits.hits).then(hits => {
-      return _.map(hits, hit => ({
-        name: `${hit._source.name}, ${hit._source.country}`,
-        latitude: hit._source.location.lat,
-        longitude: hit._source.location.lon,
-        score: hit._score
-      }));
+      return _.map(hits, Cities.formatHitToCity);
     });
   }
 }
