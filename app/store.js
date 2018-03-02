9e6b48c1eb40ad68b714510c5c38716104c2e2ab
Wole Obayomi <oluwole.obayomi@mail.mcgill.ca>
2018-03-02 18:14:41 +0000

    adding support for case insensitivity

--- app/store.js
+++ app/store.js
@@ -38,7 +38,7 @@ Store.prototype.query = function(query) {
     //to the city's name
     var storedCitiesStartingWithLetter = this.store[firstLetterOfTerm] ? this.store[firstLetterOfTerm].data : (this.store[firstLetterOfTerm] = this.fetch(firstLetterOfTerm).data);
     var citiesStartingWithQuery = storedCitiesStartingWithLetter.filter(function(city) {
-        var capitalized = query.term[0].toUpperCase().concat(query.term.slice(1)));
+        var capitalized = query.term[0].toUpperCase().concat(query.term.slice(1));
         if (city.ascii.match(capitalized)) {
             return city;
         }
