d5f198f173bc5187f916a66e5da3fed4637ebf8f
Wole Obayomi <oluwole.obayomi@mail.mcgill.ca>
2018-03-02 17:18:09 +0000

    changed server to express. added middleware to restrict access to only suggestions and /

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var http = require("http");
 var port = process.env.PORT || 2345;
 var host = process.env.IP;
 var fs = require("fs");
 var path = require("path");
 var pathExists = require("path-exists");
-var url = require("url");
+var express = require("express");
+var app = express();
 
 var outputPath = path.resolve(__dirname + "/data/cities_canada-usa.json");
 var inputPath = path.resolve(__dirname + "/data/cities_canada-usa.tsv");
@@ -12,6 +12,9 @@ var inputPath = path.resolve(__dirname + "/data/cities_canada-usa.tsv");
 
 var tsv2JSON = require("./app/tsv2json");
 var store = require("./app/store");
+
+var defaultLat = "45.5017";
+var defaultLn = "-73.58781";
 
 //convert the tsv file to json
 pathExists(outputPath).then(function(exists) {
@@ -23,8 +26,8 @@ pathExists(outputPath).then(function(exists) {
           throw new Error("Could not read data file");
         }
         store.init(JSON.parse(cities));
-      })
-    })
+      });
+    });
   }
   //the file exists, so only need to initialize the store
   else {
@@ -33,47 +36,57 @@ pathExists(outputPath).then(function(exists) {
         throw new Error("Could not read data file");
       }
       store.init(JSON.parse(cities));
-    })
+    });
   }
 });
 
-module.exports = http.createServer(function(req, res) {
-  var defaultLat = "45.5017";
-  var defaultLn  = "-73.58781";
-  
-  if (req.url.indexOf('/suggestions') === 0) {
-    //parse query string
-    var query = url.parse(req.url, true).query;
-    
-    //if no query parameter provided, send 404
-    if (!query.q) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end();
-    }
-    //query the store for data. Default geo info is for Montreal
-    var suggestions = store.query({
-      term: query.q,
-      longitude: (query.longitude ? query.longitude : defaultLn),
-      latitude: (query.latitude ? query.latitude : defaultLat)
-    });
-    
-    if (suggestions.length == 0) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end(JSON.stringify({
-        suggestions: []
-      }));
-    }
-    else {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(JSON.stringify({
-        suggestions: suggestions
-      }));
-    }
+app.use(function restrictAccess(req, res, next) {
+  if (req.url.match("suggestions")) {
+    next();
   }
   else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end();
   }
-}).listen(port, host);
+});
 
+app.get("/suggestions", function(req, res) {
+  var query = req.query;
+  //if no query parameter provided, send 404
+  if (!query.q) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end();
+  }
+  //query the store for data. Default geo info is for Montreal
+  var suggestions = store.query({
+    term: query.q,
+    longitude: (query.longitude ? query.longitude : defaultLn),
+    latitude: (query.latitude ? query.latitude : defaultLat)
+  });
+
+  //if no data returned from store, send a 404 with
+  //an empty suggestion array
+  if (suggestions.length == 0) {
+
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end(JSON.stringify({
+      suggestions: []
+    }));
+
+  }
+  else {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(JSON.stringify({
+      suggestions: suggestions
+    }));
+
+  }
+});
+
+
+app.listen(port);
+
+module.exports = app;
 
 console.log('Server running at http://%s:%d/suggestions', host, port);
