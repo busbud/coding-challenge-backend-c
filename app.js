0ac2931c81cfe16951428f92800fa1bc5373aa98
Benoit Koenig <koenigbenoit47@gmail.com>
2018-05-13 20:39:04 +0100

    Fixing: heroku couldnt bind to $PORT within 60 seconds

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ lib.updateDatabase();
 var http = require('http');
 var port = process.env.PORT || 2345;
 
-module.exports = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   if (req.url.indexOf('/suggestions') === 0) {
     const params = url.parse(req.url, true).query;
     res.writeHead(200, {'Content-Type': 'application/json'});
@@ -24,6 +24,6 @@ module.exports = http.createServer(function (req, res) {
     res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end();
   }
-}).listen(port, '127.0.0.1');
+}).listen(port);
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
