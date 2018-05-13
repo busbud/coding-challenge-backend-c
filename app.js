4807147636209a2cd43df67c433c7d65162d9201
Benoit Koenig <koenigbenoit47@gmail.com>
2018-05-13 21:18:06 +0100

    Reset the module.exports in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ lib.updateDatabase();
 var http = require('http');
 var port = process.env.PORT || 2345;
 
-http.createServer(function (req, res) {
+module.exports = http.createServer(function (req, res) {
   if (req.url.indexOf('/suggestions') === 0) {
     const params = url.parse(req.url, true).query;
     res.writeHead(200, {'Content-Type': 'application/json'});
