9b660d6be71988f63df57f340aa344598a5f0925
Kevin Delemme <kevin.delemme@transferwise.com>
2018-06-28 18:13:32 -0400

    Add final scoring

--- src/infrastructure/score.js
+++ src/infrastructure/score.js
@@ -1,6 +1,8 @@
 const score = require("string-score");
 
 const METERS_IN_KM = 1000;
+const WEIGHT_SCORING_DISTANCE = 0.7;
+const WEIGHT_SCORING_NAME = 0.3;
 
 module.exports = {
   scoreName: (text, pattern) => score(text, pattern, 0.8),
@@ -11,5 +13,10 @@ module.exports = {
 
     let distanceInKm = distanceInMeters / METERS_IN_KM;
     return 1 - distanceInKm / radiusInKm;
+  },
+  scoring: ({ scoringName = 0, scoringDistance = null } = {}) => {
+    return scoringDistance
+      ? WEIGHT_SCORING_DISTANCE * scoringDistance + WEIGHT_SCORING_NAME * scoringName
+      : scoringName;
   }
 };
--- test/infrastructure/score.js
+++ test/infrastructure/score.js
@@ -1,5 +1,5 @@
 const expect = require("chai").expect;
-const { scoreName, scoreDistance } = require("../../src/infrastructure/score");
+const { scoreName, scoreDistance, scoring } = require("../../src/infrastructure/score");
 
 describe("Score", () => {
   describe("scoreName", () => {
@@ -32,6 +32,24 @@ describe("Score", () => {
       expect(scoreDistance(47 * METERS_IN_KM, 100))
         .to.be.above(0)
         .and.below(1);
+    });
+  });
+
+  describe("scoring", () => {
+    it("should give a score of 1 if both score are 1", () => {
+      expect(scoring({ scoringName: 1, scoringDistance: 1 })).to.eq(1);
+    });
+
+    it("should give a score of 0 if both score are 0", () => {
+      expect(scoring({ scoringName: 0, scoringDistance: 0 })).to.eq(0);
+    });
+
+    it("should use scoringName with full weight if scoringDistance is not present", () => {
+      expect(scoring({ scoringName: 0.567 })).to.eq(0.567);
+    });
+
+    it("should give more weight to the distance than name", () => {
+      expect(scoring({ scoringName: 0, scoringDistance: 1 })).to.eq(0.7);
     });
   });
 });
