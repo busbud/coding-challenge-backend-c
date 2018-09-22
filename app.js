c15f9b51a9335cbe88748800601d45e70aa3616a
Alexandre Couret <alex.couret18@gmail.com>
2018-09-22 10:56:24 -0400

    [ADD] Send 200 when accessing root API, can be used for health checks

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const {
 app.listen(PORT, () => {
     console.log(`Server running at http://127.0.0.1:${PORT}/suggestions`);
 });
+app.get('/', (req, res, next) => res.json('ok'));
 app.use(routes);
 
 module.exports = app;
