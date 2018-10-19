c16e95dd84e461ba7842ba7c778ecf7dba2230d6
lleki <linhlekim@gmail.com>
2018-10-19 15:13:22 -0400

    added distance function

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const url = require('url');
 const querystring = require('querystring');
 // var suggestionsRouter = require('./api/suggestions');
 
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+          c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
 function isNorthAmerica(item) {
   if (item.country == 'US' ||  item.country == 'CA'){
     return true;
@@ -24,18 +33,21 @@ function findCity(data, search) {
   let list = [];
    data.forEach(function(city) {
     let cityName = (city.name).toLowerCase();
-
-    if(cityName.startsWith(search)){
+    let distance = calculateDistance(city.latitude, city.longitude, search.latitude, search.longitude)
+    if(cityName.startsWith(search.name)){
         list.push({
             name: city.name,
             area: city.area,
             country: city.country,
             latitude: city.latitude,
-            longitude: city.longitude})
+            longitude: city.longitude,
+            distance: distance})
     }
   })
    return list;
 }
+
+
 // app.use('/api/suggestions', suggestionsRouter);
 app.get('/suggestions', function(req, res) {
     if (req.url.indexOf('/suggestions') === 0) {
@@ -65,9 +77,13 @@ app.get('/suggestions', function(req, res) {
         filteredArray = newArr.filter(filterResults)
 
       });
-      let query = req.query.q.toLowerCase();
+      let query = {
+        name: req.query.q.toLowerCase(),
+        latitude: req.query.latitude,
+        longitude: req.query.longitude };
+
       console.log(query);
-      let results = findCity(filteredArray, req.query.q)
+      let results = findCity(filteredArray, query)
 
       let suggestions = {
         suggestions: results
