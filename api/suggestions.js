465910c52bef83829df4c27984e6a2be80a183a5
lleki <linhlekim@gmail.com>
2018-10-19 10:16:35 -0400

    extracted city names only from json file data

--- api/suggestions.js
+++ api/suggestions.js
@@ -1,32 +1,14 @@
 
-var express = require("express");
-var router = express.Router();
-const fs = require('fs');
+// var express = require("express");
+// var router = express.Router();
+// const fs = require('fs');
 
-// app.get('/suggestions', function(req, res) {
 
-//   var q = req.query.q
-//   var latitude = req.param('latitude');
-//   var longitude = req.param('longitude');
 
-//   res.send(q + ' ' + latitude + ' ' + longitude);
+// router.get("/", function(req, res) {
+//   console.log("testing get suggestions end point");
 // });
 
-function parseQuery(queryString) {
-    var query = {};
-    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (var i = 0; i < pairs.length; i++) {
-        var pair = pairs[i].split('=');
-        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-    }
-    return query;
-}
 
 
-router.get("/", function(req, res) {
-  console.log("testing get suggestions end point");
-});
-
-
-
-module.exports = router;
+// module.exports = router;
--- app.js
+++ app.js
@@ -1,45 +1,58 @@
 const http = require('http');
 const port = process.env.PORT || 2345;
+
 const express = require('express');
+
 const app = express();
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
-var suggestionsRouter = require('./api/suggestions');
+// var suggestionsRouter = require('./api/suggestions');
 
-app.use('/api/suggestions', suggestionsRouter);
+// app.use('/api/suggestions', suggestionsRouter);
+app.get('/suggestions', function(req, res) {
+    if (req.url.indexOf('/suggestions') === 0) {
+    res.status(404);
+      }
+    res.setHeader('Content-Type', 'application/json')
+    // res.json({ name: req.query.q,
+    //   latitude: req.query.latitude,
+    //   longitude: req.query.longitude })
+     fs.readFile('./data/cities_canada-us.json', function(err, data){
 
-function parseQuery(queryString) {
-    var query = {};
-    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (var i = 0; i < pairs.length; i++) {
-        var pair = pairs[i].split('=');
-        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-    }
-    console.log("console in parse function", query);
-    return query;
-}
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-type':'text/plan'});
+      let arr = JSON.parse(data);
+      arr.forEach(function(city) {
+        console.log(city.name);
+      });
 
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    // fs.readFile('./data/cities_canada-us.json', function(err, data){
-    //   res.writeHead(200, {'Content-Type': 'text/plain'});
-    //   res.write(data);
-    //   return res.end();
-    // });
-     pathName= url.parse(req.url).pathname;
-     query= url.parse(req.url).query;
-     console.log('pathName1' + pathName);
-     console.log('query2' + query);
-     query = parseQuery(query);
-     res.end(JSON.stringify(query));
+      // res.write(data.toString());
+      return res.end();
+    });
 
-  } else {
-    res.end()
+
+});
+// if no match
+// {
+//   "suggestions": []
+// }
+
+// function parseQuery(queryString) {
+//     var query = {};
+//     var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+//     for (var i = 0; i < pairs.length; i++) {
+//         var pair = pairs[i].split('=');
+//         query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+//     }
+//     console.log("console in parse function", query);
+//     return query;
+// }
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('err', err)
   }
-}).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+  console.log(`Server running at http://127.0.0.1:${port}/suggestions`);
+})
