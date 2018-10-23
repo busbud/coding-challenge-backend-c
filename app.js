8e31aaa5e7ba50cbcb4b676403d95f4aec18f57c
Administrator <moi.ju1@gmail.com>
2018-10-22 23:48:52 -0400

    add overload-protection module

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const path = require('path');
 const fuzzysort = require('fuzzysort');
 const geohash = require('latlon-geohash');
 
+//overload-protection module
+const protectCfg = {
+  production: process.env.NODE_ENV === 'production', // if production is false, detailed error messages are exposed to the client
+  clientRetrySecs: 1, // Client-Retry header, in seconds (0 to disable) [default 1]
+  sampleInterval: 5, // sample rate, milliseconds [default 5]
+  maxEventLoopDelay: 42, // maximum detected delay between event loop ticks [default 42]
+  maxHeapUsedBytes: 0, // maximum heap used threshold (0 to disable) [default 0]
+  maxRssBytes: 0, // maximum rss size threshold (0 to disable) [default 0]
+  errorPropagationMode: false // dictate behavior: take over the response
+                              // or propagate an error to the framework [default false]
+};
+const protect = require('overload-protection')('http', protectCfg);
+
 const port = process.env.PORT || 2345;
 
 //function that tests if latitude and longitude is valid (takes input pre-converted to Number)
@@ -95,6 +108,8 @@ data = null;
 provincesObject = null;
 
 module.exports = http.createServer(function (req, res) {
+  //send 503 if overload
+  if (protect(req, res) === true) return;
   // Parse the url
   const parsedUrl = url.parse(req.url, true);
 
