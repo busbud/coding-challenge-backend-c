547db72726bd18ede0a33634f7d8cfbded2ba349
Timothee Clain <tclain@pyx4.com>
2018-12-03 10:53:50 -0500

    fix bug in index endpoint

--- utils/suggestions/fromIndex.js
+++ utils/suggestions/fromIndex.js
@@ -1,14 +1,17 @@
 const { suggest } = require(".");
+const { sanitizeString } = require("../text");
 
 /** main search function in index */
 function findMatchesInIndex(db, query) {
   if (query === "") return [];
 
+  const sanitizedQuery = sanitizeString(query);
   // try to find
-  const hasDirectMatchId = db.index.matches[query];
+  const hasDirectMatchId = db.index.matches[sanitizedQuery];
   if (hasDirectMatchId) return db.objects[hasDirectMatchId];
   else {
-    const partialMatches = db.index.partials[query];
+    const partialMatches = db.index.partials[sanitizedQuery];
+
     return partialMatches.map(id => db.objects[id]);
   }
 }
