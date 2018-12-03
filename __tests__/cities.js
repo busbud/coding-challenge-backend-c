26bde14c97c4271f6ac9fc1eeb120b5dfa6045c8
Timothée Clain <timothee.clain@gmail.com>
2018-12-02 19:58:02 -0500

    add integration test for cities

--- __tests__/cities.js
+++ __tests__/cities.js
@@ -1,3 +1,23 @@
+jest.mock("line-reader", () => {
+  const lineWalker = data => {
+    return (fpath, cb) => {
+      let index = 0;
+      for (const line of data) {
+        const last = index === data.length - 1;
+        cb(line, last);
+        index++;
+      }
+    };
+  };
+  const exampleTsvData = [
+    "id	name	ascii	alt_name	lat	long	feat_class	feat_code	country	cc2	admin1	admin2	admin3	admin4	population	elevation	dem	tz	modified_at",
+    "5882142	Acton Vale	Acton Vale		45.65007	-72.56582	P	PPL	CA		10	16			5135		90	America/Montreal	2008-04-11",
+    "5889745	Baie-Comeau	Baie-Comeau	Baie-Comeau,Be Komo,Be-Komo,YBC,bay kamyw,bh-kwmw,byh kwmw, kybk,Бе Комо,Бе-Комо,بائ کامیو,به-کومو,بيه كومو، كيبك	49.21679	-68.14894	P	PPL	CA		10	09			29808		76	America/Montreal	2013-08-17"
+  ];
+  return {
+    eachLine: lineWalker(exampleTsvData)
+  };
+});
 const { sanitizeString, indexCities } = require("../utils/cities");
 
 describe("sanitizeString", () => {
@@ -5,5 +25,43 @@ describe("sanitizeString", () => {
     expect(sanitizeString("à is véry    goôd to bî ù")).toEqual(
       "aisverygoodtobiu"
     );
+  });
+});
+
+describe("indexData", () => {
+  it("should transform a tsv file to an array of objects", () => {
+    return indexCities("/path/to").then(data => {
+      expect(data).toEqual({
+        objects: {
+          "5882142": {
+            latitude: "45.65007",
+            longitude: "-72.56582",
+            name: "Acton Vale, QC, CA",
+            onlyName: "Acton Vale"
+          },
+          "5889745": {
+            latitude: "49.21679",
+            longitude: "-68.14894",
+            name: "Baie-Comeau, QC, CA",
+            onlyName: "Baie-Comeau"
+          }
+        },
+        cities: [
+          {
+            latitude: "45.65007",
+            longitude: "-72.56582",
+            name: "Acton Vale, QC, CA",
+            onlyName: "Acton Vale"
+          },
+          {
+            latitude: "49.21679",
+            longitude: "-68.14894",
+            name: "Baie-Comeau, QC, CA",
+            onlyName: "Baie-Comeau"
+          }
+        ],
+        index: {}
+      });
+    });
   });
 });
--- utils/cities.js
+++ utils/cities.js
@@ -1,26 +1,10 @@
 const lineReader = require("line-reader");
 const { set, get, has } = require("lodash");
 const { tsvLineToMap } = require("./tsv");
+const provinceFips = require("../data/provinceFips.json");
 
 // globals vars
 const POPULATION_THRESHOLD = 5000;
-
-// Regexp
-const NAME_WITH_SPACE = /(\d+ )([^\t]+)(\t)/g;
-const SPACE = / /g;
-const WHITE_SPACE = /\s/g;
-
-/** custom line parser to solve inconsistencies in spacing and missing fields in tsv format */
-function reshapeLineWithHeuristics(line) {
-  return line;
-  /*return line
-    .replace(NAME_WITH_SPACE, (match, before, content, after) => {
-      console.log("MATCH", before, content, after);
-      return before + content.replace(/ /g, "_") + after;
-    })
-    .replace(WHITE_SPACE, "\t");
-    */
-}
 
 /** Business-aware utilities to construct the town index */
 /** create object with these keys from a line */
@@ -55,7 +39,7 @@ function sanitizeString(str) {
     .replace(/[àâä]/g, "a")
     .replace(/[ïìî]/g, "i")
     .replace(/[üûù]/g, "u")
-    .replace(/[ôòö]/g, "u");
+    .replace(/[ôòö]/g, "o");
 }
 
 /** is a given city valid in our search */
@@ -83,11 +67,13 @@ function indexCities(filePath) {
       const lineObject = lineMapper(line);
       // the city is big enough
       if (isCityValid(lineObject, POPULATION_THRESHOLD)) {
+        // get the province or state code
+        const stateCode = provinceFips[lineObject.admin1] || lineObject.admin1;
         const object = {
           latitude: lineObject.lat,
           longitude: lineObject.long,
-          name: lineObject.name,
-          fullName: `${lineObject.name}`
+          name: `${lineObject.name}, ${stateCode}, ${lineObject.country}`,
+          onlyName: lineObject.name
         };
         db.objects[lineObject.id] = object;
         db.cities.push(object);
