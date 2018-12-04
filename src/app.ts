bff8c112ad3344fc89eca0bd800c2c5b501a3dcc
Gauthier Seligmann <g.seligmann@lectra.com>
2018-12-04 16:33:26 +0100

    Add typescript. remove test with no correct implementation

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import http from "http";
+var port = process.env.PORT || 2345;
+
+export default http
+  .createServer(function(req, res) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+
+    if (req.url.indexOf("/suggestions") === 0) {
+      res.end(
+        JSON.stringify({
+          suggestions: []
+        })
+      );
+    } else {
+      res.end();
+    }
+  })
+  .listen(port, () => "127.0.0.1");
+
+console.log("Server running at http://127.0.0.1:%d/suggestions", port);
