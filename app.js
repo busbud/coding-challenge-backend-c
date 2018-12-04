7d5962fe26ffe4ca9095e3d7faac2d363995c2a5
lnalex <dev@alexandrelion.com>
2018-12-04 17:19:16 +0100

    Export server

--- app.js
+++ app.js
@@ -46,3 +46,6 @@ app.get('/suggestions', function(req, res) {
 app.listen(port, function () {
   console.log('Server running at http://127.0.0.1:%d/suggestions', port);
 });
+
+
+module.exports = app;
