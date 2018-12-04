938ffcf84df73151bd48c0d6dbd4750268e39207
Alexandre Lion <dev@alexandrelion.com>
2018-12-04 21:36:47 +0100

    Add cors header

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const app = express();
 const search = require('./src/search');
 
+const app = express();
+
 var port = process.env.PORT || 2345;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/suggestions', function(req, res) {
 
