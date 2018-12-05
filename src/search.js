68a06d21067409da601e27bee3ff3cfe53879475
Alexandre Lion <dev@alexandrelion.com>
2018-12-05 10:54:44 +0100

    Fix Canada codes

--- src/search.js
+++ src/search.js
@@ -92,12 +92,14 @@ function search(term, limit, lat, long) {
                 country = "N/A";
         }
 
+        var admin = city.country === "CA" ? getCode(city.admin1) : city.admin1;
+
         var score = distance.get(term.toLowerCase(), name.replace(/\./g, '').replace(/,/g, '').toLowerCase(), lat, long, city.lat, city.long);
 
         // Do not use cities outside CA/US and null score
         if (country !== "N/A" && score >= 0.1) {
             results.push({
-                'name': name + ", " + city.admin1 + ", " + country,
+                'name': name + ", " + admin + ", " + country,
                 'latitude': city.lat,
                 'longitude': city.long,
                 'score': score
@@ -120,4 +122,37 @@ function search(term, limit, lat, long) {
     }
 
     return filteredResults;
+}
+
+function getCode(code) {
+    switch(code) {
+        case "01":
+            return "AB";
+        case "02":
+            return "BC";
+        case "03":
+            return "MB";
+        case "04":
+            return "NB";
+        case "05":
+            return "NL";
+        case "07":
+            return "NS";
+        case "08":
+            return "ON";
+        case "09":
+            return "PE";
+        case "10":
+            return "QC";
+        case "11":
+            return "SK";
+        case "12":
+            return "YT";
+        case "13":
+            return "NT";
+        case "14":
+            return "NU";
+        default:
+            return code;
+    }
 }
