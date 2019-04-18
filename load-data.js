53c29b68c64cebf869fa8f6f9c587294bd1de57f
Code Bling <codeblingx@gmail.com>
2019-04-18 13:48:20 -0400

    Map and filter data before returning it

--- load-data.js
+++ load-data.js
@@ -12,5 +12,18 @@ module.exports = path => new Promise( (resolve, reject) => {
     let rows = d3.tsvParseRows(line, d3.autoType);
     citiesData.push(...rows);
   });
-  pump(citiesDataStream, lineStream, err => err ? reject(err) : resolve(citiesData));
+  pump(citiesDataStream, lineStream, err => {
+    if(!err) {
+      let headerArray = citiesData.shift();
+      let mappedCitesData = citiesData.map(cityValuesArray => {
+        let cityData = {};
+        cityValuesArray.forEach( (value, columnIndex) => cityData[headerArray[columnIndex]] = value);
+        return cityData;
+      });
+      let filteredCitiesData = mappedCitesData.filter(cityData => cityData.population >= 5000);
+      resolve(filteredCitiesData);
+    } else {
+      reject(err)
+    }
+  });
 });
