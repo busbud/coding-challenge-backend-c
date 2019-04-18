6b22d1243decb9416edc929b6dff380e82ff020f
Code Bling <codeblingx@gmail.com>
2019-04-18 13:05:19 -0400

    Add a basic data loader. Data is array-form rows and still needs to be mapped to objects and filtered

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
+const loadData = require('./load-data');
 const port = process.env.PORT || 2345;
 
+loadData('./data/cities_canada-usa.tsv')
+  .then(data => console.log(`${data.length} lines of data. ` + JSON.stringify(data.slice(0,5), ' ')))
+;
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
@@ -15,5 +19,6 @@ module.exports = http.createServer(function (req, res) {
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
 
+//});
 process.on('unhandledRejection', error => {throw error});
 process.on('uncaughtException', error => console.log(error));
---
+++ load-data.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const LineStream = require('byline').LineStream;
+const pump = require('pump');
+const d3 = require('d3-dsv');
+
+module.exports = path => new Promise( (resolve, reject) => {
+  let citiesData = [];
+  const citiesDataStream = fs.createReadStream('./data/cities_canada-usa.tsv', 'utf8');
+  const lineStream = new LineStream();
+
+  lineStream.on('data', line => {
+    let rows = d3.tsvParseRows(line, d3.autoType);
+    citiesData.push(...rows);
+  });
+  pump(citiesDataStream, lineStream, err => err ? reject(err) : resolve(citiesData));
+});
