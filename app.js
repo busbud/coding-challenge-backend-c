415c132d5e4c69a3f451631a573a0a8e15a6e4f8
Code Bling <codeblingx@gmail.com>
2019-04-19 10:56:30 -0400

    Reindent

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ let server;
 module.exports = server = http.createServer();
 loadData('./data/cities_canada-usa.tsv')
   .then(data => {
-server.on('request', function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+    server.on('request', function (req, res) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
+      if (req.url.indexOf('/suggestions') === 0) {
+        res.end(JSON.stringify({
+          suggestions: []
+        }));
+      } else {
+        res.end();
+      }
+    }).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
-
+    console.log('Server running at http://127.0.0.1:%d/suggestions', port);
 });
+
 process.on('unhandledRejection', error => {throw error});
 process.on('uncaughtException', error => console.log(error));
