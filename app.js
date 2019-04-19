9f0ba7fe9b1c5eb0228e9eebfb22741f8c63232a
Code Bling <codeblingx@gmail.com>
2019-04-19 10:57:50 -0400

    Reindent

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ loadData('./data/cities_canada-usa.tsv')
       } else {
         res.end();
       }
-    }).listen(port, '127.0.0.1', () =>
-
-    console.log('Server running at http://127.0.0.1:%d/suggestions', port));
+    }).listen(port, '127.0.0.1', () => console.log('Server running at http://127.0.0.1:%d/suggestions', port));
 });
 
 process.on('unhandledRejection', error => {throw error});
