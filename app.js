60011ef101854c6e172b6a5a3a757d97c8e03a11
Code Bling <codeblingx@gmail.com>
2019-04-19 10:57:37 -0400

    Don't say we're listening until we are

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ loadData('./data/cities_canada-usa.tsv')
       } else {
         res.end();
       }
-    }).listen(port, '127.0.0.1');
+    }).listen(port, '127.0.0.1', () =>
 
-    console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+    console.log('Server running at http://127.0.0.1:%d/suggestions', port));
 });
 
 process.on('unhandledRejection', error => {throw error});
