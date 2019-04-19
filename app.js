32464b2e192f60294a61daf2779762ae3ad25419
Code Bling <codeblingx@gmail.com>
2019-04-19 15:23:04 -0400

    Load city data synchronously

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const http = require('http');
-const loadData = require('./load-data');
+const citiesData = require('./alt-load-data');
 const port = process.env.PORT || 2345;
 
-let server;
-module.exports = server = http.createServer();
-loadData('./data/cities_canada-usa.tsv')
-  .then(data => {
-    server.on('request', function (req, res) {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
+module.exports = http.createServer(function (req, res) {
+  res.writeHead(404, {'Content-Type': 'text/plain'});
 
-      if (req.url.indexOf('/suggestions') === 0) {
-        res.end(JSON.stringify({
-          suggestions: []
-        }));
-      } else {
-        res.end();
-      }
-    }).listen(port, '127.0.0.1', () => console.log('Server running at http://127.0.0.1:%d/suggestions', port));
-});
+  if (req.url.indexOf('/suggestions') === 0) {
+    res.end(JSON.stringify({
+      suggestions: []
+    }));
+  } else {
+    res.end();
+  }
+}).listen(port, '127.0.0.1', () => console.log('Server running at http://127.0.0.1:%d/suggestions', port));
 
 process.on('unhandledRejection', error => {throw error});
 process.on('uncaughtException', error => console.log(error));
+
