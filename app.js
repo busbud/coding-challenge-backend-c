e49e906744fe118d432e23157b8e27a5596ce428
Code Bling <codeblingx@gmail.com>
2019-04-19 16:01:42 -0400

    Use Express instead of vanilla Node HTTP server

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const dataUtils = require('./data-utils');
 const port = process.env.PORT || 2345;
 
 let citiesData = require('./sync-load-data');
 citiesData = dataUtils.makeRegionsReadable(citiesData);
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
-
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1', () => console.log('Server running at http://127.0.0.1:%d/suggestions', port));
+app.get('/suggestions', (req, res) => {
+  const suggestions = [];
+  res.send({
+    suggestions: suggestions
+  });
+});
+app.use((req, res) => res.sendStatus(404));
+app.listen(port, '127.0.0.1', () => console.log('Server running at http://127.0.0.1:%d/suggestions', port));
 
 process.on('unhandledRejection', error => {throw error});
 process.on('uncaughtException', error => console.log(error));
 
+module.exports = app; 
