f6056cd0f6f61d588d635641b2420aefd4727653
Code Bling <codeblingx@gmail.com>
2019-04-19 10:55:59 -0400

    Only start listening once data has been loaded

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const http = require('http');
 const loadData = require('./load-data');
 const port = process.env.PORT || 2345;
 
-loadData('./data/cities_canada-usa.tsv')
-  .then(data => console.log(`${data.length} lines of data. ` + JSON.stringify(data.slice(0,5), null, '  ')))
-;
 let server;
 module.exports = server = http.createServer();
+loadData('./data/cities_canada-usa.tsv')
+  .then(data => {
 server.on('request', function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
@@ -21,6 +20,6 @@ server.on('request', function (req, res) {
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
 
-//});
+});
 process.on('unhandledRejection', error => {throw error});
 process.on('uncaughtException', error => console.log(error));
