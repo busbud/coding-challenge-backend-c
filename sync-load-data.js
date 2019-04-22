d9d83ff12f3f21b37cee17a3ac46d34154f32b5e
Code Bling <codeblingx@gmail.com>
2019-04-22 14:48:32 -0400

    Parse alt_name into an array

--- sync-load-data.js
+++ sync-load-data.js
@@ -4,11 +4,23 @@ const dataUtils = require('./data-utils');
 
 let stringData = fs.readFileSync('./data/cities_canada-usa.tsv', 'utf8');
 stringData = stringData.replace(/["]/g, ''); //Force data to conform to RFC 4180 before parsing it
+
+const citiesData = d3.tsvParse(stringData, d3.autoType);
+citiesData.forEach(cityData => {
+  if(cityData.alt_name != null) {
+    cityData.alt_name = d3.csvParseRows(cityData.alt_name);
+    if(cityData.alt_name.length == 1) {
+      cityData.alt_name = cityData.alt_name[0];
+    }
+  } else {
+    cityData.alt_name = [];
+  }
+});
 module.exports =
   dataUtils.sortDataByPopulation(
     dataUtils.filterByPopAndByCountry(
       dataUtils.dropUnusedDataFields(
-        d3.tsvParse(stringData, d3.autoType)
+        citiesData
       )
     )
   )
