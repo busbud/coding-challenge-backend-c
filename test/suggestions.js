46f91bc28fb9c31bd8b8180d6b6f13bc81ba7b31
Code Bling <codeblingx@gmail.com>
2019-04-22 13:26:45 -0400

    Use arrow functions in tests

--- test/suggestions.js
+++ test/suggestions.js
@@ -4,127 +4,120 @@ var app     = require('../app');
 var request = require('supertest')(app);
 chai.use(require('chai-things'));
 
-describe('GET /suggestions', function() {
-  describe('with a non-existent city', function () {
+describe('GET /suggestions', () => {
+  describe('with a non-existent city', () => {
     var response;
 
-    before(function (done) {
+    before(done => {
       request
         .get('/suggestions?q=SomeRandomCityInTheMiddleOfNowhere')
-        .end(function (err, res) {
+        .end((err, res) => {
           response = res;
           response.json = JSON.parse(res.text);
           done(err);
         });
     });
 
-    it('returns a 404', function () {
+    it('returns a 404', () => {
       expect(response.statusCode).to.equal(404);
     });
 
-    it('returns an empty array of suggestions', function () {
+    it('returns an empty array of suggestions', () => {
       expect(response.json.suggestions).to.be.instanceof(Array);
       expect(response.json.suggestions).to.have.length(0);
     });
   });
 
-  describe('with a partial string (no lat/long)', function () {
+  describe('with a partial string (no lat/long)', () => {
     var response;
 
-    before(function (done) {
+    before(done => {
       request
         .get('/suggestions?q=Ot')
-        .end(function (err, res) {
+        .end((err, res) => {
           response = res;
           response.json = JSON.parse(res.text);
           done(err);
         });
     });
 
-    it('returns a 200', function () {
+    it('returns a 200', () => {
       expect(response.statusCode).to.equal(200);
     });
 
-    it('returns an array of suggestions', function () {
+    it('returns an array of suggestions', () => {
       expect(response.json.suggestions).to.be.instanceof(Array);
     });
 
-    it('contains multiple suggestions', function () {
+    it('contains multiple suggestions', () => {
       expect(response.json.suggestions).to.have.length.above(1);
     });
 
-    it('contains latitudes and longitudes', function () {
-      expect(response.json.suggestions).to.satisfy(function (suggestions) {
-        return suggestions.every(function (suggestion) {
-          return suggestion.latitude && suggestion.longitude;
-        });
-      })
+    it('contains latitudes and longitudes', () => {
+      expect(response.json.suggestions).to.satisfy(suggestions =>
+        suggestions.every(suggestion => suggestion.latitude && suggestion.longitude)
+      )
     });
 
-    it('contains scores between 0 and 1', function () {
-      expect(response.json.suggestions).to.satisfy(function (suggestions) {
-        return suggestions.every(function (suggestion) {
-          const inRange = suggestion.score >= 0 && suggestion.score <= 1;
-          return suggestion.score != null && inRange;
-        });
-      })
+    it('contains scores between 0 and 1', () => {
+      expect(response.json.suggestions).to.satisfy(suggestions =>
+        suggestions.every(suggestion => 
+          suggestion.score != null && suggestion.score >= 0 && suggestion.score <= 1
+        )
+      )
     });
 
-    it('should NOT include a distance for any suggestions', function () {
+    it('should NOT include a distance for any suggestions', () => {
       expect(response.json.suggestions).to.have.length.at.least(1);
       expect(response.json.suggestions).to.all.not.have.property('distanceInKM');
     });
   });
 
-  describe('with a partial string and a latitude and longitude', function () {
+  describe('with a partial string and a latitude and longitude', () => {
     var response;
 
-    before(function (done) {
+    before(done => {
       request
         .get('/suggestions?q=LA&latitude=45.5&longitude=-73.5')
-        .end(function (err, res) {
+        .end((err, res) => {
           response = res;
           response.json = JSON.parse(res.text);
           done(err);
         });
     });
 
-    it('should include a distance with each suggestion', function () {
+    it('should include a distance with each suggestion', () => {
       expect(response.json.suggestions).to.have.length.at.least(1);
       expect(response.json.suggestions).to.all.have.property('distanceInKM');
     });
 
-    it('should return local results reasonably high in the rankings', function () {
-      expect(response.json.suggestions.slice(0, 5)).to.satisfy(function (suggestions) {
-        return suggestions.some(function (suggestion) {
-          return suggestion.name == 'Laval';
-        });
-      })
+    it('should return local results reasonably high in the rankings', () => {
+      expect(response.json.suggestions.slice(0, 5)).to.satisfy(suggestions =>
+        suggestions.some(suggestion => suggestion.name == 'Laval')
+      )
     });
 
-    it('should return balance top population suggestions with local results', function () {
-      expect(response.json.suggestions.slice(0, 5)).to.satisfy(function (suggestions) {
-        return suggestions.some(function (suggestion) {
-          return suggestion.name == 'Las Vegas';
-        });
-      })
+    it('should return balance top population suggestions with local results', () => {
+      expect(response.json.suggestions.slice(0, 5)).to.satisfy(suggestions =>
+        suggestions.some(suggestion => suggestion.name == 'Las Vegas')
+      )
     });
   });
 
-  describe('with a valid and unique city', function () {
+  describe('with a valid and unique city', () => {
     var response;
 
-    before(function (done) {
+    before(done => {
       request
         .get('/suggestions?q=Chambly')
-        .end(function (err, res) {
+        .end((err, res) => {
           response = res;
           response.json = JSON.parse(res.text);
           done(err);
         });
     });
 
-    it('contains exactly one match', function () {
+    it('contains exactly one match', () => {
       expect(response.json.suggestions.length).to.equal(1);
       expect(response.json.suggestions[0].name.match(/Chambly/i));
     });
