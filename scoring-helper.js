497f3d3ff706cdece6f7aedf736acd3fae68051f
Code Bling <codeblingx@gmail.com>
2019-04-23 16:00:14 -0400

    Apply Busbud style to scoring-helper.js

--- scoring-helper.js
+++ scoring-helper.js
@@ -2,50 +2,50 @@ const geolib = require('geolib');
 
 const addDistanceToSuggestions = (suggestions, lat, long) => {
   if (lat != null && long != null) {
-    const userCoord = {
+    const user_Coord = {
       latitude: lat,
       longitude: long
     };
-    suggestions.forEach(cityData => {
-      const cityCoord = {
-        latitude: cityData.latitude,
-        longitude: cityData.longitude
+    suggestions.forEach(city_data => {
+      const city_Coord = {
+        latitude: city_data.latitude,
+        longitude: city_data.longitude
       };
-      cityData.distanceInKM = geolib.getDistance(userCoord, cityCoord) / 1000; // distance in kilometers
+      city_data.distanceInKM = geolib.getDistance(user_Coord, city_Coord) / 1000; // distance in kilometers
     });
   }
 };
 
 const scoreSuggestions = suggestions => {
-  suggestions.forEach(cityData => { cityData.score = score(cityData.distanceInKM, cityData.population); });
+  suggestions.forEach(c => { c.score = score(c.distanceInKM, c.population); });
   sortByScore(suggestions);
   normaliseSuggestionScores(suggestions);
 };
 
 const score = (distance, population) => {
-  let distanceScore = distance ? Math.pow(distance, -10 / 3) : 1;
-  let populationScore = Math.pow(population, 6);
-  return distanceScore * populationScore;
+  let distance_score = distance ? Math.pow(distance, -10 / 3) : 1;
+  let population_score = Math.pow(population, 6);
+  return distance_score * population_score;
 };
 
-const sortByScore = suggestions => suggestions.sort((cityDataA, cityDataB) => cityDataB.score - cityDataA.score);
+const sortByScore = suggestions => suggestions.sort((a, b) => b.score - a.score);
 
 const normaliseSuggestionScores = suggestions => {
-  const maxScore = suggestions.length >= 1 ? Math.log(suggestions[0].score) : null;
-  const minScore = suggestions.length >= 1 ? Math.log(suggestions.slice(-1)[0].score) : null;
+  const max_score = suggestions.length >= 1 ? Math.log(suggestions[0].score) : null;
+  const min_score = suggestions.length >= 1 ? Math.log(suggestions.slice(-1)[0].score) : null;
 
   // if min and max are even (happens when only 1 result, or possibly edge cases),
   // use max to get a score of 1 instead of dividing by 0
-  const scoreDiff = (maxScore - minScore) || maxScore;
+  const score_diff = (max_score - min_score) || max_score;
 
-  suggestions.forEach(cityData => {
-    cityData.score = Math.log(cityData.score); // take the log, to avoid too much variance in the scores
+  suggestions.forEach(city_data => {
+    city_data.score = Math.log(city_data.score); // take the log, to avoid too much variance in the scores
 
     // normalise the score
-    cityData.score -= minScore; // subtract by minimum score to get same baseline
-    cityData.score /= scoreDiff; // divide by difference between min and max
+    city_data.score -= min_score; // subtract by minimum score to get same baseline
+    city_data.score /= score_diff; // divide by difference between min and max
 
-    cityData.score = Math.round(cityData.score * 100) / 100; // round and truncate to 2 decimals
+    city_data.score = Math.round(city_data.score * 100) / 100; // round and truncate to 2 decimals
   });
 };
 
