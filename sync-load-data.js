7e89e5333eb9ede79a1444364f1ac551bd309eb8
Code Bling <codeblingx@gmail.com>
2019-04-23 11:10:08 -0400

    Add spacing and comment

--- sync-load-data.js
+++ sync-load-data.js
@@ -5,6 +5,8 @@ let stringData = fs.readFileSync('./data/cities_canada-usa.tsv', 'utf8');
 stringData = stringData.replace(/["]/g, ''); // Force data to conform to RFC 4180 before parsing it
 
 const citiesData = d3.tsvParse(stringData, d3.autoType);
+
+// post processing, change comman-separated alt_name field into array of alt names
 citiesData.forEach(cityData => {
   if (cityData.alt_name != null) {
     cityData.alt_name = d3.csvParseRows(cityData.alt_name); // unwrap comma-separated alternative names
