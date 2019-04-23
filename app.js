b39bf9db23282e2bf3b1d0aae53630be0e830d2d
Code Bling <codeblingx@gmail.com>
2019-04-23 15:52:25 -0400

    Apply Busbud style to app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const slowDown = require('express-slow-down');
-const dataUtils = require('./data-utils');
-const scoringHelper = require('./scoring-helper');
+const SlowDown = require('express-slow-down');
+const date_utils = require('./data-utils');
+const scoring_helper = require('./scoring-helper');
 
 const app = express();
 const port = process.env.PORT || 2345;
@@ -9,44 +9,44 @@ const port = process.env.PORT || 2345;
 app.enable('trust proxy'); // trust `X-Forwarded-` HTTP headers, since we're behind a reverse proxy on Heroku
 
 // create rate-limiting middleware, which will delay requests that are coming in too fast
-const speedLimiter = slowDown({
+const slow_down = SlowDown({
   windowMs: 60 * 1000, // 1 minute
   delayAfter: 100, // allow 100 requests per minute, then...
   delayMs: 100 // begin adding 100ms of delay per request above 100:
 });
 
 // load and process data
-let citiesData = require('./sync-load-data');
-dataUtils.dropUnusedDataFields(citiesData)
-  .then(dataUtils.filterDataByCountry)
-  .then(dataUtils.filterDataByMinPopulation)
-  .then(dataUtils.sortDataByPopulationDesc)
-  .then(dataUtils.replaceRegionCodesWithNames)
-  .then(dataUtils.renameLatLong)
-  .then(dataUtils.addEasyDisplayName)
-  .then(processedCitiesData => { citiesData = processedCitiesData; }) // save the final processed data set
+let cities_data = require('./sync-load-data');
+date_utils.dropUnusedDataFields(cities_data)
+  .then(date_utils.filterDataByCountry)
+  .then(date_utils.filterDataByMinPopulation)
+  .then(date_utils.sortDataByPopulationDesc)
+  .then(date_utils.replaceRegionCodesWithNames)
+  .then(date_utils.renameLatLong)
+  .then(date_utils.addEasyDisplayName)
+  .then(d => { cities_data = d; }) // save the final processed data set
 ;
 
-app.use(speedLimiter); // rate-limit requests, per challenge requirements
+app.use(slow_down); // rate-limit requests, per challenge requirements
 app.get('/suggestions', (req, res) => {
   let suggestions = [];
 
   if (req.query.q != null && req.query.q.length >= 1) {
     // create a case-insensitive regex of our query, as this is probably the easiest way to check strings
-    const nameQueryRegex = new RegExp(`^${req.query.q}.*`, 'i');
+    const name_query_regex = new RegExp(`^${req.query.q}.*`, 'i');
 
-    suggestions = citiesData.filter(cityData => // filter the data set to find matching entries
-      cityData.name.match(nameQueryRegex) || // with the query in the name,
-        cityData.ascii.match(nameQueryRegex) || // with the query in the ascii name (ignores accents),
-        cityData.alt_name.some(altName => altName.match(nameQueryRegex)) // or with the query as one of the alternative names
+    suggestions = cities_data.filter(city_data => // filter the data set to find matching entries
+      city_data.name.match(name_query_regex) || // with the query in the name,
+        city_data.ascii.match(name_query_regex) || // with the query in the ascii name (ignores accents),
+        city_data.alt_name.some(n => n.match(name_query_regex)) // or with the query as one of the alternative names
     );
   }
 
   // clone each suggestion so that we can modify (add properties) them without affecting our original/raw data
-  suggestions = suggestions.map(cityData => Object.assign({}, cityData));
+  suggestions = suggestions.map(c => Object.assign({}, c));
 
-  scoringHelper.addDistanceToSuggestions(suggestions, req.query.latitude, req.query.longitude);
-  scoringHelper.scoreSuggestions(suggestions); // apply scores, normalise and sort
+  scoring_helper.addDistanceToSuggestions(suggestions, req.query.latitude, req.query.longitude);
+  scoring_helper.scoreSuggestions(suggestions); // apply scores, normalise and sort
 
   if (suggestions.length <= 0) { // if we can't find any suggestions
     res.status(404); // respond 404, per API requirements
