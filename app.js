e36009de0602de5fd15afdf9e84ad6ac3ded6dca
Gordon <gordonzhou68@outlook.com>
2019-08-06 14:50:35 -0400

    app setup using express server, ejs template set up, with a search bar, and stylesheet set up using sass

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const express = require("express");
+const app = express();
+const sass = require("node-sass-middleware");
+app.set("view engine", "ejs");
+app.use(
+  "/styles",
+  sass({
+    src: __dirname + "/styles",
+    dest: __dirname + "/public/styles",
+    debug: true,
+    outputStyle: "expanded"
+  })
+);
+PORT = 3000;
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+// module.exports = http.createServer(function (req, res) {
+//   res.writeHead(404, {'Content-Type': 'text/plain'});
+
+//   if (req.url.indexOf('/suggestions') === 0) {
+//     res.end(JSON.stringify({
+//       suggestions: []
+//     }));
+//   } else {
+//     res.end();
+//   }
+// }).listen(port, '127.0.0.1');
+
+
+
+app.get("/", (req, res) => {
+  res.send("hello! Please go to /suggestions");
+});
+
+app.get("/suggestions", (req, res) => {
+  res.render("index");
+});
+
+
+app.listen(PORT, () => {
+  console.log('Server running at localhost:%d/suggestions', PORT);
+});
+
+
+module.exports = app;
