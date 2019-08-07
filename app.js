9992c1655b17704525c80d0a709f264bcb2c9efa
Robin Westhaver <robinwesthaver@gmail.com>
2019-08-06 22:22:04 -0400

    having issues with accents and status 404

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var http = require('http');
 var port = process.env.PORT || 2345;
-const querystring = require('querystring');
 const url = require('url');
 const fs = require('fs');
 
 
 
-fs.readFile('./data/cities_canada-usa.tsv', function (err, data) {
+fs.readFile('./data/cities_canada-usa.tsv', 'utf8', function (err, data) {
     if (err) {
         throw err;
     }
@@ -14,7 +13,7 @@ fs.readFile('./data/cities_canada-usa.tsv', function (err, data) {
     processFile(content);         
 });
 
-let cities;
+let cities = [];
 function processFile(content) {
   cities = content.split('\n').map(city => {
     const info = city.split('\t');
@@ -40,32 +39,75 @@ function processFile(content) {
       // modDate: info[18],
     };
   });
+
+  for (i=0; i<cities.length; i++) {
+   switch(cities[i].admin1){
+   case "01":
+     cities[i].name = `${cities[i].name}, Alberta`
+     break;
+    case "10":
+      cities[i].name = `${cities[i].name}, Quebec`
+      break;
+  }}
   return cities;
 }
+//http://download.geonames.org/export/dump/admin1CodesASCII.txt
+// CA.02	British Columbia	British Columbia	5909050
+// CA.03	Manitoba	Manitoba	6065171
+// CA.04	New Brunswick	New Brunswick	6087430
+// CA.13	Northwest Territories	Northwest Territories	6091069
+// CA.07	Nova Scotia	Nova Scotia	6091530
+// CA.14	Nunavut	Nunavut	6091732
+// CA.08	Ontario	Ontario	6093943
+// CA.09	Prince Edward Island	Prince Edward Island	6113358
+// CA.10	Quebec	Quebec	6115047
+// CA.11	Saskatchewan	Saskatchewan	6141242
+// CA.12	Yukon	Yukon	6185811
+// CA.05	Newfoundland and Labrador	Newfoundland and Labrador	6354959
+
 
 function filter(cities, query) {
-  const filteredCities = cities.filter((city) => (city.population > 5000) && (city.countryCode == "US" || "CA") && (city.name.toLowerCase().includes(query.q.toLowerCase())))
+  const filteredCities = cities.filter((city) => (city.population > 5000) && (city.countryCode == "US" || "CA") && (city.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(query.q.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase())))
   for (i = 0; i < filteredCities.length; i++) {
     filteredCities[i].score = 0.1;
+    if (query.q.toLowerCase() == filteredCities[i].name.toLowerCase()) {
+      filteredCities[i].score = 0.8;
+    }
     if (query.latitude) {
       if (parseFloat(query.latitude) - parseFloat(filteredCities[i].latitude) < 1) {
         filteredCities[i].score += 0.1;
       }
     }
+    if (query.longitude) {
+      if (parseFloat(query.longitude) - parseFloat(filteredCities[i].longitude) < 1) {
+        filteredCities[i].score += 0.1;
+      }
+    }
   }
-  const sortedSuggestions = filteredCities.sort((a,b) => (b.score - a.score))
+  const sortedSuggestions = filteredCities.sort((a,b) => (b.score - a.score));
+  console.log(sortedSuggestions);
   return sortedSuggestions;
 }
 
 module.exports = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
 
   if (req.url.indexOf('/suggestions') === 0) {
     const parsedUrl = url.parse(req.url, true);
     const query = parsedUrl.query;
-    res.end(JSON.stringify({
-      suggestions: filter(cities, query),
-    }));
+    const suggestions = filter(cities, query);
+    if (suggestions.length < 1) {
+      res.status = 404;
+      res.statuscode = 404;
+      res.end(JSON.stringify({
+        suggestions: suggestions,
+      }));
+    } else {
+      res.statusCode = 200;
+      res.end(JSON.stringify({
+        suggestions: suggestions,
+      }));
+    }
   } else {
     res.end();
   }
