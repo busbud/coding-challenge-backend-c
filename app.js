76a9c87fb8a13b84f5a58e335005bc002ff09ef8
Gordon <gordonzhou68@outlook.com>
2019-08-08 18:17:16 -0400

    convert tsv to string

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
-const express = require("express");
-const app = express();
-const sass = require("node-sass-middleware");
-app.set("view engine", "ejs");
-app.use(
-  "/styles",
-  sass({
-    src: __dirname + "/styles",
-    dest: __dirname + "/public/styles",
-    debug: true,
-    outputStyle: "expanded"
-  })
-);
-app.use(express.static(__dirname + "/public"));
-PORT = 3000;
+// const express = require("express");
+// const app = express();
+// const sass = require("node-sass-middleware");
+// app.set("view engine", "ejs");
+// app.use(
+//   "/styles",
+//   sass({
+//     src: __dirname + "/styles",
+//     dest: __dirname + "/public/styles",
+//     debug: true,
+//     outputStyle: "expanded"
+//   })
+// );
+// app.use(express.static(__dirname + "/public"));
+// PORT = 3000;
 
 
 
-// module.exports = http.createServer(function (req, res) {
-//   res.writeHead(404, {'Content-Type': 'text/plain'});
 
-//   if (req.url.indexOf('/suggestions') === 0) {
-//     res.end(JSON.stringify({
-//       suggestions: []
-//     }));
-//   } else {
-//     res.end();
-//   }
-// }).listen(port, '127.0.0.1');
+// app.get("/", (req, res) => {
+//   res.send("hello! Please go to /suggestions");
+// });
 
 
+// app.listen(PORT, () => {
+//   console.log('Server running at localhost:%d/suggestions', PORT);
+// });
 
-app.get("/", (req, res) => {
-  res.send("hello! Please go to /suggestions");
+
+// module.exports = app;
+
+////////////////////////////////////////////////////////////////////////////
+const http = require('http');
+const port = process.env.PORT || 2345;
+const fs = require('fs');
+
+fs.readFile('./data/cities_canada-usa.tsv', 'utf-8', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data.toString();
+    console.log(content)
 });
 
-app.get("/suggestions", (req, res) => {
-  res.render("index");
-});
 
+module.exports = http.createServer(function (req, res) {
+  res.writeHead(404, {'Content-Type': 'text/plain'});
 
-app.listen(PORT, () => {
-  console.log('Server running at localhost:%d/suggestions', PORT);
-});
+  if (req.url.indexOf('/suggestions') === 0) {
+    res.end(JSON.stringify({
+      suggestions: []
+    }));
+  } else {
+    res.end();
+  }
+}).listen(port, '127.0.0.1');
 
-
-module.exports = app;
+console.log('Server running at http://127.0.0.1:%d/suggestions', port);
