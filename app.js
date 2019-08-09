14ec7c1c20133f8f10ce540ffafdb2b9623e654f
Robin Westhaver <robinwesthaver@gmail.com>
2019-08-09 12:23:55 -0400

    received port error message, trying again

--- app.js
+++ app.js
@@ -138,6 +138,6 @@ module.exports = http.createServer(function(req, res) {
       res.end();
     }
   })
-  .listen(port, '127.0.0.1');
+  .listen(port);
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
