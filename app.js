d39832197e3aa56ef2be464ae30bc87296f307dd
nadeem ghani <ghani_nadeem@yahoo.com>
2019-08-15 07:48:08 -0700

    learning to node

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 var http = require('http');
+var url = require('url');
 var port = process.env.PORT || 2345;
+
+var fs = require('fs');
+
+var cities = {};
+var partialCities = {};
+
+var csvData = fs.readFileSync('data/cities_canada-usa.tsv', 'utf-8');
+var csvLines = csvData.split('\n');
+var headers = csvLines.shift().split('\t');
+
+function addOrAppend(dict, key, val) {
+    if (dict[key]) {
+        dict[key].push(val);
+    } else {
+        dict[key] = [val];
+    }
+}
+
+csvLines.forEach(function(line){
+    row = line.split('\t');
+    cityName = String(row[1])
+    addOrAppend(cities, cityName, row);
+    for (var i=1; i<cityName.length; i++) {
+        addOrAppend(partialCities, cityName.substring(0,i), row);
+    }
+})
 
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
   if (req.url.indexOf('/suggestions') === 0) {
+    queryParams = url.parse(req.url, true).query;
+    if (queryParams.q) {
+        if (cities[queryParams.q]) {
+            console.log(cities[queryParams.q]);
+        }
+        if (partialCities[queryParams.q]) {
+            console.log("-----------")
+            console.log(partialCities[queryParams.q])
+        }
+    }
     res.end(JSON.stringify({
       suggestions: []
     }));
