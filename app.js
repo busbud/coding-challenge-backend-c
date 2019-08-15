924edd6c5fe12a064b70f71456e2821c25d95ec6
nadeem ghani <ghani_nadeem@yahoo.com>
2019-08-15 09:35:30 -0700

    what am i gonna do

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+// TODO: calculate distance from user lat, long
+// estimate is good enough
+// http://jonisalonen.com/2014/computing-distance-between-coordinates-can-be-simple-and-fast/
+// https://stackoverflow.com/a/15737369
+// TODO: user metro area lookup? bounding box? voronoi map?
+// for each city, find nearest city. bounding box vertex goes midway (prorate
+// by population?) on line connecting two cities. if user lat,long are in city
+// bounding box, city gets a score bump (how much?)
+// TODO: partialCities has many keys -> same val; custom dict to reduce size
+// https://codereview.stackexchange.com/questions/85842/a-dictionary-that-allows-multiple-keys-for-one-value
+// TODO: handle typos in city query
+// this is hard: https://medium.com/snipette/types-of-typos-aeee34ab0424
+// most queries from phone? no keyboard clustering assumptions
+// supported languages? each language has its own list of common typos
+// TODO: scoring function?!? score = max(1, f(nameScore, distanceScore))
+// TODO: nameScore = 1 for perfect match, 1 - n * delta
+//                      where n distance from perfect match
+// TODO: distance score = exp(-1 * distance from city center)
+// TODO:
+//
 var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 2345;
-
 var fs = require('fs');
 
 var cities = {};
