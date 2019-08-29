2a3a69e93039f93620b4f8631f5778e302e034e5
Tino Vyatkin <tino@vtkn.io>
2019-08-28 23:55:14 -0400

    all tests passed

--- app.js
+++ app.js
@@ -1,20 +1,90 @@
-var http = require("http");
-var port = process.env.PORT || 2345;
+'use strict';
 
-module.exports = http
-  .createServer(function(req, res) {
-    res.writeHead(404, { "Content-Type": "text/plain" });
+const Koa = require('koa');
+const session = require('koa-session2');
+const { RateLimiterMemory } = require('rate-limiter-flexible');
 
-    if (req.url.indexOf("/suggestions") === 0) {
-      res.end(
-        JSON.stringify({
-          suggestions: []
-        })
-      );
-    } else {
-      res.end();
+const { isQueryCached, cacheResults } = require('./lib/requests-cache');
+const buildSuggestions = require('./lib/suggestions-builder');
+const { LONGEST_CITY_NAME } = require('./lib/data-loader');
+
+// we will allow up to 4 request per second
+const rateLimiter = new RateLimiterMemory({
+  points: 4,
+  duration: 1,
+  blockDuration: 2
+});
+
+const { PORT = '1234' } = process.env;
+
+const app = new Koa();
+app.use(session()).use(
+  /**
+   * @param {import('koa').Context} ctx
+   */
+  async ctx => {
+    // Validating input
+    ctx.assert(
+      ctx.path === '/suggestions',
+      404,
+      'We have only `suggestions` endpoint here'
+    );
+    ctx.assert(ctx.method === 'GET', 405, 'Method not allowed');
+    ctx.assert(
+      ctx.query.q,
+      400,
+      `Bad request, expecting non-empty "q" parameter`
+    );
+    if (ctx.query.q.length > LONGEST_CITY_NAME && !/[,\s]/.test(ctx.query.q)) {
+      // By design it should return empty array as JSON body even on 4xx errors
+      ctx.body = { suggestions: [] };
+      ctx.status = 404;
+      return;
     }
-  })
-  .listen(port, "127.0.0.1");
+    // if latitude or longitude is supplied they must be both valid and present both
+    const [latitude, longitude] = ['latitude', 'longitude'].map(p =>
+      parseFloat(ctx.query[p])
+    );
+    if (latitude || longitude) {
+      // Longitude must be from -180 to 180
+      // Latitude must be from -90 to 90
+      ctx.assert(
+        latitude >= -90 &&
+          latitude <= 90 &&
+          longitude >= -180 &&
+          longitude <= 180,
+        400,
+        'Longitude and latitude must either both present and valid or both absent from query'
+      );
+    }
 
-console.log("Server running at http://127.0.0.1:%d/suggestions", port);
+    // Rate limiting
+    try {
+      const rateLimiterRes = await rateLimiter.consume(ctx.request.ip, 1);
+      ctx.set('X-RateLimit-Remaining', rateLimiterRes.remainingPoints);
+    } catch (rateLimiterRes) {
+      ctx.set('X-RateLimit-Remaining', rateLimiterRes.remainingPoints);
+      ctx.set('Retry-After', rateLimiterRes.msBeforeNext / 1000);
+      ctx.set('X-RateLimit-Limit', 3);
+      ctx.set(
+        'X-RateLimit-Reset',
+        new Date(Date.now() + rateLimiterRes.msBeforeNext)
+      );
+      ctx.throw(429, 'Too many requests');
+    }
+
+    // Request cache test
+    if (await isQueryCached(ctx)) return;
+
+    // all good, let's do data
+    ctx.body = {
+      suggestions: buildSuggestions(ctx.query.q, latitude, longitude)
+    };
+
+    await cacheResults(ctx);
+  }
+);
+
+app.once('listen', () => console.log('Server listening on port %s', PORT));
+
+module.exports = app.listen(parseInt(PORT, 10));
--- test/suggestions.js
+++ test/suggestions.js
@@ -1,73 +1,133 @@
-var expect = require("chai").expect;
-var app = require("../app");
-var request = require("supertest")(app);
+// Following is for busbud-lint who want me to remove space between async ()
+/* eslint-disable space-before-function-paren */
 
-describe("GET /suggestions", function() {
-  describe("with a non-existent city", function() {
-    var response;
+const expect = require('chai').expect;
+const app = require('../app');
+const request = require('supertest')(app);
 
-    before(function(done) {
-      request
-        .get("/suggestions?q=SomeRandomCityInTheMiddleOfNowhere")
-        .end(function(err, res) {
-          response = res;
-          response.json = JSON.parse(res.text);
-          done(err);
-        });
+describe('GET /suggestions', () => {
+  after(async () => new Promise(resolve => app.close(resolve)));
+
+  describe('with a non-existent city', () => {
+    let response;
+
+    before(async () => {
+      response = await request.get(
+        '/suggestions?q=SomeRandomCityInTheMiddleOfNowhere'
+      );
     });
 
-    it("returns a 404", function() {
+    it('returns a 404', () => {
       expect(response.statusCode).to.equal(404);
     });
 
-    it("returns an empty array of suggestions", function() {
-      expect(response.json.suggestions).to.be.instanceof(Array);
-      expect(response.json.suggestions).to.have.length(0);
+    it('returns an empty array of suggestions', () => {
+      expect(response.body.suggestions).to.be.instanceof(Array);
+      expect(response.body.suggestions).to.have.length(0);
     });
   });
 
-  describe("with a valid city", function() {
-    var response;
+  describe('with a valid city', () => {
+    let response;
 
-    before(function(done) {
-      request.get("/suggestions?q=Montreal").end(function(err, res) {
-        response = res;
-        response.json = JSON.parse(res.text);
-        done(err);
-      });
+    before(async () => {
+      response = await request.get('/suggestions?q=Montreal');
     });
 
-    it("returns a 200", function() {
+    it('returns a 200', () => {
       expect(response.statusCode).to.equal(200);
     });
 
-    it("returns an array of suggestions", function() {
-      expect(response.json.suggestions).to.be.instanceof(Array);
-      expect(response.json.suggestions).to.have.length.above(0);
+    it('returns an array of suggestions', () => {
+      expect(response.body.suggestions).to.be.instanceof(Array);
+      expect(response.body.suggestions).to.have.length.above(0);
     });
 
-    it("contains a match", function() {
-      expect(response.json.suggestions).to.satisfy(function(suggestions) {
-        return suggestions.some(function(suggestion) {
-          return suggestion.name.test(/montreal/i);
-        });
+    it('contains a match', () => {
+      expect(response.body.suggestions).to.satisfy(suggestions => {
+        return suggestions.some(suggestion =>
+          /montréal/i.test(suggestion.name)
+        );
       });
     });
 
-    it("contains latitudes and longitudes", function() {
-      expect(response.json.suggestions).to.satisfy(function(suggestions) {
-        return suggestions.every(function(suggestion) {
-          return suggestion.latitude && suggestion.longitude;
-        });
+    it('contains latitudes and longitudes', () => {
+      expect(response.body.suggestions).to.satisfy(suggestions => {
+        return suggestions.every(
+          suggestion => suggestion.latitude && suggestion.longitude
+        );
       });
     });
 
-    it("contains scores", function() {
-      expect(response.json.suggestions).to.satisfy(function(suggestions) {
-        return suggestions.every(function(suggestion) {
-          return suggestion.latitude && suggestion.longitude;
-        });
-      });
+    it('contains scores', () => {
+      expect(response.body.suggestions).to.satisfy(suggestions =>
+        suggestions.every(
+          suggestion => suggestion.score >= 0 && suggestion.score <= 1
+        )
+      );
+    });
+  });
+
+  describe('geo biasing', () => {
+    // prevent rate limiting
+    afterEach(done => setTimeout(done, 500));
+
+    it('return Washington from Utah while calling from San Francisco', async () => {
+      // San Francisco coordinates
+      const { body } = await request.get(
+        '/suggestions?q=Washing&latitude=37.7577627&longitude=-122.4727052'
+      );
+      expect(body.suggestions[0].name).to.contains('Utah, US');
+    });
+
+    it('return Washington from D.C. while calling from New York', async () => {
+      // San Francisco coordinates
+      const { body } = await request.get(
+        '/suggestions?q=Washing&latitude=40.6976633&longitude=-74.1201063'
+      );
+      expect(body.suggestions[0].name).to.contains('New Jersey, US');
+    });
+  });
+
+  describe('allow localized names', () => {
+    // prevent rate limiting
+    afterEach(done => setTimeout(done, 500));
+
+    // IT SEEMS LIKE A BUG IN SUPERAGENT - it doesn't encode query string
+
+    it('return Washington for Вашинг', async () => {
+      // San Francisco coordinates
+      const { body } = await request
+        .get('/suggestions')
+        .query(`q=${encodeURIComponent('Вашинг')}`);
+      expect(body.suggestions[0].name).to.contains('Washington');
+    });
+
+    it('return Montreal for Monreāl', async () => {
+      // San Francisco coordinates
+      const { body } = await request
+        .get('/suggestions')
+        .query(`q=${encodeURIComponent('monreāl')}`);
+      expect(body.suggestions[0].name).to.contains('Montréal');
+    });
+  });
+
+  describe('allow some misspellings and case insensitive names', () => {
+    // prevent rate limiting
+    afterEach(done => setTimeout(done, 500));
+
+    // IT SEEMS LIKE A BUG IN SUPERAGENT - it doesn't encode query string
+
+    it('return Montréal for Nonreal', async () => {
+      // San Francisco coordinates
+      const { body } = await request.get('/suggestions').query(`q=Nonreal`);
+      expect(body.suggestions[0].name).to.contains('Montréal');
+    });
+
+    it('return Montreal for MONtREaL', async () => {
+      // San Francisco coordinates
+      const { body } = await request.get('/suggestions').query(`q=MONtREaL`);
+      expect(body.suggestions[0].name).to.contains('Montréal');
     });
   });
 });
