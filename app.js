eb62afa8841d0ac80826082fc45c4dcbf1040e5b
Lisa Zhao <zhao.lisa@hotmail.com>
2019-09-25 22:30:15 -0400

    added express. parse data on server start

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const suggestions = require('./src/suggestions.js');
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+const express = require('express');
+const parse = require('csv-parse/lib/sync');
+const http = require('http');
+const fs = require('fs');
+const port = process.env.PORT || 2345;
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
+const app = express();
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+// load file on server start to only load once
+const data = fs.readFileSync('./data/cities_canada-usa.tsv');
+const cities = parse(data, {
+  columns: true,
+  delimiter: '\t',
+  escape: '\\',
+  quote: null
+});
+
+// parse and trim alt_name
+cities.forEach((city) => {
+  const altNames = city.alt_name.split(",").map((name) => {
+    return name.trim();
+  })
+  city.alt_name = altNames;
+})
+
+app.get('/suggestions', (req, res) => {
+  const suggestionList = suggestions.getSuggestions(req.query);
+  res.status(200).send(JSON.stringify(cities));
+});
+
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+});
---
+++ src/suggestions.js
@@ -0,0 +1,6 @@
+function getSuggestions(query) {
+  console.log(query);
+  return {suggestions: 'me'};
+}
+
+module.exports.getSuggestions = getSuggestions;
