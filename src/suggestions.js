c95025d53ba9a534d0601be68e095f54420d3c53
Lisa Zhao <zhao.lisa@hotmail.com>
2019-09-27 18:15:18 -0400

    add logic for scoring with given location

--- src/suggestions.js
+++ src/suggestions.js
@@ -1,3 +1,5 @@
+const geolib = require('geolib');
+
 function getSuggestions(query, cities) {
   const { q, longitude, latitude } = query;
 
@@ -35,23 +37,46 @@ for the city, but we currently have no data for that so we will base confidence 
 the name string that is provided in the query string, weighted with population (assumption that people
 usually search for places with larger populations)
 
-Current Weight:
+Basic Weight:
 0.8 for proportion of query string given
 0.2 for population
 */
 
 function getCityScores(cities, query, qLongitude, qLatitude) {
-  // TODO: implement
   const largestPopulation = Math.max.apply(Math, cities.map((city) => {return city.population}));
   console.log(largestPopulation);
   const qLength = query.length;
+  const hasLocation = qLongitude && qLatitude
 
-  const basicScoreCities = cities.map((city) => {
-    city.score = (query.length/city.name.length) * 0.8 + (city.population/largestPopulation)* 0.2
+  let citiesWithScores = cities.map((city) => {
+    city.score = (query.length/city.name.length) * 0.8 + (city.population/largestPopulation)* 0.2;
     return city;
   })
 
-  return basicScoreCities;
+  if (hasLocation) {
+    console.log("calculating location");
+    const citiesWithLocDifference = citiesWithScores.map((city) => {
+      city.locDifference = geolib.getDistance(
+        {latitude: city.lat, longitude: city.long},
+        {latitude: qLatitude, longitude: qLongitude}
+      );
+      return city;
+    });
+
+    const farthestDistance = Math.max.apply(Math, citiesWithLocDifference.map((city) => {return city.locDifference}));
+
+    /* weight original score and distance score 50/50
+    New weight distribution:
+    0.5 for location
+    0.4 for proportion of query string given
+    0.1 for population
+     */
+    citiesWithGeoScores = citiesWithLocDifference.map((city) => {
+      city.score = city.score * 0.5 + (1-(city.locDifference/farthestDistance)) * 0.5;
+      return city;
+    })
+  }
+  return citiesWithScores;
 }
 
 function cleanCitiesObjects(cities) {
@@ -70,7 +95,7 @@ function cleanCitiesObjects(cities) {
 
 function createUniqueName(city) {
   const { admin1, country, name } = city;
-  return country == "US" ? [name, admin1, country].join(", ") : [name, country].join(", ");
+  return country == 'US' ? [name, admin1, country].join(', ') : [name, country].join(', ');
 }
 
 module.exports.getSuggestions = getSuggestions;
