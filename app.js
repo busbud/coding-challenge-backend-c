0cb1f531bb73dd02696092e3d1fba9aed2661339
Javed Nissar <javed.nissar@mail.utoronto.ca>
2019-10-03 02:31:56 -0400

    Fixed host name

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ module.exports = http.createServer(function (req, res) {
   } else {
     res.end();
   }
-}).listen(port, '127.0.0.1');
+}).listen(port, '0.0.0.0');
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
