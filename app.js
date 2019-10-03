62e12d967d42ad6c6c33d6e0d13a9d26ff16079a
Javed Nissar <javed.nissar@mail.utoronto.ca>
2019-10-03 02:23:46 -0400

    Finalized app.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+'use strict';
+
 var http = require('http');
 var port = process.env.PORT || 2345;
+const url = require('url');
+const Cities = require('./cities.js').Cities;
+const cities = new Cities();
 
 module.exports = http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
+  const queryData = url.parse(req.url, true).query;
 
   if (req.url.indexOf('/suggestions') === 0) {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    var suggestions = [];
+    if(queryData.q && queryData.longitude && queryData.latitude){
+      suggestions = cities.queryWithLocation(queryData.q, parseFloat(queryData.longitude), parseFloat(queryData.latitude));
+    }else if(queryData.q){
+      suggestions = cities.queryWithoutLocation(queryData.q);
+    }
+    if(suggestions.length > 0){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+    }
     res.end(JSON.stringify({
-      suggestions: []
+      suggestions: suggestions
     }));
   } else {
     res.end();
