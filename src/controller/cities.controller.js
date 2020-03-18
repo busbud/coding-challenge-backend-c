0c2ceef4891ee2a798a32c777f41caf14a4d5097
arsalan.ahmed <arsalanabc@gmail.com>
2020-03-18 13:06:57 -0400

    Catch based endpoint in cities controller

--- src/controller/cities.controller.js
+++ src/controller/cities.controller.js
@@ -3,13 +3,19 @@ import beautify from 'json-beautify';
 
 const citiesController = {
     suggestions: (request, response) => {
-        const result = citiesModel.getSuggestions(request);
-        if(result && result.suggestions.length > 0){
-            response.writeHead(200, { 'Content-Type': 'text/plain' });
+        if (request === "/suggestions") {
+            return response.end(
+                JSON.stringify({ "suggestions": [] })
+            );
+        } else {
+            const result = citiesModel.getSuggestions(request);
+            if (result && result.suggestions.length > 0) {
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+            }
+            return response.end(
+                beautify(result, null, 2)
+            );
         }
-        return response.end(
-            beautify(result, null, 2)
-        );
     }
 };
 
