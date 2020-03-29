97c537824529dadef5cdb9ba8a09135d01cb0e60
Doan Tran Quy <quydoantran@gmail.com>
2020-03-29 15:21:01 -0400

    Implement suggestion feature

--- src/app.js
+++ src/app.js
@@ -1,12 +1,39 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+import path from 'path';
+import http from 'http';
+import url from 'url';
+import { SearchCityFromFile } from './services';
+
+const port = process.env.PORT || 2345;
+
+const cityDataFile = path.resolve('./data/cities_canada-usa.tsv');
+const searchService = new SearchCityFromFile({ dataFile: cityDataFile });
+
+function searchSuccess (cities, res) {
+  if (cities.length <= 0) {
+    searchFailure(res);
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      suggestions: cities
+    }));
+  }
+}
+
+function searchFailure (res) {
+  res.writeHead(404, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    suggestions: []
+  }));
+}
 
 module.exports = http.createServer(function (req, res) {
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.writeHead(404, { 'Content-Type': 'json/plain' });
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
+  // eslint-disable-next-line node/no-deprecated-api
+  const urlParts = url.parse(req.url, { parseQueryString: true });
+  if (urlParts.pathname === '/suggestions') {
+    searchService.search(urlParts.query)
+      .then((cities) => searchSuccess(cities, res))
+      // eslint-disable-next-line handle-callback-err
+      .catch(err => searchFailure(res));
   } else {
     res.end();
   }
