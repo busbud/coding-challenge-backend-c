e423928ecfb269698796d3f6d6fc71838969dc72
Khaled Souf <khaled.souf@gmail.com>
2020-07-15 15:45:19 -0400

    adding linter

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const http = require('http');
+const port = process.env.PORT || 2345;
 
-module.exports = http.createServer(function (req, res) {
+module.exports = http.createServer(function(req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
 
   if (req.url.indexOf('/suggestions') === 0) {
     res.end(JSON.stringify({
-      suggestions: []
+      suggestions: [],
     }));
   } else {
     res.end();
