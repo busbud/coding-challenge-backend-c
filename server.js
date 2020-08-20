665db2f1b57dd0c54e5102dcbe43b3436d45fd49
Patrick Frank <patr.frank@gmx.de>
2020-08-20 18:35:56 +0200

    validate querystring for incoming get requests

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// node imports
 const path = require('path');
-const fs = require('fs');
 const express = require('express');
+
+// own imports
+const location = require('./location');
 
 
 // instantiate express app
@@ -13,10 +16,21 @@ app.use(express.static(path.join(__dirname, 'client')));
 // handle GET requests to /suggestions
 // querystrings of the form ?q=a&lat=91.0&long=18.2 are available in req.query as key:value object
 app.get("/suggestions", (req, res) => {
+  let params = req.query;
+  let query = "";
+  let lat = null;
+  let long = null;
+
+  if (/^[a-zA-Z]+$/igm.test(params.q)){
+    query = params.q;
+  } else {
+    res.status(400).send("Bad request.");
+  }
+
   res.send(JSON.stringify(req.query));
 });
 
 // express app listens on specified port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
