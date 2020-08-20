4b74e2dc3f642a4c43fa27634e568c3d4de43ba7
Patrick Frank <patr.frank@gmx.de>
2020-08-20 18:12:01 +0200

    create suggestions endpoint

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const port = process.env.port || 3000;
 // this function serves static files from the ./client directory
 app.use(express.static(path.join(__dirname, 'client')));
 
-
+// handle GET requests to /suggestions
+// querystrings of the form ?q=a&lat=91.0&long=18.2 are available in req.query as key:value object
+app.get("/suggestions", (req, res) => {
+  res.send(JSON.stringify(req.query));
+});
 
 // express app listens on specified port
 app.listen(port, () => {
