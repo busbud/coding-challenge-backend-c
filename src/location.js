bd858f9c3070604fe220e7f6d7ac6adb0501977c
Patrick Frank <patr.frank@gmx.de>
2020-08-25 22:27:33 +0200

    implemented function to measure spatial distances

--- src/location.js
+++ src/location.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
 
 // constant names for canadian provinces as of https://en.wikipedia.org/wiki/List_of_FIPS_region_codes_(A%E2%80%93C)#CA:_Canada
-const provincesCA = [
+const PROVINCESCA = [
   'Alberta','British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador',
   'Nova Scotia', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Yukon',
   'Northwest Territories', 'Nunavut'
 ];
+// one degree of latitude in kilometres
+const LAT2KM = 111.0
 
 const extractDataFromCSV = () => {
   // read data from csv file
@@ -27,6 +29,18 @@ const extractDataFromCSV = () => {
   }));
   return data;
 };
+
+// calculate distance between two coordinates, as taken from and explained by
+// https://stackoverflow.com/questions/365826/calculate-distance-between-2-gps-coordinates
+const spatialDistance = (lat1, lon1, lat2, lon2) => {
+  const p = 0.017453292519943295; // Math.PI / 180
+  const R2 = 12742; // 2 * R; R = 6371 km
+  const a = 0.5 - Math.cos((lat2 - lat1) * p)/2 + 
+    Math.cos(lat1 * p) * Math.cos(lat2 * p) * 
+    (1 - Math.cos((lon2 - lon1) * p))/2;
+
+  return R2 * Math.asin(Math.sqrt(a))
+}; 
 
 const search = (query, lat, long) => {
   
