06e00ea09fc8594b3522884cf71e2dd53d913dcd
Patrick Frank <patr.frank@gmx.de>
2020-08-27 13:49:00 +0200

    implement function to write array of objects to file

--- src/security.js
+++ src/security.js
@@ -1,3 +1,18 @@
+const fs = require('fs')
+
+// write an array with object elements to file
+const writeArray = (arr, filePath) => {
+  const serialized = arr.reduce((accumulated, current) => {
+    return accumulated += JSON.stringify(current) + '\n'
+  }, "");
+
+  fs.writeFileSync(filePath, serialized);
+};
+
+// test
+writeArray([{user: "usr1", password: "pw1"}, {user: "usr2", password:"pw2"}], "./data/users.txt");
+
+// check if user has an jwt access cookie set
 module.exports.validateUser = () => {
-  
+
 };
