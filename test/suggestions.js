305711c428294ddd6f769bdcaf7d9221ec0da2fa
Jonas <alessi.jonas@gmail.com>
2020-08-29 12:07:13 -0300

    Add a few fix in the test

--- test/suggestions.js
+++ test/suggestions.js
@@ -1,8 +1,8 @@
-var expect  = require('chai').expect;
-var app     = require('../app');
+var expect = require('chai').expect;
+var app = require('../app');
 var request = require('supertest')(app);
 
-describe('GET /suggestions', function() {
+describe('GET /suggestions', function () {
   describe('with a non-existent city', function () {
     var response;
 
@@ -16,17 +16,17 @@ describe('GET /suggestions', function() {
         });
     });
 
-    it('returns a 404', function () {
+    it('should returns a 404', function () {
       expect(response.statusCode).to.equal(404);
     });
 
-    it('returns an empty array of suggestions', function () {
+    it('should returns an empty array of suggestions', function () {
       expect(response.json.suggestions).to.be.instanceof(Array);
       expect(response.json.suggestions).to.have.length(0);
     });
   });
 
-  describe('with a valid city', function () {
+  describe('should with a valid city', function () {
     var response;
 
     before(function (done) {
@@ -39,41 +39,37 @@ describe('GET /suggestions', function() {
         });
     });
 
-    it('returns a 200', function () {
+    it('should returns a 200', function () {
       expect(response.statusCode).to.equal(200);
     });
 
-    it('returns an array of suggestions', function () {
+    it('should returns an array of suggestions', function () {
       expect(response.json.suggestions).to.be.instanceof(Array);
       expect(response.json.suggestions).to.have.length.above(0);
     });
 
-    describe.skip('Validate the shape of the data being returned', function() {
-      it('contains latitudes and longitudes', function () {	
-        expect(response.json.suggestions).to.satisfy(function (suggestions) {	
-          return suggestions.every(function (suggestion) {	
-            return suggestion.latitude && suggestion.longitude;	
-          });	
-        })	
-      });	
+    describe.skip('Validate the shape of the data being returned', function () {
+      it('should contains latitudes and longitudes', function () {
+        expect(response.json.suggestions).to.satisfy(function (suggestions) {
+          return suggestions.every(function (suggestion) {
+            return suggestion.latitude && suggestion.longitude;
+          });
+        })
+      });
 
-      it('contains scores', function () {	
-        expect(response.json.suggestions).to.satisfy(function (suggestions) {	
-          return suggestions.every(function (suggestion) {	
-            return suggestion.latitude && suggestion.longitude;	
-          });	
-        })	
+      it('should contains scores', function () {
+        expect(response.json.suggestions).to.satisfy(function (suggestions) {
+          return suggestions.every(function (suggestion) {
+            return suggestion.latitude && suggestion.longitude;
+          });
+        })
       });
     });
-    
-    it('is a gratuitously failing test you should remove to prove you ran the tests', function () {	
-      expect(true).to.equal(false);	
-    });	    
 
-    it('contains a match', function () {
+    it('should contains a match', function () {
       expect(response.json.suggestions).to.satisfy(function (suggestions) {
         return suggestions.some(function (suggestion) {
-          return suggestion.name.test(/montreal/i);
+          return new RegExp(suggestion.name, 'i').test('montreal');
         });
       })
     });
