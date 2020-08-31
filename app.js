1575d59625c20cb804d2e82d2780c8171050e502
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-08-31 11:15:42 -0400

    refactor: installed nodemon & express. Built server using express

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const express = require("express");
+const app = express();
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+const port = process.env.PORT || 2345;
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
-  }
-}).listen(port, '127.0.0.1');
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("test");
+});
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+app.listen(port, () => {
+  console.log("Server running at http://127.0.0.1:%d", port);
+});
+// // app.listen(port, "127.0.0.1");
+// console.log("Server running at http://127.0.0.1:%d", port);
+
+// app.get('/suggestions')
+
+// var http = require("http");
+// var port = process.env.PORT || 2345;
+
+// module.exports = http
+//   .createServer(function (req, res) {
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+
+//     if (req.url.indexOf("/suggestions") === 0) {
+//       res.end(
+//         JSON.stringify({
+//           suggestions: [],
+//         })
+//       );
+//     } else {
+//       res.end();
+//     }
+//   })
+//   .listen(port, "127.0.0.1");
+
+// console.log("Server running at http://127.0.0.1:%d/suggestions", port);
