76681a0336af7ff38fddef43ae3235d6d4d7cd03
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-03 23:37:19 -0400

    feat: root endpoint returning all rows

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 const express = require("express");
 const app = express();
+const database_pool = require("./db/index");
 
 const port = process.env.PORT || 2345;
+// app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.send("test");
+/*get all rows from the table*/
+app.get("/", async (req, res, next) => {
+  try {
+    const allCities = await database_pool.query("SELECT *  FROM geoname");
+    res.json(allCities);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get("/suggestions", (req, res) => {
-  res.send("suggestions");
-});
+// //get suggestions
+// app.get("/suggestions", (req, res) => {
+//   res.send("suggestions");
+//   res.send(
+//     JSON.stringify({
+//       // suggestions:
+//     })
+//   );
+// });
 
 app.listen(port, () => {
   console.log("Server running at http://127.0.0.1:%d", port);
 });
-
-// module.exports = http
-//   .createServer(function (req, res) {
-//     res.writeHead(404, { "Content-Type": "text/plain" });
-
-//     if (req.url.indexOf("/suggestions") === 0) {
-//       res.end(
-//         JSON.stringify({
-//           suggestions: [],
-//         })
-//       );
-//     } else {
-//       res.end();
-//     }
-//   })
-//   .listen(port, "127.0.0.1");
-
-// console.log("Server running at http://127.0.0.1:%d/suggestions", port);
