d960463d86f4987376b859f94a70f271057d2bb4
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-14 16:46:22 -0400

    test: resolve before hook json token error by returning json

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const database_pool = require("./db/index");
 
 const port = process.env.PORT || 2345;
 
-//GET all rows from TABLE geoname
 app.get("/", async (req, res, next) => {
   try {
     const allCities = await database_pool.query("SELECT *  FROM geoname");
@@ -23,16 +22,25 @@ app.get("/suggestions", async (req, res, next) => {
     let longMin = parseInt(long) - 3;
     let score = 0;
 
+    if (!q) {
+      return res.status(404).send("Please enter a valid search term");
+    }
+
     const suggestedCities = await database_pool.query(
-      "SELECT concat(name, ', ', admin1, ', ', country) AS name, lat AS latitude, long AS longitude, $8 AS score FROM geoname WHERE population > 5000 AND country IN ('US', 'CA') AND (name iLIKE $1 OR ascii iLIKE $1) AND (lat BETWEEN $2 AND $3 OR lat = $4 IS NULL) AND (long BETWEEN $5 AND $6 OR long = $7 IS NULL)",
+      "SELECT concat(name, ', ', admin1, ', ', country) AS name, lat AS latitude, long AS longitude, $8 AS score FROM geoname WHERE population > 5000 AND country IN ('US', 'CA') AND (name iLIKE $1 OR ascii iLIKE $1 OR ascii iLIKE $1) AND (lat BETWEEN $2 AND $3 OR lat = $4 IS NULL) AND (long BETWEEN $5 AND $6 OR long = $7 IS NULL)",
       [q + "%", latMin, latMax, lat, longMin, longMax, long, score]
     );
 
-    if (!q) {
-      return res.status(404).send("Sorry, we cannot find that!");
+    if (suggestedCities.rowCount === 0) {
+      return res
+        .status(404)
+        .send(
+          "No matches available.",
+          res.json({ suggestions: suggestedCities.rows })
+        );
+    } else {
+      res.json({ suggestions: suggestedCities.rows });
     }
-
-    res.json({ suggestions: suggestedCities.rows });
   } catch (err) {
     next(err);
   }
