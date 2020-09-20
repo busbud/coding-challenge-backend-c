a0905daa9d395cd6b6ff8c6b55db1c9e423268e9
Patrick Frank <patr.frank@gmx.de>
2020-09-20 12:11:57 +0200

    Cors no 9

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.set('trust proxy', true);
 
 // handle cors
 let corsParams = {
-  origin: 'https://city-search-react-ui.herokuapp.com'
+  origin: 'https://city-search-react-ui.herokuapp.com',
+  credentials: true
 };
 app.use(cors(corsParams));
 // app.use(function(req, res, next) {
