02f53de60cd0a6c7a188e6c5eaef34c62dfb121b
Lucas Reta <lucas.reta@ibm.com>
2020-09-25 12:46:22 -0300

    fixes response header and scope of endpoint
    - Only requests that are exact matches of the suggestions endpoint will be
      processed and an application/json reply will be issued. For now, a
      response status 200 is assumed. URIs that include the word
      "suggestions" but also contain extra characters (excluding whatever
      comes in the querystring or anchor) will now be excluded
    - All other URIs will throw a 404 Not Found error.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var http = require('http');
 var port = process.env.PORT || 2345;
 
 module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
-
-  if (req.url.indexOf('/suggestions') === 0) {
+  if (req.url.split('/').pop().split(/[?#]/).shift() === 'suggestions') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({
       suggestions: []
     }));
   } else {
-    res.end();
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404 Not Found');
   }
 }).listen(port, '127.0.0.1');
 
