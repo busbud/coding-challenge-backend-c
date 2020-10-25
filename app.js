25142d9d24ce67fcfd8941f4b063b5cc156f724e
Bruno <tharos@gmail.com>
2020-10-25 14:08:59 -0300

    initial working version

--- app.js
+++ app.js
@@ -1,16 +1,70 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const express = require('express');
 
-module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+const app = express();
 
-  if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
-  } else {
-    res.end();
+const defaultport = process.env.PORT || 2345;
+
+const config = require('config');
+
+const RateLimit = require('express-rate-limit');
+const CircuitBreaker = require('opossum');
+const handleRequest = require('./lib/handleRequest');
+const data = require('./lib/dataParser');
+
+const port = config.port ? config.port : defaultport;
+
+app.set('trust proxy', '127.0.0.1');
+
+const limiter = new RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  delayMs: 0,
+});
+
+// apply rate-limiting to all requests
+app.use(limiter);
+
+// load cities on startup. on error, fail hard.
+data.loadData(config.datafile, handleRequest.trie, config)
+  .catch((err) => { console.error('err'); console.error(err); process.exit(); });
+
+app.listen(port, () => {
+  console.log(`App listening on port : ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome! This site only has one API endpoint implemented. See /suggestions/?q=Londo');
+});
+
+const circuitBreakerOptions = {
+  timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+  errorThresholdPercentage: 100, // When 100% of requests fail, trip the circuit
+  resetTimeout: 5000, // After 5 seconds, try again.
+};
+const breaker = new CircuitBreaker(handleRequest.getFromCacheOrProcess, circuitBreakerOptions);
+
+const sendResponse = async (req, response, result) => {
+  response.set('Cache-Control', 'public, max-age=31557600, s-maxage=31557600'); // 1 year
+  response.status(result && result.data && result.data.length > 0 ? 200 : 404);
+  if (result.time != null) {
+    // This is not proper http for this case...
+    // If data was generated, this would indicate generation date.
+    // Not so valid for cities which are constant.
+    response.set('Last-Modified', new Date(result.time).toUTCString());
   }
-}).listen(port, '127.0.0.1');
+  response.send(JSON.stringify({
+    suggestions: result.data || [],
+  }));
+  Promise.resolve();
+};
 
-console.log('Server running at http://127.0.0.1:%d/suggestions', port);
+function sendToBreaker(req, res) {
+  return breaker.fire(req, res)
+    .then((result) => sendResponse(req, res, result))
+    .catch((err) => sendResponse(req, res, err));
+  // send the error to the end-user? challenge purposes only, don't do this in prod!
+}
+
+app.get('/suggestions', sendToBreaker);
+
+module.exports = app;
--- test/suggestions.js
+++ test/suggestions.js
@@ -1,81 +1,499 @@
-var expect  = require('chai').expect;
-var app     = require('../app');
-var request = require('supertest')(app);
+const { expect } = require('chai');
+const app = require('../app');
+const request = require('supertest')(app);
+const querystring = require('querystring');
 
-describe('GET /suggestions', function() {
-  describe('with a non-existent city', function () {
-    var response;
+describe('GET /suggestions', () => {
+  const simpleTestCases = [
+    // this array of tests was taken from PR #89
+    // @author Mohammad Fares <faressoft.com@gmail.com>
+    // I've commented out the tests with typos because my solution doesn't support that
+    {
+      params: { q: 'SomeRandomCityInTheMiddleOfNowhere' },
+      expectedFirstSuggestionName: undefined,
+      expectedFirstSuggestionScore: undefined,
+    },
+    {
+      params: { q: 'New York' },
+      expectedFirstSuggestionName: 'New York City, NY, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'New York', latitude: 43.70011, longitude: -79.4163 },
+      expectedFirstSuggestionName: 'New York City, NY, US',
+      expectedFirstSuggestionScore: null,
+    },
+    // {
+    //   params: {q: 'New123244142York'},
+    //   expectedFirstSuggestionName: 'New York City, NY, US',
+    //   expectedFirstSuggestionScore: null
+    // },
+    // {
+    //   params: {q: 'New12324  4142York'},
+    //   expectedFirstSuggestionName: 'New York City, NY, US',
+    //   expectedFirstSuggestionScore: null
+    // },
+    // {
+    //   params: {q: 'New123--##24  4142York'},
+    //   expectedFirstSuggestionName: 'New York City, NY, US',
+    //   expectedFirstSuggestionScore: null
+    // },
+    {
+      params: { q: 'New' },
+      expectedFirstSuggestionName: 'New York City, NY, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'NewY' },
+      expectedFirstSuggestionName: 'New York City, NY, US',
+      expectedFirstSuggestionScore: null,
+    },
+    // {
+    //  params: {q: 'Neww York'},
+    //  expectedFirstSuggestionName: 'New York City, NY, US',
+    //  expectedFirstSuggestionScore: null
+    // },
+    {
+      params: { q: 'Los Ang' },
+      expectedFirstSuggestionName: 'Los Angeles, CA, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Ottawa' },
+      expectedFirstSuggestionName: 'Ottawa, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'otaw' },
+      expectedFirstSuggestionName: 'Ottawa, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Otaw' },
+      expectedFirstSuggestionName: 'Ottawa, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Saint-Augustin-de-Desmaures' },
+      expectedFirstSuggestionName: 'Saint-Augustin-de-Desmaures, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Oakvi' },
+      expectedFirstSuggestionName: 'Oakville, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Blainville' },
+      expectedFirstSuggestionName: 'Blainville, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Atlanta' },
+      expectedFirstSuggestionName: 'Atlanta, GA, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'atlanta' },
+      expectedFirstSuggestionName: 'Atlanta, GA, US',
+      expectedFirstSuggestionScore: null,
+    },
+    // {
+    //  params: {q: 'alanta'},
+    //  expectedFirstSuggestionName: 'Atlanta, GA, US',
+    //  expectedFirstSuggestionScore: null
+    // },
+    {
+      params: { q: 'San Jose' },
+      expectedFirstSuggestionName: 'San Jose, CA, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'SanJose' },
+      expectedFirstSuggestionName: 'San Jose, CA, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Montreal' },
+      expectedFirstSuggestionName: 'Montréal, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'montreal' },
+      expectedFirstSuggestionName: 'Montréal, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    // {
+    //   params: {q: 'monetreal'},
+    //   expectedFirstSuggestionName: 'Montréal, QC, CA',
+    //   expectedFirstSuggestionScore: null
+    // },
+    // {
+    //   params: {q: 'montereal'},
+    //   expectedFirstSuggestionName: 'Montréal, QC, CA',
+    //   expectedFirstSuggestionScore: null
+    // },
+    {
+      params: { q: 'mont' },
+      expectedFirstSuggestionName: 'Montréal, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'mo' },
+      expectedFirstSuggestionName: 'Montréal, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'London' },
+      expectedFirstSuggestionName: 'London, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    // {
+    //   params: {q: 'Lendon'},
+    //   expectedFirstSuggestionName: 'London, ON, CA',
+    //   expectedFirstSuggestionScore: null
+    // },
+    {
+      params: { q: 'Londonderry' },
+      expectedFirstSuggestionName: 'Londonderry, NH, US',
+      expectedFirstSuggestionScore: 1,
+    },
+    {
+      params: { q: 'lond' },
+      expectedFirstSuggestionName: 'London, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'lo' },
+      expectedFirstSuggestionName: 'Los Angeles, CA, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'Londo', latitude: 43.70011, longitude: -79.4163 },
+      expectedFirstSuggestionName: 'London, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+  ];
 
-    before(function (done) {
+  // Foreach defiend text case
+  simpleTestCases.forEach((testCase) => {
+    // Generate query string
+    const params = querystring.stringify(testCase.params);
+
+    describe(`search by the query '${params}'`, () => {
+      let response;
+
+      before((done) => {
+        request
+          .get(`/suggestions?${params}`)
+          .end((err, res) => {
+            response = res;
+            response.json = JSON.parse(res.text);
+            done(err);
+          });
+      });
+
+      // Check for no suggestions
+      if (testCase.expectedFirstSuggestionName === undefined) {
+        it('should return no suggestions', () => {
+          expect(response.json.suggestions.length).to.equal(0);
+        });
+      }
+
+      // Check for name
+      if (testCase.expectedFirstSuggestionName) {
+        // it('should return 1 or more suggestions', function() {
+        //  expect(response.json.suggestions).to.have.length.above(0);
+        // });
+
+        it(`the first suggestion's name should be ${testCase.expectedFirstSuggestionName}`, () => {
+          expect(response.json.suggestions[0].displayName).to.equal(testCase.expectedFirstSuggestionName);
+        });
+      }
+      /*
+      // Check for score
+      if (testCase.expectedFirstSuggestionScore) {
+
+        it(`the first suggestion's score should be ${testCase.expectedFirstSuggestionScore}`, function() {
+          expect(response.json.suggestions).to.have.length.above(0);
+          expect(response.json.suggestions[0].score).to.equal(testCase.expectedFirstSuggestionScore);
+        });
+
+      }
+*/
+    });
+  });
+
+  const aliasedTestCases = [
+    {
+      params: { q: 'bigappl' }, // popular nickname "Big Apple"
+      expectedFirstSuggestionName: 'New York City, NY, US',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'kebek' }, // mi'kmaq name
+      expectedFirstSuggestionName: 'Québec, QC, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'atawa' }, // anishinaabe name
+      expectedFirstSuggestionName: 'Ottawa, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'ybr' }, // IATA name
+      expectedFirstSuggestionName: 'Brandon, MB, CA',
+      expectedFirstSuggestionScore: null,
+    },
+    {
+      params: { q: 'yto' }, // IATA name
+      expectedFirstSuggestionName: 'Toronto, ON, CA',
+      expectedFirstSuggestionScore: null,
+    },
+  ];
+  // Foreach defiend text case
+  aliasedTestCases.forEach((testCase) => {
+    // Generate query string
+    const params = querystring.stringify(testCase.params);
+
+    describe(`search by the aliased name '${params}'`, () => {
+      let response;
+
+      before((done) => {
+        request
+          .get(`/suggestions?${params}`)
+          .end((err, res) => {
+            response = res;
+            response.json = JSON.parse(res.text);
+            done(err);
+          });
+      });
+
+      // Check for name
+      if (testCase.expectedFirstSuggestionName) {
+        it(`the first suggestion's name should be ${testCase.expectedFirstSuggestionName}`, () => {
+          expect(response.json.suggestions[0].displayName).to.equal(testCase.expectedFirstSuggestionName);
+        });
+      }
+    });
+  });
+
+  describe('with a non-existent city', () => {
+    let response;
+
+    before((done) => {
       request
         .get('/suggestions?q=SomeRandomCityInTheMiddleOfNowhere')
-        .end(function (err, res) {
+        .end((err, res) => {
           response = res;
           response.json = JSON.parse(res.text);
           done(err);
         });
     });
 
-    it('returns a 404', function () {
+    it('returns a 404', () => {
       expect(response.statusCode).to.equal(404);
     });
 
-    it('returns an empty array of suggestions', function () {
+    it('returns an empty array of suggestions', () => {
       expect(response.json.suggestions).to.be.instanceof(Array);
       expect(response.json.suggestions).to.have.length(0);
     });
   });
 
-  describe('with a valid city', function () {
-    var response;
+  describe('with a valid ascii city', () => {
+    let response;
 
-    before(function (done) {
+    before((done) => {
       request
         .get('/suggestions?q=Montreal')
-        .end(function (err, res) {
+        .end((err, res) => {
           response = res;
           response.json = JSON.parse(res.text);
           done(err);
         });
     });
 
-    it('returns a 200', function () {
+    it('returns a 200', () => {
       expect(response.statusCode).to.equal(200);
     });
 
-    it('returns an array of suggestions', function () {
+    it('returns an array of suggestions', () => {
       expect(response.json.suggestions).to.be.instanceof(Array);
       expect(response.json.suggestions).to.have.length.above(0);
     });
 
-    describe.skip('Validate the shape of the data being returned', function() {
-      it('contains latitudes and longitudes', function () {	
-        expect(response.json.suggestions).to.satisfy(function (suggestions) {	
-          return suggestions.every(function (suggestion) {	
-            return suggestion.latitude && suggestion.longitude;	
-          });	
-        })	
-      });	
-
-      it('contains scores', function () {	
-        expect(response.json.suggestions).to.satisfy(function (suggestions) {	
-          return suggestions.every(function (suggestion) {	
-            return suggestion.latitude && suggestion.longitude;	
-          });	
-        })	
-      });
+    it('contains latitudes and longitudes', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.every((suggestion) => suggestion.latitude && suggestion.longitude));
     });
-    
-    it('is a gratuitously failing test you should remove to prove you ran the tests', function () {	
-      expect(true).to.equal(false);	
-    });	    
 
-    it('contains a match', function () {
-      expect(response.json.suggestions).to.satisfy(function (suggestions) {
-        return suggestions.some(function (suggestion) {
-          return suggestion.name.test(/montreal/i);
+    it('contains scores', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.every((suggestion) => suggestion.latitude && suggestion.longitude));
+    });
+
+    it('contains a match', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.some((suggestion) => suggestion.name.match(/montreal/i)));
+    });
+  });
+
+  describe('with a valid url-escaped city', () => {
+    let response;
+
+    before((done) => {
+      request
+        .get('/suggestions?q=montr%C3%A9al')
+        .end((err, res) => {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
         });
-      })
+    });
+
+    it('returns a 200', () => {
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('returns an array of suggestions', () => {
+      expect(response.json.suggestions).to.be.instanceof(Array);
+      expect(response.json.suggestions).to.have.length.above(0);
+    });
+
+    it('contains latitudes and longitudes', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.every((suggestion) => suggestion.latitude && suggestion.longitude));
+    });
+
+    it('contains scores', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.every((suggestion) => suggestion.latitude && suggestion.longitude));
+    });
+
+    it('contains a match', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.some((suggestion) => suggestion.name.match(/montreal/i)));
+    });
+  });
+
+  describe('with a valid utf8 city', () => {
+    let response;
+
+    before((done) => {
+      request
+        .get('/suggestions?q=montréal')
+        .end((err, res) => {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
+        });
+    });
+
+    it('returns a 200', () => {
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('returns an array of suggestions', () => {
+      expect(response.json.suggestions).to.be.instanceof(Array);
+      expect(response.json.suggestions).to.have.length.above(0);
+    });
+
+    it('contains latitudes and longitudes', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.every((suggestion) => suggestion.latitude && suggestion.longitude));
+    });
+
+    it('contains scores', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.every((suggestion) => suggestion.latitude && suggestion.longitude));
+    });
+
+    it('contains a match', () => {
+      expect(response.json.suggestions).to.satisfy((suggestions) => suggestions.some((suggestion) => suggestion.name.match(/montreal/i)));
+    });
+  });
+
+  describe('with a city that includes apostrophe', () => {
+    let response;
+
+    before((done) => {
+      request
+        .get("/suggestions?q=Val-d'or")
+        .end((err, res) => {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
+        });
+    });
+
+    it('returns a 200', () => {
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('returns the expected city', () => {
+      expect(response.json.suggestions).to.be.instanceof(Array);
+      expect(response.json.suggestions).to.have.length.above(0);
+      expect(response.json.suggestions[0].displayName).to.equal("Val-d'Or, QC, CA");
+    });
+  });
+
+  describe('with two identical requests', () => {
+    let firstResponse;
+    let secondResponse;
+    before((done) => {
+      request
+        .get('/suggestions?q=Waterl')
+        .end((err, res) => {
+          firstResponse = res;
+          firstResponse.json = JSON.parse(res.text);
+          done(err);
+        });
+      request
+        .get('/suggestions?q=Waterl')
+        .end((err, res) => {
+          secondResponse = res;
+          secondResponse.json = JSON.parse(res.text);
+        });
+    });
+
+    it('processes first request', () => {
+      expect(firstResponse.headers).to.not.have.property('last-modified');
+    });
+
+    it('gets second request from cache', () => {
+      expect(secondResponse.headers).to.have.property('last-modified');
+    });
+
+    it('returns a 200 for both', () => {
+      expect(firstResponse.statusCode).to.equal(200);
+      expect(secondResponse.statusCode).to.equal(200);
+    });
+  });
+
+  describe('with two near requests', () => {
+    let firstResponse;
+    let secondResponse;
+    before((done) => {
+      // querying from similar coordinates {0.00001,0.00001}, {0.00005,0.00009}
+      request
+        .get('/suggestions?q=Waterl&latitude=0.00001&lontigude=0.00001')
+        .end((err, res) => {
+          firstResponse = res;
+          firstResponse.json = JSON.parse(res.text);
+          done(err);
+        });
+      request
+        .get('/suggestions?q=Waterl&latitude=0.00005&lontigude=-0.00009')
+        .end((err, res) => {
+          secondResponse = res;
+          secondResponse.json = JSON.parse(res.text);
+        });
+    });
+
+    it('processes first request', () => {
+      expect(firstResponse.headers).to.not.have.property('last-modified');
+    });
+
+    it('gets second request from cache', () => {
+      expect(secondResponse.headers).to.have.property('last-modified');
+    });
+
+    it('returns a 200 for both', () => {
+      expect(firstResponse.statusCode).to.equal(200);
+      expect(secondResponse.statusCode).to.equal(200);
     });
   });
 });
