e792d9ffb41ec8a70cd59400d21792efabe6cff8
Víctor Hugo Valle Castillo <victouk@gmail.com>
2020-10-26 23:10:22 -0300

    Fixing integration tests.

--- test/suggestions.js
+++ test/suggestions.js
@@ -1,6 +1,7 @@
 /* eslint-disable func-names, prefer-arrow-callback */
 
 const { expect } = require('chai');
+const nock = require('nock');
 const supertest = require('supertest');
 
 const app = require('../app');
@@ -33,6 +34,19 @@ describe('GET /suggestions', function () {
     let response;
 
     before(function (done) {
+      const suggestions = [
+        {
+          name: 'Montreal Bay',
+          latitude: '54.9169',
+          longitude: '-66.48183',
+          score: 1,
+        },
+      ];
+
+      nock('http://127.0.0.1:2345')
+        .get('/suggestions?q=Montreal')
+        .reply(200, { suggestions });
+
       request.get('/suggestions?q=Montreal').end((err, res) => {
         response = res;
         response.json = JSON.parse(res.text);
@@ -49,7 +63,54 @@ describe('GET /suggestions', function () {
       expect(response.json.suggestions).to.have.length.above(0);
     });
 
-    describe.skip('Validate the shape of the data being returned', function () {
+    describe('Validate the shape of the data being returned', function () {
+      before(function (done) {
+        const suggestions = [
+          {
+            name: 'Montreal Bay',
+            latitude: '54.9169',
+            longitude: '-66.48183',
+            score: 1,
+          },
+          {
+            name: 'London, ON, Canada',
+            latitude: '42.98339',
+            longitude: '-81.23304',
+            score: 0.9,
+          },
+          {
+            name: 'London, OH, USA',
+            latitude: '39.88645',
+            longitude: '-83.44825',
+            score: 0.5,
+          },
+          {
+            name: 'London, KY, USA',
+            latitude: '37.12898',
+            longitude: '-84.08326',
+            score: 0.5,
+          },
+          {
+            name: 'Londontowne, MD, USA',
+            latitude: '38.93345',
+            longitude: '-76.54941',
+            score: 0.3,
+          },
+        ];
+
+        nock('http://127.0.0.1:2345')
+          .get('/suggestions?q=Londo&latitude=43.70011&longitude=-79.4163')
+          .reply(200, { suggestions });
+
+        request
+          .get('/suggestions?q=Londo&latitude=43.70011&longitude=-79.4163')
+          .end((err, res) => {
+            response = res;
+            response.json = JSON.parse(res.text);
+            done(err);
+          });
+      });
+
       it('contains latitudes and longitudes', function () {
         expect(response.json.suggestions).to.satisfy((suggestions) => {
           return suggestions.every((suggestion) => {
@@ -70,7 +131,7 @@ describe('GET /suggestions', function () {
     it('contains a match', function () {
       expect(response.json.suggestions).to.satisfy((suggestions) => {
         return suggestions.some((suggestion) => {
-          return suggestion.name.test(/montreal/i);
+          return suggestion.name.match(/montreal/i);
         });
       });
     });
