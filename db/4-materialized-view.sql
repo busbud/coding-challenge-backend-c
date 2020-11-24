a32921a2c3b7c55bc102a9d8d269a884a0f908fe
Steve Ferreira <sjferreira516@gmail.com>
2020-11-23 20:58:36 -0500

    Add db script to create and index materialized view

---
+++ db/4-materialized-view.sql
@@ -0,0 +1,15 @@
+DROP MATERIALIZED VIEW IF EXISTS cities_mv;
+DROP INDEX IF EXISTS name_idx;
+
+-- Create materialized view of refined cols and fixed minimum population requirement 
+CREATE MATERIALIZED VIEW cities_mv AS
+SELECT id, name, ascii, lat, long, country, admin1, population
+FROM cities
+WHERE population > 5000;
+
+-- Create index for partial searches on name
+CREATE INDEX name_idx
+ON cities_mv
+USING GIN(to_tsvector('english', name));
+
+REFRESH MATERIALIZED VIEW cities_mv WITH DATA;
