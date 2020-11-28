5303d3d975b5421e99723d9ef3e551e8b021be11
Steve Ferreira <sjferreira516@gmail.com>
2020-11-28 15:35:25 -0500

    Add basic scoring function

--- app.js
+++ app.js
@@ -2,9 +2,46 @@ const http = require('http');
 const express = require('express');
 const accents = require('remove-accents');
 const db = require('./src/db');
+const distance = require('gps-distance');
 const PORT = process.env.PORT || 2345;
 
 const app = express();
+
+const scoreCity = (searchCity, searchLat, searchLong, asciiName, lat, long) => {
+  let score = 0;
+  const weights = {
+    doesNotStartWithWeight: 3,
+    letterMatchWeight: 2,
+    distanceWeight: 0.1
+  }
+
+  // Check if name starts search
+  const re = new RegExp(`^${searchCity}`);
+
+  if (!asciiName.match(re)) {
+    score += 1 * weights.doesNotStartWithWeight;
+  }
+
+  // Score strength of string match
+  const nameLengthDiff = asciiName.length - searchCity.length;
+  weightTotal += weights.letterMatchWeight;
+  score += nameLengthDiff * weights.letterMatchWeight;
+
+  // Score gps distance
+  if (searchLat && searchLong) {
+    const path = [
+      [ searchLat, searchLong],
+      [lat, long]
+    ];
+
+    const gpsDistance = distance(path);
+
+    weightTotal += weights.distanceWeight;
+    score += gpsDistance * weights.distanceWeight;
+  }
+
+  return score;
+}
 
 /**
  * GET /suggestions
@@ -30,11 +67,14 @@ app.get('/suggestions', async (req, res) => {
       else if (country == "CA") countryFull = "Canada"
       else countryFull = "";
 
+      // Calculate score
+      const score = scoreCity(city, latitude, longitude, ascii, lat, long);
+
       const suggestion = {
         name: `${ascii}, ${admin1}, ${countryFull}`,
         latitude: lat,
         longitude: long,
-        score: 0
+        score: score
       };
 
       return suggestion;
@@ -43,6 +83,7 @@ app.get('/suggestions', async (req, res) => {
     return res.json({ suggestions });
   }
   catch (e) {
+    return res.json({ e });
     // TODO: handle error
   }
 });
