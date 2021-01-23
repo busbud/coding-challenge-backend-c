c68e5055631150d7b12fdcd3eea1a36839654c77
Vinicius Gava <gava.vinicius@gmail.com>
2021-01-23 15:27:50 -0300

    refact suggestion controller

--- src/user/rest/controller/suggestions.controller.js
+++ src/user/rest/controller/suggestions.controller.js
@@ -3,20 +3,28 @@ const query = require('../../../query/suggestions.query');
 const suggestionResponse = require('../response/suggestion.response');
 const errorResponse = require('../response/error.response');
 
+const handleQueryParameters = (req) => {
+  const rawQueryString = req.url.split('?').slice(-1)[0];
+  const params = querystring.parse(rawQueryString);
+
+  return {
+    q: params.q || null,
+    latitude: params.latitude || null,
+    longitude: params.longitude || null,
+  };
+};
+
 const handler = async (req, res) => {
   if (req.url.indexOf('/suggestions') !== 0) {
     return false;
   }
 
-  const rawQueryString = req.url.split('?').slice(-1)[0];
-  const params = querystring.parse(rawQueryString);
-  if (params.q === undefined) {
+  const { q, latitude, longitude } = handleQueryParameters(req);
+  if (q === null) {
     errorResponse.badRequest(res, "Querystring 'q' must be informed.");
     return true;
   }
-  const { q } = params;
-  const latitude = params.latitude || null;
-  const longitude = params.longitude || null;
+
   query.search(q, latitude, longitude)
     .then((results) => suggestionResponse.success(res, results))
     .catch((reason) => errorResponse.internalError(res, reason));
