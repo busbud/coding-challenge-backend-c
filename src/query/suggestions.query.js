f2b2e91d04f849119cbf26aad4a17e40e40628e7
Vinicius Gava <gava.vinicius@gmail.com>
2021-01-23 18:31:57 -0300

    POC REDIS AS CACHE

--- src/query/suggestions.query.js
+++ src/query/suggestions.query.js
@@ -1,4 +1,24 @@
+const md5 = require('crypto');
+const redis = require('redis');
 const adapter = require('../infrastructure/suggestions/search.adapter');
 
-module.exports.search = async (q, latitude, longitude) => adapter
-  .fulltextSearch(q, latitude, longitude);
+const redisClient = redis.createClient();
+
+module.exports.search = async (q, latitude, longitude) => new Promise((resolve, reject) => {
+  const hash = md5.createHash('md5')
+    .update(`q_${q}_l_${latitude}_l_${longitude}`).digest('hex');
+  const key = `suggestion_${hash}`;
+  redisClient.get(key, (err, reply) => {
+    if (reply !== null) {
+      console.log('hit redis');
+      resolve(JSON.parse(reply));
+      return;
+    }
+
+    adapter.fulltextSearch(q, latitude, longitude).then((result) => {
+      console.log('hit elastic');
+      redisClient.set(key, JSON.stringify(result));
+      resolve(result);
+    }).catch((reason) => reject(reason));
+  });
+});
