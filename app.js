5c1e38fda2e68fea0db5993587faa146c65c1286
Feras Allaou <feras@Ferass-MacBook-Pro.local>
2021-02-17 16:41:57 +0300

    Fixed Node-V & Port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3104;
+const port = process.env.PORT || 3101;
 const Suggestions = require('./Services/Suggestions/router');
 const expressLimiter = require('express-rate-limit');
 
