46c09d3d4184a8cb41c6f9c0cff8445da0083cf6
Arkerone <shaita.axel@gmail.com>
2021-06-30 19:51:02 +0200

    feat: merge the services objects into an index file

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const cacheManager = require('./cache')
+const logger = require('./logger')
+const searchEngine = require('./searchEngine')
+
+module.exports = {
+  cacheManager,
+  logger,
+  searchEngine
+}
