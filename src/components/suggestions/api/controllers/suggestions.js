6aa49da3a7b1d593fb47c9f9f7262341edaed3dc
Arkerone <shaita.axel@gmail.com>
2021-07-01 14:44:00 +0200

    fix: return status 404 if suggestions are empty

--- src/components/suggestions/api/controllers/suggestions.js
+++ src/components/suggestions/api/controllers/suggestions.js
@@ -3,10 +3,14 @@ module.exports = async function (request, reply) {
 
   const cacheKey = JSON.stringify(query)
   return this.cacheManager.wrap(cacheKey, async () => {
+    let statusCode = 200
     const repository = this.searchEngineClient.getRepository('suggestions')
     const suggestions = await repository.findSuggestions(query.q, query)
-    return {
-      suggestions
+
+    if (suggestions.length === 0) {
+      statusCode = 404
     }
+
+    reply.status(statusCode).send({ suggestions })
   })
 }
