a991e494bc072e7d639e2a031e713768ce7bffb2
Arkerone <shaita.axel@gmail.com>
2021-07-01 14:42:16 +0200

    fix: use logger level from the environment variables

--- src/config/logger.js
+++ src/config/logger.js
@@ -9,11 +9,13 @@ module.exports = (env) => ({
       filename: env.LOGGER_TRANSPORT_ROTATING_FILE_FILE_NAME,
       size: env.LOGGER_TRANSPORT_ROTATING_FILE_SIZE,
       interval: env.LOGGER_TRANSPORT_ROTATING_FILE_INTERVAL,
-      compress: env.LOGGER_TRANSPORT_ROTATING_FILE_COMPRESS
+      compress: env.LOGGER_TRANSPORT_ROTATING_FILE_COMPRESS,
+      level: env.LOGGER_TRANSPORT_ROTATING_FILE_LEVEL
     },
     stdout: {
       enabled: env.LOGGER_TRANSPORT_STDOUT_ENABLED,
-      pretty: env.LOGGER_TRANSPORT_STDOUT_PRETTY
+      pretty: env.LOGGER_TRANSPORT_STDOUT_PRETTY,
+      level: env.LOGGER_TRANSPORT_STDOUT_LEVEL
     }
   }
 })
