4755610e95c4f95f163c759cc0689db075d6863e
Arkerone <shaita.axel@gmail.com>
2021-07-02 23:20:17 +0200

    fix: fix a mistake about the usage of cache

--- src/components/suggestions/api/controllers/suggestions.js
+++ src/components/suggestions/api/controllers/suggestions.js
@@ -2,15 +2,17 @@ module.exports = async function (request, reply) {
   const { query } = request
 
   const cacheKey = JSON.stringify(query)
-  return this.cacheManager.wrap(cacheKey, async () => {
-    let statusCode = 200
+
+  const suggestions = await this.cacheManager.wrap(cacheKey, async () => {
     const repository = this.searchEngineClient.getRepository('suggestions')
-    const suggestions = await repository.findSuggestions(query.q, query)
-
-    if (suggestions.length === 0) {
-      statusCode = 404
-    }
-
-    reply.status(statusCode).send({ suggestions })
+    return repository.findSuggestions(query.q, query)
   })
+
+  let statusCode = 200
+
+  if (suggestions.length === 0) {
+    statusCode = 404
+  }
+
+  reply.status(statusCode).send({ suggestions })
 }
