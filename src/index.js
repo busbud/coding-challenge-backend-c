f316039647a43571a580226e9f672c9c4d4d85e7
Benoit de Bernardy <bdebernardy@ea.com>
2021-09-23 18:00:27 +0200

    Setup nodemon

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var port = process.env.PORT ?? 2345;
+
+module.exports = http
+  .createServer((req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+
+    if (req.url.indexOf('/suggestions') === 0) {
+      res.end(
+        JSON.stringify({
+          suggestions: []
+        })
+      );
+    } else {
+      res.end();
+    }
+  })
+  .listen(port, () => {
+    console.log(`App listening to port ${port}`);
+  });
