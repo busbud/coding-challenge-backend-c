6bd6d615531fd343711e54f831d26aba3b2348be
Murilo Campaner <apps@campaner.dev>
2021-11-09 00:27:27 -0300

    feat: added .env library

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 import express from 'express';
+
+const PORT = process.env.APP_PORT || 8000;
+
 const app = express();
-const PORT = 8000;
 app.get('/', (req, res) => res.send('Busbud Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
