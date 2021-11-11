521375f7f01eb5e5951e191b59b1d13df23cc48b
Murilo Campaner <apps@campaner.dev>
2021-11-11 13:24:28 -0300

    refactor: autocomplete controller

--- src/controllers/AutocompleteController.ts
+++ src/controllers/AutocompleteController.ts
@@ -1,37 +1,42 @@
-import elasticsearch from 'elasticsearch';
-
-import RequestContract from "../types/RequestContract";
 import SearchService from "../services/SearchService";
+import { SuggestionsRequest } from "../types";
 
 //** Suggestions endpoint
-const suggestions: RequestContract = async (request, response, next) => {
-  const { q: term, latitude, longitude, limit = 50, offset = 0 } = request.query;
-  
-  const { hits: { hits, max_score } } = await SearchService.search({
-    term,
-    location: latitude && longitude ?{
-      lat: latitude,
-      lon: longitude,
-    } : undefined,
-    limit, 
-    offset,
-  })
-  
-  const suggestions = hits.map(({ _source, _score }: any) => {
+const suggestions: SuggestionsRequest = async (request, response, next) => {
+  const params = request.query;
+
+  const location = params.latitude && params.longitude
+    ? { lat: params.latitude, lon: params.longitude}
+    : undefined;
+
+  const {
+    hits: { hits, max_score },
+  } = await SearchService.search({
+    term: params.q,
+    location,
+    limit: params.limit,
+    offset: params.offset,
+  });
+
+  /*
+  @TODO: Move this logic to ES Query, selecting
+  only the needed fields to preserve network resources 
+  */
+  const suggestions = hits.map(({ _source, _score }) => {
     return {
-      "name": `${_source.name}, ${_source.admin1_code}, ${_source.country_code}`, 
-      "latitude": _source.location.lat,
-      "longitude": _source.location.lon,
-      "score": _score * (1 / max_score),
+      name: `${_source.name}, ${_source.admin1_code}, ${_source.country_code}`,
+      latitude: _source.location.lat,
+      longitude: _source.location.lon,
+      score: _score * (1 / max_score),
     };
-  })
-  
+  });
+
   const status = suggestions.length > 0 ? 200 : 404;
-  response.status(status).send({ suggestions })
+  response.status(status).send({ suggestions });
 
   next();
-}
+};
 
-export default ({ 
-  suggestions 
-});
+export default {
+  suggestions,
+};
