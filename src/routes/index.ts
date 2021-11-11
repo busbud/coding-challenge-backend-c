e9bd837165c37de5486ced7f215964e272745acd
Murilo Campaner <apps@campaner.dev>
2021-11-11 13:24:42 -0300

    refactor: removed request contract type

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@
 import express from 'express';
 const router = express.Router();
 import AutocompleteController from '../controllers/AutocompleteController';
-import RequestContract from '../types/RequestContract';
  
 /** Define routes */
 router.get('/suggestions', AutocompleteController.suggestions);
