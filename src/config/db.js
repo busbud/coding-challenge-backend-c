09e9f27782f9756083b502cfda6e775aea5d9db4
Svetlana Brind <svetclaire@yahoo.ca>
2021-11-28 16:29:07 -0500

    add ssl dialectOptions for heroku

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+const {
+  DATABASE_URL = 'suggestions_dev',
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+} = process.env;
 
-module.exports = new Sequelize('suggestions_dev', DB_USERNAME, DB_PASSWORD, {
+module.exports = new Sequelize(DATABASE_URL, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: 'postgres',
   operatorsAliases: false,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL ? true : false,
+  },
   pool: {
     max: 5,
     min: 0,
