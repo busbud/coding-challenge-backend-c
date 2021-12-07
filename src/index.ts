5fdfd9b4a00116e14c076a97c4ce1a4650f7e101
Eduardo Godoy <eduardo.ewgo@gmail.com>
2021-12-06 22:56:48 -0500

    fix: minor ops

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,19 @@ import suggestionRoutes from './routes/suggestion-routes';
 
 const app = express();
 
-(async () => {
-  // Middleware
-  app.use(express.json());
+// Middleware
+app.use(express.json());
 
-  // Routes
-  app.use('/suggestion', suggestionRoutes);
+// Routes
+app.use('/suggestion', suggestionRoutes);
 
-  // Basic error handling
-  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack || err.message);
-    if (err.statusCode) res.status(err.statusCode).json(err);
-    else res.status(500).json({ message: 'Uh, something went wrong' });
-  });
+// Basic error handling
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack || err.message);
+  if (err.statusCode === 400) res.status(err.statusCode).json(err);
+  else res.status(500).json({ message: 'Uh, something went wrong' });
+});
 
-  app.listen(process.env.PORT, () => {
-    console.log(`Live at ${process.env.PORT}!`);
-  });
-})();
+app.listen(process.env.PORT, () => {
+  console.log(`Live at ${process.env.PORT}!`);
+});
