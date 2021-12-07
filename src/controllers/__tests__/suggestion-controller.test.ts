3adf6201e2f8576b2cd133df715e192cb85fd96e
Eduardo Godoy <eduardo.ewgo@gmail.com>
2021-12-06 23:13:07 -0500

    chore: unit tests for caching on controller

--- src/controllers/__tests__/suggestion-controller.test.ts
+++ src/controllers/__tests__/suggestion-controller.test.ts
@@ -1,11 +1,13 @@
 /* eslint-disable import/first */
 
 const mockGetSuggestions = jest.fn();
+const mockSetKey = jest.fn();
 
 import { Request, Response } from 'express';
 import { SuggestionController } from '../suggestion-controller';
 import { SuggestionService } from '../../services/suggestion-service';
 import { ElasticSearchClient } from '../../clients/elasticsearch-client';
+import { RedisClient } from '../../clients/redis-client';
 
 jest.mock('../../services/suggestion-service', () => ({
   SuggestionService: jest.fn().mockImplementation(() => ({
@@ -17,13 +19,20 @@ jest.mock('../../clients/elasticsearch-client', () => ({
   ElasticSearchClient: jest.fn().mockImplementation()
 }));
 
+jest.mock('../../clients/redis-client', () => ({
+  RedisClient: jest.fn().mockImplementation(() => ({
+    setKey: mockSetKey
+  }))
+}));
+
 const mockedEsClient = new ElasticSearchClient();
+const mockedRedisClient = new RedisClient();
 const mockedSuggestionService: SuggestionService = new SuggestionService(mockedEsClient);
 let suggestionController: SuggestionController;
 
 describe('SuggestionController', () => {
   beforeEach(() => {
-    suggestionController = new SuggestionController(mockedSuggestionService);
+    suggestionController = new SuggestionController(mockedSuggestionService, mockedRedisClient);
   });
 
   describe('getSuggestions', () => {
@@ -56,6 +65,22 @@ describe('SuggestionController', () => {
 
       expect(mockGetSuggestions).toHaveBeenCalledWith(term, location);
       expect(mockedResJson).toHaveBeenCalledWith({ suggestions: [] });
+    });
+
+    it('should set cache', async () => {
+      const term = '_term_';
+      const location = { lat: 1, long: 2 };
+
+      const mockedResJson = jest.fn();
+      const mockedReq = { query: { q: term, ...location } } as unknown as Request;
+      const mockedRes = { json: mockedResJson } as unknown as Response;
+
+      mockGetSuggestions.mockResolvedValueOnce([{ hey: 'there' }]);
+
+      await suggestionController.getSuggestions(mockedReq, mockedRes);
+
+      expect(mockSetKey).toHaveBeenCalledTimes(1);
+      expect(mockSetKey).toHaveBeenCalledWith(`${term}:${location.lat}:${location.long}`, [{ hey: 'there' }]);
     });
   });
 });
