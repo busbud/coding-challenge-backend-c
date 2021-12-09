fbe0100ca9a3e6796ff296919ed304f95288a026
StephaneC <Stephane.castrec@gmail.com>
2021-12-09 09:00:23 +0100

    Handle no q param

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ var host = process.env.HOST || '127.0.0.1';
 
 module.exports = http.createServer(async function (req, res) {
   res.writeHead(404, { 'Content-Type': 'text/plain' });
-  
+
 
   if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-          msg: "App is running"
-        }));
-    } else if (req.url.indexOf('/suggestions') === 0) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    const params = url.parse(req.url,true).query
+    res.end(JSON.stringify({
+      msg: "App is running"
+    }));
+  } else if (req.url.indexOf('/suggestions') === 0) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const params = url.parse(req.url, true).query
     console.log(params.q)
+    if (!params.q) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        error: "Please add a q param to your request"
+      }));
+      return;
+    }
     const suggestions = await suggestionsService.getSuggestions(params.q, params.latitude, params.longitude);
     if (suggestions.length === 0) {
       res.writeHead(404, { 'Content-Type': 'application/json' });
