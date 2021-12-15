a1baf3a1ed06345ca6444a0eabf97c57d6006ca0
Manuel DUPONT <manuel@lajavaness.com>
2021-12-15 13:43:39 +0100

    Add rate limiter

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,24 @@ import express, {Application} from 'express'
 import * as path from 'path'
 import cityController from "./controller/city-controller";
 import getRouter from './routes'
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
 
 export default class Server {
   init(): Application {
     const app = express()
         .use(express.json())
         .use('/', getRouter()) // Local API
+        .use(limiter)
+        // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB or API Gateway, Nginx, etc)
+        // see https://expressjs.com/en/guide/behind-proxies.html
+        .set('trust proxy', 1)
+
 
     return app
   }
