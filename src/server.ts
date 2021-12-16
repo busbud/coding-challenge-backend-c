72ab306d242b5b8c0ac90b06a6739b21391f1ec1
Manuel DUPONT <manuel@lajavaness.com>
2021-12-16 10:19:43 +0100

    Add documentation

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import express, {Application} from 'express'
 import getRouter from './routes'
 import rateLimit from 'express-rate-limit'
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-
-
+/**
+ * This class represents an express server.
+ */
 export default class Server {
   init(): Application {
+    // New limiter for high levels traffic.
+    const limiter = rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100 // limit each IP to 100 requests per windowMs
+    });
+
     const app = express()
         .use(express.json())
         .use('/', getRouter()) // Local API
