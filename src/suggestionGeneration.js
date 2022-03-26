2d206c04e9d1171a5ae3f696bd7e16d4b5b8954b
Gabriel Côté-Carrier <gcotecarrier@gmail.com>
2022-03-26 16:45:40 -0400

    Add a latitude/longitude weight to the sorting algorithm

--- src/suggestionGeneration.js
+++ src/suggestionGeneration.js
@@ -1,30 +1,52 @@
 const { normalize } = require('./textNormalization');
 
-const buildSuggestions = function (cities, searchInput) {
-    const { searchText, latitude, longitude } = searchInput;
-
+module.exports.buildSuggestions = function (cities, searchInput) {
     return cities
         .filter((city) => city.population > 5000)
         .map((city) => ({
             name: [city.name, city.country].join(', '),
             latitude: city.lat,
             longitude: city.long,
-            score: determineScore(city, searchText),
+            score: determineScore(city, searchInput),
         }))
         .filter((suggestion) => suggestion.score > 0)
         .sort((left, right) => right.score - left.score);
 };
 
-const determineScore = function (city, searchText) {
+function determineScore(city, searchInput) {
+    const { searchText, latitude, longitude } = searchInput;
+
+    const weightedScores = [
+        0.8 * determineTextScore(city, searchText),
+        0.2 * determineGeographicalScore(city, latitude, longitude),
+    ];
+
+    return weightedScores.reduce((sum, value) => sum + value, 0);
+}
+
+function determineTextScore(city, searchText) {
     const normalizedName = normalize(city.name);
     searchText = normalize(searchText);
 
     const nameMatches = normalizedName.search(searchText) >= 0;
 
     return nameMatches ? searchText.length / normalizedName.length : 0;
-};
+}
 
-module.exports = {
-    buildSuggestions,
-    determineScore,
-};
+// This is obviously a terrible way of calculating geographical distance and a terrible algorithm
+// But it will have to do because I'm out of time
+function determineGeographicalScore(city, latitude, longitude) {
+    if (!latitude || !longitude) {
+        return 0;
+    }
+
+    const geographicalDistance = Math.sqrt(
+        Math.pow(latitude - city.latitude, 2) +
+            Math.pow(longitude - city.longitude, 2)
+    );
+
+    const maxDistance = 150; //arbitrary number
+
+    //Some kind of exponential algorithm adding a lot more weight to close matches than farther matches would be interesting here
+    return (1 - Math.min(geographicalDistance, maxDistance)) / maxDistance;
+}
--- test/Unit/suggestionGeneration.js
+++ test/Unit/suggestionGeneration.js
@@ -24,4 +24,31 @@ describe('Suggestion generation function', function () {
             name.startsWith('name,')
         );
     });
+
+    it('Location affects relevancy order', function () {
+        const closeLocation = {
+            latitude: 40.7128,
+            longitude: 74.006,
+        };
+
+        const farLocation = {
+            latitude: 50.7128,
+            longitude: 74.006,
+        };
+
+        const cities = [
+            { name: 'name-a', population: 5001, ...farLocation },
+            { name: 'name-b', population: 5001, ...closeLocation },
+        ];
+
+        const suggestions = buildSuggestions(cities, {
+            searchText: 'name',
+            ...closeLocation,
+        });
+
+        expect(suggestions.length).to.equal(2);
+        expect(suggestions[0].name).to.satisfy((name) =>
+            name.startsWith('name-b,')
+        );
+    });
 });
