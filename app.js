bc8ff725e3f8460cad2f9846dc9e39c754c904c4
Gabriel Côté-Carrier <gcotecarrier@gmail.com>
2022-03-26 14:12:44 -0400

    Introduce a concept of 404 vs 200 to app.js

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ var http = require('http');
 var port = process.env.PORT || 2345;
 
 module.exports = http.createServer(function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/plain'});
+  
 
   if (req.url.indexOf('/suggestions') === 0) {
-    res.end(JSON.stringify({
-      suggestions: []
-    }));
+
+    const suggestions = [];
+
+    const returnCode = suggestions.length > 0 ? 200 : 404;
+
+    res.writeHead(returnCode, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ suggestions: suggestions }));
   } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end();
   }
 }).listen(port, '127.0.0.1');
