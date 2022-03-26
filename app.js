706787f07ee05e9e173fb2e9c16a64c28a1c18a3
Gabriel Côté-Carrier <gcotecarrier@gmail.com>
2022-03-26 17:36:01 -0400

    Remove the localhost specification

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ module.exports = http
             res.end();
         }
     })
-    .listen(port, '127.0.0.1');
+    .listen(port);
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
 
