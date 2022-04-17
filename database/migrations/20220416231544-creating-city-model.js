88c53cd0c46ed9468b0bc04b90aed56d745e9dee
Hama <hama75020@gmail.com>
2022-04-17 15:08:58 +0200

    complete City model

--- database/migrations/20220416231544-creating-city-model.js
+++ database/migrations/20220416231544-creating-city-model.js
@@ -17,6 +17,66 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      altNames: {
+        type: Sequelize.JSON,
+        allowNull: false
+      },
+      latitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      longitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      featClass: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      featCode: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cc2: {
+        type: Sequelize.JSON,
+        allowNull: false
+      },
+      admin1: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      admin2: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      admin3: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      admin4: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      population: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      elevation: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      dem: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      tz: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       modifiedAt: {
         type: Sequelize.DATE,
         allowNull: true
--- src/domain/model/entity/city.ts
+++ src/domain/model/entity/city.ts
@@ -1,13 +1,82 @@
 import { Table, Column, Model, DataType } from 'sequelize-typescript'
+import CityDTO from '../value-object/city-dto';
 
 @Table({timestamps: false})
 export class City extends Model {
   @Column({ type: DataType.INTEGER })
-  geonameId!: number
+  public geonameId: number
 
   @Column({ type: DataType.STRING })
-  name!: string
+  public name: string
+
+  @Column({ type: DataType.JSON })
+  public altNames: string[];
+
+  @Column({ type: DataType.FLOAT })
+  public latitude: number;
+  
+  @Column({ type: DataType.FLOAT })
+  public longitude: number;
+  
+  @Column({ type: DataType.STRING })
+  public featClass: string;
+
+  @Column({ type: DataType.STRING })
+  public featCode: string;
+  
+  @Column({ type: DataType.STRING })
+  public country: string;
+  
+  @Column({ type: DataType.JSON })
+  public cc2: string[];
+
+  @Column({ type: DataType.STRING })
+  public admin1: string;
+
+  @Column({ type: DataType.STRING })
+  public admin2: string;
+
+  @Column({ type: DataType.STRING })
+  public admin3: string;
+
+  @Column({ type: DataType.STRING })
+  public admin4: string;
+
+  @Column({ type: DataType.INTEGER })
+  public population: number;
+
+  @Column({ type: DataType.INTEGER })
+  public elevation: number;
+
+  @Column({ type: DataType.STRING })
+  public dem: string;
+
+  @Column({ type: DataType.STRING })
+  public tz: string;
 
   @Column({ type: DataType.DATE })
   modifiedAt: Date;
+
+  static createFromDTO(dto: CityDTO): City {
+    return new this({
+      geonameId: dto.geonameid,
+      name: dto.name,
+      altNames: dto.alt_name,
+      latitude: dto.lat,
+      longitude: dto.long,
+      featClass: dto.feat_class,
+      featCode: dto.feat_code,
+      country: dto.country,
+      cc2: dto.cc2,
+      admin1: dto.admin1,
+      admin2: dto.admin2,
+      admin3: dto.admin3,
+      admin4: dto.admin4,
+      population: dto.population,
+      elevation: dto.elevation,
+      dem: dto.dem,
+      tz: dto.tz,
+      modifiedAt: dto.modified_at
+    });
+  }
 }
