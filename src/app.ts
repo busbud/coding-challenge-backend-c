5ff4c6087177c6031f4016ed4011a1a305ed16bf
maryam babalola <maryam.babalola@q4inc.com>
2023-01-22 15:46:45 -0500

    add host url based on env

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import http from "http";
 import { ExpressServer } from "./config";
 import config from "./config/env";
 
+const hostUrl = config.environment === "development" ? "127.0.0.1" : "0.0.0.0";
+
 const app = new ExpressServer().getInstance();
 
-http.createServer(app).listen(config.port, "0.0.0.0");
-console.log("Server running at http://127.0.0.1:%d/suggestions", config.port);
+http.createServer(app).listen(config.port, hostUrl);
+console.log(`Server running at http://${hostUrl}:${config.port}/suggestions`);
 
 export default app;
