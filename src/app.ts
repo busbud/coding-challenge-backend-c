2901b69967f33962bd9435a6254e096acbecf83a
maryam babalola <maryam.babalola@q4inc.com>
2023-01-22 15:18:26 -0500

    use config port

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import http from "http";
 import { ExpressServer } from "./config";
 import config from "./config/env";
 
-console.log("config: ", config);
-const PORT = Number(process.env.PORT) || 2345;
 const app = new ExpressServer().getInstance();
 
-http.createServer(app).listen(PORT, "127.0.0.1");
-console.log("Server running at http://127.0.0.1:%d/suggestions", PORT);
+http.createServer(app).listen(config.port, "127.0.0.1");
+console.log("Server running at http://127.0.0.1:%d/suggestions", config.port);
 
 export default app;
