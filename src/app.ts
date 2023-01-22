4a309df4ef877d2374315a616c8a0eaee3d5e92d
maryam babalola <maryam.babalola@q4inc.com>
2023-01-22 14:50:30 -0500

    refactor app file

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,7 @@ import config from "./config/env";
 const PORT = config.port;
 const app = new ExpressServer().getInstance();
 
-const httpServer = http.createServer(app);
-
-httpServer.listen(PORT, "127.0.0.1", () => {
-  console.log(`🚀 Server running at http://127.0.0.1:${PORT}/suggestions`);
-});
+http.createServer(app).listen(PORT, "127.0.0.1");
+console.log("Server running at http://127.0.0.1:%d/suggestions", PORT);
 
 export default app;
