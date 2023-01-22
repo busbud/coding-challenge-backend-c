ebebf5c2db3b5da1a3df37bb455eb239d5892246
maryam babalola <maryam.babalola@q4inc.com>
2023-01-22 15:35:03 -0500

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import config from "./config/env";
 
 const app = new ExpressServer().getInstance();
 
-http.createServer(app).listen(config.port);
+http.createServer(app).listen(config.port, "0.0.0.0");
 console.log("Server running at http://127.0.0.1:%d/suggestions", config.port);
 
 export default app;
