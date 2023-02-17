a5221f92f9dbe21bb2a541ecc91f1f3cfba5a4ed
Prince Brown <codingproprince@gmail.com>
2023-02-17 16:56:56 -0500

    Heroku

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes/main'));
+// app.use(require('./routes/main'));
 
  
 
