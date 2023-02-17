bfa86257b2efbc64403348bda6c682c2ff6ca639
Prince Brown <codingproprince@gmail.com>
2023-02-17 09:16:55 -0500

    testing completed

--- dist/services/redisClient.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisClient = void 0;
-const redis_1 = require("redis");
-exports.redisClient = (0, redis_1.createClient)();
-exports.redisClient.on('error', err => console.log('Redis Client Error', err));
-//# sourceMappingURL=redisClient.js.map
