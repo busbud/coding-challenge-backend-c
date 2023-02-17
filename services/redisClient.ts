b319c1099576def70ebc20b6069dcd15814318c5
Prince Brown <codingproprince@gmail.com>
2023-02-17 18:02:52 -0500

    Heroku

--- services/redisClient.ts
+++ services/redisClient.ts
@@ -1,6 +1,6 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 import { createClient } from 'redis';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const REDIS_PORT = process.env.REDIS_PORT_ID as string;
 
