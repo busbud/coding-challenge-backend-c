4ed498160436f8e9d4ea2aa29d29e1da2d41352f
Prince Brown <codingproprince@gmail.com>
2023-02-17 17:45:56 -0500

    Heroku

--- build/services/redisClient.js
+++ build/services/redisClient.js
@@ -7,6 +7,7 @@ exports.redisClient = void 0;
 const redis_1 = require("redis");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const REDIS_PORT = process.env.REDIS_PORT_ID;
 exports.redisClient = (0, redis_1.createClient)({
     password: process.env.REDIS_PASSWORD_ID,
     socket: {
--- services/redisClient.ts
+++ services/redisClient.ts
@@ -2,6 +2,7 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const REDIS_PORT = process.env.REDIS_PORT_ID as string;
 
 export const redisClient = createClient({
     password: process.env.REDIS_PASSWORD_ID,
