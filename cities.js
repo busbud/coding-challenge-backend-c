655d2229e7868b65b49f7696fafb774870ad094b
Kyle Spaans <kyle@spaans.ca>
2023-03-14 12:03:07 -0400

    feat(cities): parse and return cities data

---
+++ cities.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs'
+
+import { parse } from 'csv-parse'
+
+// Load the cities data into memory on startup. This works while the data is
+// small enough to not affect app startup time or memory footprint, and not
+// changing frequently. If we expected this data to change more often it would
+// live in a database.
+const c = []
+fs.createReadStream('./data/cities_canada-usa.tsv')
+
+  .pipe(parse({
+    // the delimiter in the data file is the tab \t char
+    delimiter: '\t',
+    // " and ' are used in some phonetic spellings of city names, so don't try
+    // to parse any quoted fields
+    quote: false,
+    // return data as objects, using the first row as keys
+    columns: true
+  }))
+  .on('data', row => {
+    c.push(row)
+  })
+  .on('end', () => {
+    console.log('  data parsing complete!')
+  })
+
+export const cities = {
+  c
+}
--- service.js
+++ service.js
@@ -1,8 +1,27 @@
+import { cities } from './cities.js'
+
 export class Service {
   constructor() {}
 
   async getSuggestions(req, resp) {
-    const result = { suggestions: [] }
+    const result = { suggestions: cities.c.map(x => {
+      let n
+      let lat
+      let lon
+      if (x != undefined) {
+        n = x.name
+        lat = x.lat
+        lon = x['long']
+      } else {
+        n = "nil"
+      }
+      return {
+        name: n,
+        latitude: lat,
+        longitude: lon,
+        score: "0.5"
+      }
+    })}
     return result
   }
 }
