888db5e5f1f31de12c664000e5db4a4eaf4da6b4
rodrigovk <rodrigocasadeialanis@hotmail.com>
2023-08-10 19:15:22 -0300

    test: add first new test using jest in typescript

---
+++ test/suggestions.spec.ts
@@ -0,0 +1,35 @@
+import { setupApp } from '../src/app';
+
+import { Express } from 'express';
+import request from 'supertest';
+
+let app: Express;
+
+describe('GET /suggestions', () => {
+  beforeAll(async () => {
+    app = setupApp();
+  });
+
+  describe('with a non-existent city', function () {
+    let response;
+
+    beforeAll((done) => {
+      request(app)
+        .get('/suggestions?q=SomeRandomCityInTheMiddleOfNowhere')
+        .end((err, res) => {
+          response = res;
+          response.json = JSON.parse(res.text);
+          done(err);
+        });
+    });
+
+    test('returns a 404', () => {
+      expect(response.statusCode).toEqual(404);
+    });
+
+    test('returns an empty array of suggestions', () => {
+      expect(response.json.suggestions).toBeInstanceOf(Array);
+      expect(response.json.suggestions).toHaveLength(0);
+    });
+  });
+});
