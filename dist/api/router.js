1c6bfdab064ffa22d74c018019661f0135a3b129
Michael Nolan <94009684+mklnln@users.noreply.github.com>
2023-08-13 13:58:09 -0400

    Delete dist

--- dist/api/router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.suggestionsRouter = void 0;
-const express_1 = require("express");
-const findCities_1 = require("./handlers/findCities");
-exports.suggestionsRouter = (0, express_1.Router)();
-exports.suggestionsRouter.get('/', findCities_1.findCities);
