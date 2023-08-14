c69b8273f9435c2b33769a414e308472f5947cd8
rodrigovk <rodrigocasadeialanis@hotmail.com>
2023-08-14 12:25:09 -0300

    feat: remove routes to test and fix proxy issue

--- src/routes/ip.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-export default function (router: Router) {
-  router.get('/ip', (req, res) => res.send(req.ip));
-  router.get('/x-forwarded-for', (req, res) =>
-    res.send(req.headers['x-forwarded-for'])
-  );
-}
