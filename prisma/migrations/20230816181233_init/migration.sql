3bbff2af61f1f400b133f4847a4f88b46e3babdc
jaskarnmankoo <jaskarn.mankoo@mail.utoronto.ca>
2023-08-16 14:13:30 -0400

    ran initial migration using Render spun up PostgreSQL

---
+++ prisma/migrations/20230816181233_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Location" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "lat" DOUBLE PRECISION NOT NULL,
+    "long" DOUBLE PRECISION NOT NULL,
+    "country" VARCHAR(2) NOT NULL,
+    "state" VARCHAR(2) NOT NULL,
+    "population" INTEGER NOT NULL,
+
+    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
+);
