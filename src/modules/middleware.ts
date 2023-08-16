170c5d6dbfadbaaf65d2879bc8400ca861108d27
jaskarnmankoo <jaskarn.mankoo@mail.utoronto.ca>
2023-08-16 18:18:37 -0400

    add rate limit

--- src/modules/middleware.ts
+++ src/modules/middleware.ts
@@ -1,6 +1,15 @@
 import { validationResult } from 'express-validator';
+import rateLimit from 'express-rate-limit';
 
 import type { Request, Response, NextFunction } from 'express';
+
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: 'You have exceeded the 100 requests in 15 minutes limit!',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
   const result = validationResult(req);
@@ -11,4 +20,4 @@ const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
   res.status(400).send({ errors });
 };
 
-export { handleInputErrors };
+export { rateLimiter, handleInputErrors };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import { rateLimiter } from './modules/middleware';
 import router from './router';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(morgan(process.env.MORGAN_FORMAT || 'dev'));
 
 app.use('/', router);
